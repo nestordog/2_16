46ab68e2aa2de0d0632fc7f0dbc2230e27b3eb35
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2009-12-27 21:20:36 +0000

    add getAll methods
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@2474 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/service/EntityServiceImpl.java
+++ src/com/algoTrader/service/EntityServiceImpl.java
@@ -1,5 +1,13 @@
 package com.algoTrader.service;
 
+import org.apache.commons.lang.ArrayUtils;
+
+import com.algoTrader.entity.Account;
+import com.algoTrader.entity.Position;
+import com.algoTrader.entity.Rule;
+import com.algoTrader.entity.Security;
+import com.algoTrader.entity.Transaction;
+
 public class EntityServiceImpl extends com.algoTrader.service.EntityServiceBase {
 
     protected com.algoTrader.entity.Security handleGetSecurity(int id) throws java.lang.Exception {
@@ -28,6 +36,37 @@ public class EntityServiceImpl extends com.algoTrader.service.EntityServiceBase 
     protected com.algoTrader.entity.Transaction handleGetTransaction(int id) throws java.lang.Exception {
         // @todo implement protected com.algoTrader.entity.Transaction
         // handleGetTransaction(int id)
+        return null;
+    }
+
+    @Override
+    protected Security[] handleGetAllSecurities() throws Exception {
+
+
+        return (Security[])getSecurityDao().loadAll().toArray(new Security[0]);
+    }
+
+    @Override
+    protected Account handleGetAllAccounts() throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    protected Position handleGetAllPositions() throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    protected Rule handleGetAllRules() throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    protected Transaction handleGetAllTransactions() throws Exception {
+        // TODO Auto-generated method stub
         return null;
     }
 
