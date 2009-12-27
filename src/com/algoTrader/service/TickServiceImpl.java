16e7e2fa590848a043266d38587ec935f5d19501
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2009-12-27 21:21:12 +0000

    add getStockOption
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@2475 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/service/TickServiceImpl.java
+++ src/com/algoTrader/service/TickServiceImpl.java
@@ -8,7 +8,9 @@ import java.text.NumberFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -17,6 +19,7 @@ import java.util.Map;
 import javax.xml.transform.TransformerException;
 
 import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
 import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.log4j.Logger;
@@ -29,8 +32,12 @@ import org.w3c.tidy.Tidy;
 
 import com.algoTrader.entity.Security;
 import com.algoTrader.entity.StockOption;
+import com.algoTrader.entity.StockOptionImpl;
 import com.algoTrader.entity.Tick;
 import com.algoTrader.entity.TickImpl;
+import com.algoTrader.enumeration.Currency;
+import com.algoTrader.enumeration.Market;
+import com.algoTrader.enumeration.OptionType;
 import com.algoTrader.util.BigDecimalUtil;
 import com.algoTrader.util.CsvWriter;
 import com.algoTrader.util.HttpClientUtil;
@@ -45,7 +52,9 @@ public class TickServiceImpl extends TickServiceBase {
     private static String password = PropertiesUtil.getProperty("swissquote.password");
     private static int timeout = Integer.parseInt(PropertiesUtil.getProperty("swissquote.timeout"));
 
-    private static String itemUrl = "http://www.swissquote.ch/sq_mi/public/market/Detail.action?s=";
+    private static String tickUrl = "http://www.swissquote.ch/sq_mi/public/market/Detail.action?s=";
+
+    private static final String optionUrl = "http://www.swissquote.ch/sq_mi/market/derivatives/optionfuture/OptionFuture.action?market=eu&type=option&sector=&group=id&type=option";
 
     private static Logger logger = Logger.getLogger(TickServiceImpl.class.getName());
 
@@ -98,83 +107,7 @@ public class TickServiceImpl extends TickServiceBase {
 
     protected Tick handleRetrieveTick(Security security) throws ParseException, TransformerException, IOException {
 
-        GetMethod get = new GetMethod(itemUrl + security.getIsin() + "_" + security.getMarket() + "_" + security.getCurrency());
-
-        String content;
-        try {
-            HttpClient standardClient = HttpClientUtil.getSwissquotePremiumClient(userId, password, true);
-            int status = standardClient.executeMethod(get);
-
-            if (status == HttpStatus.SC_NOT_FOUND) {
-                logger.warn("invalid security: " + security.getIsin());
-                return null;
-            }else if (status != HttpStatus.SC_OK) {
-                logger.error("could not retrieve security: " + security.getIsin() + ", status: " + get.getStatusLine());
-                return null;
-            }
-
-            // get the content
-            InputStream in = get.getResponseBodyAsStream();
-            StringBuffer out = new StringBuffer();
-            byte[] b = new byte[1024];
-            for (int n; (n = in.read(b)) != -1;) {
-                out.append(new String(b, 0, n));
-            }
-            in.close();
-            content = out.toString();
-
-        } finally {
-            get.releaseConnection();
-        }
-
-        // parse the Document using Tidy
-        Tidy tidy = TidyUtil.getInstance();
-        Document document = tidy.parseDOM(new ByteArrayInputStream(content.getBytes()), null);
-
-        // save the file
-        XmlUtil.saveDocumentToFile(document, security.getIsin() + ".xml", "results/swissquote/", false);
-
-        Tick tick = parseTick(document, security);
-
-        tick.setSecurity(security);
-        security.setLast(tick);
-
-        logger.debug(tick);
-
-        return tick;
-    }
-
-
-    protected Tick handleRetrieveTick(String isin) throws IOException, ParseException, TransformerException {
-
-        Security security = getSecurityDao().findByISIN(isin);
-        return handleRetrieveTick(security);
-    }
-
-
-    private void run() throws SuperCSVReflectionException, IOException, InterruptedException {
-
-        getCepService().runAll();
-
-        while(true) {
-
-            for (Iterator it = securities.iterator(); it.hasNext();) {
-                Security security = (Security)it.next();
-
-                Tick tick = retrieveTick(security);
-
-                getCepService().sendEvent(tick);
-
-                CsvWriter csvWriter = (CsvWriter)csvWriters.get(security);
-                csvWriter.writeTick(tick);
-            }
-
-            Thread.sleep(timeout);
-        }
-    }
-
-
-    private static Tick parseTick(Document document, Security security) throws ParseException, TransformerException {
+        Document document = getSecurityDocument(security);
 
         Tick tick = new TickImpl();
 
@@ -254,11 +187,168 @@ public class TickServiceImpl extends TickServiceBase {
             tick.setVol(volume);
             tick.setOpenIntrest(0);
             tick.setSettlement(BigDecimalUtil.getBigDecimal(0));
-
         }
+
+        tick.setSecurity(security);
+
+        logger.debug(tick);
 
         return tick;
     }
+
+
+    protected Tick handleRetrieveTick(String isin) throws IOException, ParseException, TransformerException {
+
+        Security security = getSecurityDao().findByISIN(isin);
+        return handleRetrieveTick(security);
+    }
+
+
+    protected StockOption handleRetrieveStockOption(Security underlaying, Date expiration, BigDecimal strike, OptionType type) throws ParseException, TransformerException, IOException {
+
+        Calendar cal = new GregorianCalendar();
+        cal.setTime(expiration);
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int exp = ((year - 2000) * 100 + month);
+
+        String url = optionUrl + "&underlying=" + underlaying.getIsin() +"&expiration=" + exp + "&strike=" + strike.longValue();
+
+        GetMethod get = new GetMethod(url);
+
+        String content;
+        try {
+            HttpClient standardClient = HttpClientUtil.getStandardClient(true);
+            int status = standardClient.executeMethod(get);
+
+            if (status == HttpStatus.SC_NOT_FOUND) {
+                logger.warn("invalid option request: underlying=" + underlaying.getIsin() +" expiration=" + exp + " strike=" + strike.longValue());
+                return null;
+            }else if (status != HttpStatus.SC_OK) {
+                logger.warn("invalid option request: underlying=" + underlaying.getIsin() +" expiration=" + exp + " strike=" + strike.longValue());
+                return null;
+            }
+
+            // get the content
+            InputStream in = get.getResponseBodyAsStream();
+            StringBuffer out = new StringBuffer();
+            byte[] b = new byte[1024];
+            for (int n; (n = in.read(b)) != -1;) {
+                out.append(new String(b, 0, n));
+            }
+            in.close();
+            content = out.toString();
+
+        } finally {
+            get.releaseConnection();
+        }
+
+        // parse the Document using Tidy
+        Tidy tidy = TidyUtil.getInstance();
+        Document listDocument = tidy.parseDOM(new ByteArrayInputStream(content.getBytes()), null);
+
+        // save the file
+        XmlUtil.saveDocumentToFile(listDocument, underlaying.getIsin() + "_" + exp + "_" + strike.longValue() + ".xml", "results/options/", false);
+
+        StockOption option = new StockOptionImpl();
+
+        String optionUrl = XPathAPI.selectSingleNode(listDocument, "//td[contains(a/@class,'list')][" + (OptionType.CALL.equals(type) ? 1 : 2) + "]/a/@href").getNodeValue();
+        String param = optionUrl.split("=")[1];
+        String isin = param.split("_")[0];
+        String market = param.split("_")[1];
+        String currency = param.split("_")[2];
+
+        option.setIsin(isin);
+        option.setMarket(Market.fromString(market));
+        option.setCurrency(Currency.fromString(currency));
+
+        option.setType(type);
+        option.setStrike(strike);
+
+        Document optionDocument = getSecurityDocument(option);
+
+        String dateValue = getValue(optionDocument, "//table[tr/td='Datum']/tr[10]/td[4]/strong");
+        Date expirationDate = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss").parse(dateValue + " 13:00:00");
+
+        String contractSizeValue = getValue(optionDocument, "//table[tr/td='Datum']/tr[10]/td[3]/strong");
+        int contractSize = (int)Double.parseDouble(contractSizeValue);
+
+        String symbolValue = XPathAPI.selectSingleNode(optionDocument, "//body/div[1]//h1/text()[2]").getNodeValue();
+        String symbol = symbolValue.split("\\(")[0].trim().substring(1);
+
+        option.setExpiration(expirationDate);
+        option.setSymbol(symbol);
+        option.setContractSize(contractSize);
+
+        option.setUnderlaying(underlaying);
+
+        return option;
+    }
+
+
+
+    private void run() throws SuperCSVReflectionException, IOException, InterruptedException {
+
+        getCepService().runAll();
+
+        while(true) {
+
+            for (Iterator it = securities.iterator(); it.hasNext();) {
+                Security security = (Security)it.next();
+
+                Tick tick = retrieveTick(security);
+
+                getCepService().sendEvent(tick);
+
+                CsvWriter csvWriter = (CsvWriter)csvWriters.get(security);
+                csvWriter.writeTick(tick);
+            }
+
+            Thread.sleep(timeout);
+        }
+    }
+
+
+    private Document getSecurityDocument(Security security) throws IOException, HttpException {
+
+        GetMethod get = new GetMethod(tickUrl + security.getIsin() + "_" + security.getMarket() + "_" + security.getCurrency());
+
+        String content;
+        try {
+            HttpClient standardClient = HttpClientUtil.getSwissquotePremiumClient(userId, password, true);
+            int status = standardClient.executeMethod(get);
+
+            if (status == HttpStatus.SC_NOT_FOUND) {
+                logger.warn("invalid security: " + security.getIsin());
+                return null;
+            }else if (status != HttpStatus.SC_OK) {
+                logger.error("could not retrieve security: " + security.getIsin() + ", status: " + get.getStatusLine());
+                return null;
+            }
+
+            // get the content
+            InputStream in = get.getResponseBodyAsStream();
+            StringBuffer out = new StringBuffer();
+            byte[] b = new byte[1024];
+            for (int n; (n = in.read(b)) != -1;) {
+                out.append(new String(b, 0, n));
+            }
+            in.close();
+            content = out.toString();
+
+        } finally {
+            get.releaseConnection();
+        }
+
+        // parse the Document using Tidy
+        Tidy tidy = TidyUtil.getInstance();
+        Document document = tidy.parseDOM(new ByteArrayInputStream(content.getBytes()), null);
+
+        // save the file
+        XmlUtil.saveDocumentToFile(document, security.getIsin() + ".xml", "results/swissquote/", false);
+        return document;
+    }
+
 
     private static String getValue(Node document, String expression) throws TransformerException {
 
