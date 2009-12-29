5ada170763ff7ec2f813468d17d53cde45f9c0d9
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2009-12-29 15:07:34 +0000

    implemented methods
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@2487 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/service/EntityServiceImpl.java
+++ src/com/algoTrader/service/EntityServiceImpl.java
@@ -1,7 +1,5 @@
 package com.algoTrader.service;
 
-import org.apache.commons.lang.ArrayUtils;
-
 import com.algoTrader.entity.Account;
 import com.algoTrader.entity.Position;
 import com.algoTrader.entity.Rule;
@@ -16,58 +14,48 @@ public class EntityServiceImpl extends com.algoTrader.service.EntityServiceBase 
     }
 
     protected com.algoTrader.entity.Account handleGetAccount(int id) throws java.lang.Exception {
-        // @todo implement protected com.algoTrader.entity.Account
-        // handleGetAccount(int id)
-        return null;
+
+        return getAccountDao().load(id);
     }
 
     protected com.algoTrader.entity.Position handleGetPosition(int id) throws java.lang.Exception {
-        // @todo implement protected com.algoTrader.entity.Position
-        // handleGetPosition(int id)
-        return null;
+
+        return getPositionDao().load(id);
     }
 
     protected com.algoTrader.entity.Rule handleGetRule(int id) throws java.lang.Exception {
-        // @todo implement protected com.algoTrader.entity.Rule
-        // handleGetRule(int id)
-        return null;
+
+        return getRuleDao().load(id);
     }
 
     protected com.algoTrader.entity.Transaction handleGetTransaction(int id) throws java.lang.Exception {
-        // @todo implement protected com.algoTrader.entity.Transaction
-        // handleGetTransaction(int id)
-        return null;
+
+        return getTransactionDao().load(id);
     }
 
-    @Override
     protected Security[] handleGetAllSecurities() throws Exception {
-
 
         return (Security[])getSecurityDao().loadAll().toArray(new Security[0]);
     }
 
-    @Override
-    protected Account handleGetAllAccounts() throws Exception {
-        // TODO Auto-generated method stub
-        return null;
+    protected Account[] handleGetAllAccounts() throws Exception {
+
+        return (Account[])getAccountDao().loadAll().toArray(new Account[0]);
     }
 
-    @Override
-    protected Position handleGetAllPositions() throws Exception {
-        // TODO Auto-generated method stub
-        return null;
+    protected Position[] handleGetAllPositions() throws Exception {
+
+        return (Position[])getPositionDao().loadAll().toArray(new Position[0]);
     }
 
-    @Override
-    protected Rule handleGetAllRules() throws Exception {
-        // TODO Auto-generated method stub
-        return null;
+    protected Rule[] handleGetAllRules() throws Exception {
+
+        return (Rule[])getRuleDao().loadAll().toArray(new Rule[0]);
+
     }
 
-    @Override
-    protected Transaction handleGetAllTransactions() throws Exception {
-        // TODO Auto-generated method stub
-        return null;
-    }
+    protected Transaction[] handleGetAllTransactions() throws Exception {
 
+        return (Transaction[])getTransactionDao().loadAll().toArray(new Transaction[0]);
+    }
 }
