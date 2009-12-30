0eaa8f636f57d9f4d644983c4e36f86effe2847c
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2009-12-30 21:16:13 +0000

    externalised EsperService Corrected External Timing
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@2494 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/service/CepServiceImpl.java
+++ src/com/algoTrader/service/CepServiceImpl.java
@@ -10,17 +10,16 @@ import java.util.Map;
 
 import com.algoTrader.entity.Rule;
 import com.algoTrader.entity.Security;
-import com.algoTrader.entity.TickImpl;
 import com.algoTrader.util.CustomDate;
-import com.algoTrader.util.DateUtil;
-import com.algoTrader.util.EntityUtil;
-import com.algoTrader.util.StockOptionUtil;
-import com.espertech.esper.client.Configuration;
+import com.algoTrader.util.EsperService;
 import com.espertech.esper.client.EPAdministrator;
 import com.espertech.esper.client.EPRuntime;
 import com.espertech.esper.client.EPServiceProvider;
-import com.espertech.esper.client.EPServiceProviderManager;
 import com.espertech.esper.client.EPStatement;
+import com.espertech.esper.client.StatementAwareUpdateListener;
+import com.espertech.esper.client.UpdateListener;
+import com.espertech.esper.client.time.CurrentTimeEvent;
+import com.espertech.esper.client.time.TimerControlEvent;
 import com.espertech.esperio.AdapterCoordinator;
 import com.espertech.esperio.AdapterCoordinatorImpl;
 import com.espertech.esperio.AdapterInputSource;
@@ -29,9 +28,6 @@ import com.espertech.esperio.csv.CSVInputAdapterSpec;
 import com.espertech.esperio.csv.TickCSVInputAdapter;
 
 public class CepServiceImpl extends CepServiceBase {
-
-    private EPServiceProvider cep;
-    private EPRuntime cepRT;
 
     private static String[] propertyOrder = {
         "dateTime",
@@ -60,25 +56,7 @@ public class CepServiceImpl extends CepServiceBase {
         propertyTypes.put("settlement", BigDecimal.class);
     }
 
-    protected void init() {
-
-        Configuration cepConfig = new Configuration();
-
-        cepConfig.addEventType("Tick", TickImpl.class);
-
-        cepConfig.addImport(EntityUtil.class);
-        cepConfig.addImport(StockOptionUtil.class);
-        cepConfig.addImport(DateUtil.class);
-
-        cepConfig.addImport("com.algoTrader.entity.*");
-
-        cep = EPServiceProviderManager.getDefaultProvider(cepConfig);
-        cepRT = cep.getEPRuntime();
-    }
-
     protected void handleRunAll() throws java.lang.Exception {
-
-        if (cep == null) init();
 
         Collection col = getRuleDao().findActiveRules();
 
@@ -90,36 +68,61 @@ public class CepServiceImpl extends CepServiceBase {
 
     protected void handleRun(Rule rule) throws java.lang.Exception {
 
-        if (cep == null) init();
-
+        EPServiceProvider cep = EsperService.getEPServiceInstance();
         EPAdministrator cepAdm = cep.getEPAdministrator();
-        EPStatement cepStatement = cepAdm.createEPL(rule.getDefinition(), rule.getName());
+
+        EPStatement cepStatement;
+        if (rule.isPattern()) {
+            cepStatement = cepAdm.createPattern(rule.getDefinition(), rule.getName());
+        } else {
+            cepStatement = cepAdm.createEPL(rule.getDefinition(), rule.getName());
+        }
 
         if (rule.getSubscriber() != null) {
             Class cl = Class.forName("com.algoTrader.subscriber." + rule.getSubscriber());
             Object obj = cl.newInstance();
             cepStatement.setSubscriber(obj);
         }
+
+        if (rule.getListeners() != null) {
+            String[] listeners = rule.getListeners().split("\\s");
+            for (int i = 0; i < listeners.length; i++) {
+                Class cl = Class.forName("com.algoTrader.listener." + listeners[i]);
+                Object obj = cl.newInstance();
+                if (obj instanceof StatementAwareUpdateListener) {
+                    cepStatement.addListener((StatementAwareUpdateListener)obj);
+                } else {
+                    cepStatement.addListener((UpdateListener)obj);
+                }
+            }
+        }
     }
 
     protected void handleSendEvent(Object object) throws java.lang.Exception {
 
-        cepRT.sendEvent(object);
+        EPServiceProvider cep = EsperService.getEPServiceInstance();
+        cep.getEPRuntime().sendEvent(object);
     }
 
     protected void handleStop(String ruleName) throws Exception {
 
+        EPServiceProvider cep = EsperService.getEPServiceInstance();
         EPStatement statement = cep.getEPAdministrator().getStatement(ruleName);
         statement.destroy();
-
     }
 
     protected void handleStopAll() throws Exception {
 
+        EPServiceProvider cep = EsperService.getEPServiceInstance();
+        cep.destroy();
     }
 
+    protected void handleSimulate(long startTime, String isin) throws Exception {
 
-    protected void handleSimulate(String isin) throws Exception {
+        EPServiceProvider cep = EsperService.getEPServiceInstance();
+        EPRuntime cepRT = cep.getEPRuntime();
+        cepRT.sendEvent(new TimerControlEvent(TimerControlEvent.ClockType.CLOCK_EXTERNAL));
+        cepRT.sendEvent(new CurrentTimeEvent(startTime)); // must send time event before first schedule pattern
 
         runAll();
 
@@ -138,13 +141,19 @@ public class CepServiceImpl extends CepServiceBase {
     }
 
 
-    protected void handleSimulate(List list) throws Exception {
+    protected void handleSimulate(long startTime, List list) throws Exception {
+
+        EPServiceProvider cep = EsperService.getEPServiceInstance();
+        EPRuntime cepRT = cep.getEPRuntime();
+        cepRT.sendEvent(new TimerControlEvent(TimerControlEvent.ClockType.CLOCK_EXTERNAL));
+        cepRT.sendEvent(new CurrentTimeEvent(startTime)); // must send time event before first schedule pattern
 
         runAll();
 
         AdapterCoordinator coordinator = new AdapterCoordinatorImpl(cep, true, true);
 
         for (Iterator it = list.iterator(); it.hasNext(); ) {
+
             String isin = (String)it.next();
             Security security = getSecurityDao().findByISIN(isin);
 
