d46c2315fb5ac4d14ee17b5929471e6d5598da56
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-01-02 13:18:05 +0000

    added run()
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@2509 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/starter/SimulationStarter.java
+++ src/com/algoTrader/starter/SimulationStarter.java
@@ -1,22 +1,14 @@
 package com.algoTrader.starter;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang.ArrayUtils;
-
 import com.algoTrader.ServiceLocator;
 
 public class SimulationStarter {
 
     public static void main(String[] args) {
 
-        List securities = new ArrayList();
-        CollectionUtils.addAll(securities, ArrayUtils.subarray(args, 1, args.length -1));
-
         long startTime = Long.parseLong(args[0]);
 
-        ServiceLocator.instance().getCepService().simulate(startTime, securities);
+        ServiceLocator.instance().getRuleService().activateAll();
+        ServiceLocator.instance().getSimulationService().simulateWatchlist(startTime);
     }
 }
