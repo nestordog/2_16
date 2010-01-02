f96b4568117b87883d66dc5365b8d302030f38b2
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-01-02 14:36:44 +0000

    getLastTick
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@2516 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/entity/SecurityImpl.java
+++ src/com/algoTrader/entity/SecurityImpl.java
@@ -1,32 +1,34 @@
-// license-header java merge-point
-/**
- * This is only generated once! It will never be overwritten.
- * You can (and have to!) safely modify it by hand.
- */
 package com.algoTrader.entity;
+
+import java.util.Iterator;
 
 import org.apache.commons.lang.builder.ToStringBuilder;
 
 import com.algoTrader.util.CustomToStringStyle;
+import com.algoTrader.util.EsperService;
+import com.espertech.esper.client.EPStatement;
+import com.espertech.esper.client.EventBean;
 
-/**
- * @see com.algoTrader.entity.Security
- */
-public class SecurityImpl
-    extends com.algoTrader.entity.Security
-{
-    /**
-     * The serial version UID of this class. Needed for serialization.
-     */
+public class SecurityImpl extends com.algoTrader.entity.Security {
+
     private static final long serialVersionUID = -6631052475125813394L;
 
-    /**
-     * @see com.algoTrader.entity.Security#toString()
-     */
-    public java.lang.String toString()
-    {
+    public Tick getLastTick() {
+        EPStatement statement = EsperService.getEPServiceInstance().getEPAdministrator().getStatement("lastTick");
+
+        Iterator it = statement.iterator();
+        while (it.hasNext()) {
+            EventBean bean = (EventBean) it.next();
+            Integer securityId = (Integer) bean.get("securityId");
+            if (securityId.equals(getId())) {
+                return (Tick)bean.get("tick");
+            }
+        }
+        return null;
+    }
+
+    public java.lang.String toString() {
         return ToStringBuilder.reflectionToString(this, CustomToStringStyle.getInstance());
 
     }
-
 }
