fd31fc7037b86d1c09e5af1463a26163bec5521f
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-01-08 17:50:52 +0000

    rename use ActionService
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@2530 19eef36c-86f0-de11-afab-0004ac968479

---
+++ src/com/algoTrader/listener/ExpireStockOptionsListener.java
@@ -0,0 +1,16 @@
+package com.algoTrader.listener;
+
+import com.algoTrader.ServiceLocator;
+import com.espertech.esper.client.EPServiceProvider;
+import com.espertech.esper.client.EPStatement;
+import com.espertech.esper.client.EventBean;
+import com.espertech.esper.client.StatementAwareUpdateListener;
+
+public class ExpireStockOptionsListener implements StatementAwareUpdateListener {
+
+    public void update(EventBean[] newEvents, EventBean[] oldEvents, EPStatement statement,
+            EPServiceProvider epServiceProvider) {
+
+        ServiceLocator.instance().getActionService().expireStockOptions();
+    }
+}
