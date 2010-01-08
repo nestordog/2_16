5df089e05ad693de446233286452346d474f94ff
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-01-08 17:58:55 +0000

    use ActionService
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@2548 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/subscriber/ClosePositionSubscriber.java
+++ src/com/algoTrader/subscriber/ClosePositionSubscriber.java
@@ -1,28 +1,12 @@
 package com.algoTrader.subscriber;
 
-import java.math.BigDecimal;
-import java.util.Date;
-
-import org.apache.log4j.Logger;
-
 import com.algoTrader.ServiceLocator;
-import com.algoTrader.entity.Security;
+import com.algoTrader.entity.Position;
 
 public class ClosePositionSubscriber {
 
-    private static Logger logger = Logger.getLogger(ClosePositionSubscriber.class.getName());
+    public void update(Position position) {
 
-    public void update(Date date, Security security, BigDecimal current) {
-
-        int quantity = - security.getPosition().getQuantity();
-
-        ServiceLocator serviceLocator = ServiceLocator.instance();
-        serviceLocator.getTransactionService().executeTransaction(quantity, security, current);
-
-        serviceLocator.getRuleService().deactivate("closePosition");
-        serviceLocator.getRuleService().deactivate("setExitValue");
-
-        logger.info(date + " close position " + security.getSymbol() + " at " + current);
-
+        ServiceLocator.instance().getActionService().closePosition(position);
     }
 }
