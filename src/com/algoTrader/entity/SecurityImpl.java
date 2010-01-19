5db56ba0b6b8f6b1d2a03576d9877394b0854195
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-01-19 21:00:58 +0000

    removed getCurrentValue
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@2564 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/entity/SecurityImpl.java
+++ src/com/algoTrader/entity/SecurityImpl.java
@@ -1,13 +1,8 @@
 package com.algoTrader.entity;
 
-import java.math.BigDecimal;
 import java.util.Iterator;
 
-import org.apache.commons.lang.builder.ToStringBuilder;
-
-import com.algoTrader.util.CustomToStringStyle;
 import com.algoTrader.util.EsperService;
-import com.algoTrader.util.PropertiesUtil;
 import com.espertech.esper.client.EPStatement;
 import com.espertech.esper.client.EventBean;
 
@@ -15,12 +10,8 @@ public class SecurityImpl extends com.algoTrader.entity.Security {
 
     private static final long serialVersionUID = -6631052475125813394L;
 
-    private static int lastTransactionAge = Integer.parseInt(PropertiesUtil.getProperty("lastTransactionAge"));
-    private static int minVol = Integer.parseInt(PropertiesUtil.getProperty("minVol"));
-
-
     public Tick getLastTick() {
-        EPStatement statement = EsperService.getEPServiceInstance().getEPAdministrator().getStatement("lastTick");
+        EPStatement statement = EsperService.getEPServiceInstance().getEPAdministrator().getStatement("getLastTick");
 
         if (statement != null) {
             Iterator it = statement.iterator();
@@ -37,30 +28,5 @@ public class SecurityImpl extends com.algoTrader.entity.Security {
 
     public boolean hasOpenPositions() {
         return getPosition().getQuantity() > 0;
-    }
-
-    public BigDecimal getCurrentValue() {
-
-        long currenttime = EsperService.getEPServiceInstance().getEPRuntime().getCurrentTime();
-        Tick tick = getLastTick();
-
-        if (tick != null) {
-            if (currenttime - tick.getLastDateTime().getTime() > lastTransactionAge) {
-
-                if (tick.getVolAsk() > minVol && tick.getVolBid() > minVol) {
-                    return (tick.getAsk().add(tick.getBid()).divide(new BigDecimal(2)));
-                } else {
-                    return tick.getLast();
-                }
-            } else {
-                return tick.getLast();
-            }
-        } else {
-            return null;
-        }
-    }
-
-    public java.lang.String toString() {
-        return ToStringBuilder.reflectionToString(this, CustomToStringStyle.getInstance());
     }
 }
