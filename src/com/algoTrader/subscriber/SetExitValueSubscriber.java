2def751ad6311ef9faadb4ac0790d4f4f00f5c94
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-01-19 21:10:46 +0000

    renamed to ExitValue call ActionService
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@2581 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/subscriber/SetExitValueSubscriber.java
+++ src/com/algoTrader/subscriber/SetExitValueSubscriber.java
@@ -1,18 +1,13 @@
 package com.algoTrader.subscriber;
 
 import java.math.BigDecimal;
-import java.util.Date;
-
-import org.apache.log4j.Logger;
 
 import com.algoTrader.ServiceLocator;
-import com.algoTrader.entity.Position;
-import com.algoTrader.util.MyLogger;
 
 public class SetExitValueSubscriber {
 
-    public void update(Position position, BigDecimal exitValue) {
+    public void update(int positionId, BigDecimal exitValue) {
 
-        ServiceLocator.instance().getActionService().setExitValue(position, exitValue);
+        ServiceLocator.instance().getActionService().setExitValue(positionId, exitValue);
     }
 }
