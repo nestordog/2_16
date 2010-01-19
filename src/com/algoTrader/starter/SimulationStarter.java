89213d55cf18e45306dbfeec9dd71845611c7114
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-01-19 21:06:26 +0000

    no starttime
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@2573 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/starter/SimulationStarter.java
+++ src/com/algoTrader/starter/SimulationStarter.java
@@ -6,9 +6,7 @@ public class SimulationStarter {
 
     public static void main(String[] args) {
 
-        long startTime = Long.parseLong(args[0]);
-
         ServiceLocator.instance().getRuleService().activateAll();
-        ServiceLocator.instance().getSimulationService().simulateWatchlist(startTime);
+        ServiceLocator.instance().getSimulationService().simulateWatchlist();
     }
 }
