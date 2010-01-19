d3717924356cdb363096e26b111c6c6d654ad97f
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-01-19 21:11:36 +0000

    pass securityId, settlement, currentValue & underlaying
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@2583 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/subscriber/OpenPositionSubscriber.java
+++ src/com/algoTrader/subscriber/OpenPositionSubscriber.java
@@ -1,11 +1,13 @@
 package com.algoTrader.subscriber;
 
+import java.math.BigDecimal;
+
 import com.algoTrader.ServiceLocator;
 
 public class OpenPositionSubscriber {
 
-    public void update() {
+    public void update(int securityId, BigDecimal settlement, BigDecimal currentValue, BigDecimal underlaying) {
 
-        ServiceLocator.instance().getActionService().openPosition();
+        ServiceLocator.instance().getActionService().openPosition(securityId, settlement, currentValue, underlaying);
     }
 }
