5db9cd736ad636b75dcb34b6bd5a679a13749e3e
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-01-19 21:09:30 +0000

    getNextThirdFriday
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@2577 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/util/DateUtil.java
+++ src/com/algoTrader/util/DateUtil.java
@@ -1,6 +1,8 @@
 package com.algoTrader.util;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class DateUtil {
 
@@ -12,5 +14,25 @@ public class DateUtil {
     public static Date getCurrentEPTime() {
 
         return new Date(EsperService.getEPServiceInstance().getEPRuntime().getCurrentTime());
+    }
+
+    public static Date getNextThirdFriday(Date input) {
+
+        Calendar cal = new GregorianCalendar();
+        cal.setTime(input);
+        cal.set(Calendar.WEEK_OF_MONTH, 3);
+        cal.set(Calendar.DAY_OF_WEEK, Calendar.FRIDAY);
+        cal.set(Calendar.HOUR_OF_DAY, 13);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+
+        if (cal.getTimeInMillis() < input.getTime()) {
+            cal.add(Calendar.MONTH, 1);
+            cal.set(Calendar.WEEK_OF_MONTH, 3);
+            cal.set(Calendar.DAY_OF_WEEK, Calendar.FRIDAY);
+        }
+
+        return cal.getTime();
     }
 }
