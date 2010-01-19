78b4c9210056b4886ec64aea6dfec4061da01ac4
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-01-19 21:09:20 +0000

    added Transaction event type added variables enable priorities add external TimerControl
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@2576 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/util/EsperService.java
+++ src/com/algoTrader/util/EsperService.java
@@ -1,11 +1,18 @@
 package com.algoTrader.util;
 
 import com.algoTrader.entity.TickImpl;
+import com.algoTrader.entity.TransactionImpl;
 import com.espertech.esper.client.Configuration;
 import com.espertech.esper.client.EPServiceProvider;
 import com.espertech.esper.client.EPServiceProviderManager;
+import com.espertech.esper.client.ConfigurationEngineDefaults.Threading;
+import com.espertech.esper.client.time.CurrentTimeEvent;
+import com.espertech.esper.client.time.TimerControlEvent;
 
 public class EsperService {
+
+    private static boolean simulation = new Boolean(PropertiesUtil.getProperty("simulation")).booleanValue();
+    private static long startTime = Long.parseLong(PropertiesUtil.getProperty("simulation.startTime"));
 
     private static EPServiceProvider cep;
 
@@ -13,16 +20,38 @@ public class EsperService {
 
         if (cep == null) {
 
-            Configuration cepConfig = new Configuration();
+            Configuration config = new Configuration();
 
-            cepConfig.addEventType("Tick", TickImpl.class);
+            // event types
+            config.addEventType("Tick", TickImpl.class);
+            config.addEventType("Transaction", TransactionImpl.class);
 
-            cepConfig.addImport(EntityUtil.class);
-            cepConfig.addImport(StockOptionUtil.class);
-            cepConfig.addImport(DateUtil.class);
-            cepConfig.addImport("com.algoTrader.entity.*");
+            // User Defined Functions
+            config.addImport(LookupUtil.class);
+            config.addImport(StockOptionUtil.class);
+            config.addImport(DateUtil.class);
 
-            cep = EPServiceProviderManager.getDefaultProvider(cepConfig);
+            // Entities
+            config.addImport("com.algoTrader.entity.*");
+
+            // Variables
+            config.addVariable("var_simulation", boolean.class, new Boolean(PropertiesUtil.getProperty("simulation")).booleanValue());
+            config.addVariable("var_isin", String.class, PropertiesUtil.getProperty("simulation.isin"));
+
+            // Settings
+            //config.getEngineDefaults().getExpression().setUdfCache(false); // some Methods hit the DB!
+            config.getEngineDefaults().getExecution().setPrioritized(true); // execute according to the rule priority
+            //config.getEngineDefaults().getThreading().setThreadPoolInbound(true);
+            //config.getEngineDefaults().getThreading().setThreadPoolOutbound(true);
+            //config.getEngineDefaults().getThreading().setThreadPoolTimerExec(true);
+            //config.getEngineDefaults().getThreading().setThreadPoolRouteExec(true);
+
+            cep = EPServiceProviderManager.getDefaultProvider(config);
+
+            if (simulation) {
+                cep.getEPRuntime().sendEvent(new TimerControlEvent(TimerControlEvent.ClockType.CLOCK_EXTERNAL));
+                cep.getEPRuntime().sendEvent(new CurrentTimeEvent(startTime)); // must send time event before first schedule pattern
+            }
         }
         return cep;
     }
