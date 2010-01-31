2d421546a9e73ed56b6344cf31a012f839f0b2c5
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-01-31 15:44:58 +0000

    switch to esper configuration file
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@2627 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/util/EsperService.java
+++ src/com/algoTrader/util/EsperService.java
@@ -1,11 +1,8 @@
 package com.algoTrader.util;
 
-import com.algoTrader.entity.TickImpl;
-import com.algoTrader.entity.TransactionImpl;
 import com.espertech.esper.client.Configuration;
 import com.espertech.esper.client.EPServiceProvider;
 import com.espertech.esper.client.EPServiceProviderManager;
-import com.espertech.esper.client.ConfigurationEngineDefaults.Threading;
 import com.espertech.esper.client.time.CurrentTimeEvent;
 import com.espertech.esper.client.time.TimerControlEvent;
 
@@ -20,31 +17,7 @@ public class EsperService {
         if (cep == null) {
 
             Configuration config = new Configuration();
-
-            // event types
-            config.addEventType("Tick", TickImpl.class);
-            config.addEventType("Transaction", TransactionImpl.class);
-
-            // User Defined Functions
-            config.addImport(LookupUtil.class);
-            config.addImport(StockOptionUtil.class);
-            config.addImport(DateUtil.class);
-
-            // Entities
-            config.addImport("com.algoTrader.entity.*");
-
-            // Variables
-            config.addVariable("var_simulation", boolean.class, new Boolean(PropertiesUtil.getProperty("simulation")).booleanValue());
-            config.addVariable("var_isin", String.class, PropertiesUtil.getProperty("simulation.isin"));
-
-            // Settings
-            config.getEngineDefaults().getExpression().setUdfCache(false); // some Methods hit the DB!
-            config.getEngineDefaults().getExecution().setPrioritized(true); // execute according to the rule priority
-            //config.getEngineDefaults().getThreading().setThreadPoolInbound(true);
-            //config.getEngineDefaults().getThreading().setThreadPoolOutbound(true);
-            //config.getEngineDefaults().getThreading().setThreadPoolTimerExec(true);
-            //config.getEngineDefaults().getThreading().setThreadPoolRouteExec(true);
-            //config.getEngineDefaults().getLogging().setEnableExecutionDebug(true);
+            config.configure();
 
             cep = EPServiceProviderManager.getDefaultProvider(config);
 
