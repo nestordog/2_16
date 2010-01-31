0b3222e3b07453ad7d0436aadaeeb3c94fab6fb9
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-01-31 10:36:34 +0000

    getAvailableAmount calculate Balance depending on transactionType
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@2598 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/entity/AccountImpl.java
+++ src/com/algoTrader/entity/AccountImpl.java
@@ -3,6 +3,8 @@ package com.algoTrader.entity;
 import java.math.BigDecimal;
 import java.util.Iterator;
 
+import com.algoTrader.enumeration.TransactionType;
+
 public class AccountImpl extends com.algoTrader.entity.Account {
 
     private static final long serialVersionUID = -2271735085273721632L;
@@ -12,7 +14,24 @@ public class AccountImpl extends com.algoTrader.entity.Account {
         BigDecimal balance = new BigDecimal(0);
         for (Iterator it = getTransactions().iterator(); it.hasNext(); ) {
             Transaction transaction = (Transaction)it.next();
-            balance = balance.add(transaction.getPrice().multiply(new BigDecimal(transaction.getQuantity())));
+            if (transaction.getType().equals(TransactionType.BUY) ||
+                transaction.getType().equals(TransactionType.SELL) ||
+                transaction.getType().equals(TransactionType.EXPIRATION)) {
+
+                balance = balance.subtract(transaction.getPrice().multiply(new BigDecimal(transaction.getQuantity())));
+                balance = balance.subtract(transaction.getCommission());
+
+            } else if (transaction.getType().equals(TransactionType.CREDIT) ||
+                    transaction.getType().equals(TransactionType.DIVIDEND) ||
+                    transaction.getType().equals(TransactionType.INTREST)) {
+
+                balance = balance.add(transaction.getPrice());
+
+            } else if (transaction.getType().equals(TransactionType.DEBIT) ||
+                    transaction.getType().equals(TransactionType.FEES)) {
+
+                balance = balance.subtract(transaction.getPrice());
+            }
         }
         return balance;
     }
@@ -23,10 +42,18 @@ public class AccountImpl extends com.algoTrader.entity.Account {
         for (Iterator it = getPositions().iterator(); it.hasNext(); ) {
             Position position = (Position)it.next();
             if (position.getQuantity() != 0) {
+                if (position.getMargin() == null) {
+                    break;
+                }
                 margin = margin.add(position.getMargin());
             }
         }
         return margin;
+    }
+
+    public BigDecimal getAvailableAmount() {
+
+        return getBalance().subtract(getMargin());
     }
 
     public int getPositionCount() {
@@ -34,7 +61,7 @@ public class AccountImpl extends com.algoTrader.entity.Account {
         int count = 0;
         for (Iterator it = getPositions().iterator(); it.hasNext(); ) {
             Position position = (Position)it.next();
-            if (position.getQuantity() > 0) {
+            if (position.getQuantity() != 0) {
                 count++;
             }
         }
