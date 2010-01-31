02f63ace7bff004fddc9e8cd5bc087b4441bdf4a
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-01-31 16:48:24 +0000

    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@2630 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/util/XmlUtil.java
+++ src/com/algoTrader/util/XmlUtil.java
@@ -17,17 +17,6 @@ import org.w3c.dom.Document;
 
 public class XmlUtil {
 
-    private static String endpoint = PropertiesUtil.getProperty("ebay.endpoint");
-    private static String devID = PropertiesUtil.getProperty("ebay.devID");
-    private static String appID = PropertiesUtil.getProperty("ebay.appID");
-    private static String certID = PropertiesUtil.getProperty("ebay.certID");
-    private static String token = PropertiesUtil.getProperty("ebay.token");
-    private static String siteId = PropertiesUtil.getProperty("ebay.siteId");
-    private static String apiVersion = PropertiesUtil.getProperty("ebay.apiVersion");
-
-    public static final int REQUEST_MESSAGE = 0;
-    public static final int RESPONSE_MESSAGE = 1;
-
     private static boolean saveToFile = new Boolean(PropertiesUtil.getProperty("saveToFile")).booleanValue();
 
     private static Logger logger = MyLogger.getLogger(XmlUtil.class.getName());
