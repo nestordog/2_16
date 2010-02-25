e13dddbe3552a7968555424a3040f8479dc49862
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-02-25 18:04:02 +0000

    HttpClientUtil.initTradePassword
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@2677 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/starter/TickStarter.java
+++ src/com/algoTrader/starter/TickStarter.java
@@ -1,11 +1,15 @@
 package com.algoTrader.starter;
 
+import java.io.IOException;
+
 import com.algoTrader.ServiceLocator;
+import com.algoTrader.util.HttpClientUtil;
 
 public class TickStarter {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws IOException {
 
+        HttpClientUtil.initTradePassword();
         ServiceLocator.instance().getRuleService().activateAll();
     }
 }
