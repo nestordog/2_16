b99933cdd7b1151774a1185fdfd5be732b30038c
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-03-06 21:12:17 +0000

    complete redo (spread ticks between 09:00 and 17:00)
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@2719 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/util/CsvInterpolator.java
+++ src/com/algoTrader/util/CsvInterpolator.java
@@ -1,6 +1,7 @@
 package com.algoTrader.util;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.Date;
 
 import org.supercsv.exception.SuperCSVException;
@@ -10,7 +11,9 @@ import com.algoTrader.entity.TickImpl;
 
 public class CsvInterpolator {
 
-    private static int factor = 6;
+    private static int factor = 8;
+    private static long startHour = 9 * 60 * 60 * 1000;
+    private static long offsetHour = 8 * 60 * 60 * 1000 / factor;
 
     public static void main(String[] args) throws SuperCSVException, IOException {
 
@@ -22,35 +25,27 @@ public class CsvInterpolator {
         Tick newTick;
         while ((newTick = csvReader.readTick()) != null) {
 
-            csvWriter.writeTick(oldTick);
-            for (int i=1; i < factor; i++) {
+            for (int i=0; i < factor; i++) {
 
-                long dateTimeOffset = (newTick.getDateTime().getTime() - oldTick.getDateTime().getTime()) / (long)factor;
                 double lastOffset = (newTick.getLast().doubleValue() - oldTick.getLast().doubleValue()) / (double)factor;
-                long lastDateTimeOffset = (newTick.getLastDateTime().getTime() - oldTick.getLastDateTime().getTime()) / (long)factor;
-                int volOffset = (newTick.getVol() - oldTick.getVol()) / factor;
-                int volBidOffset = (newTick.getVolBid() - oldTick.getVolBid()) / factor;
-                int volAskOffset = (newTick.getVolAsk() - oldTick.getVolAsk()) / factor;
-                double bidOffset = (newTick.getBid().doubleValue() - oldTick.getBid().doubleValue()) / (double)factor;
-                double askOffset = (newTick.getAsk().doubleValue() - oldTick.getAsk().doubleValue()) / (double)factor;
-                int openIntrestOffset = (newTick.getOpenIntrest() - oldTick.getOpenIntrest()) / factor;
 
                 Tick tick = new TickImpl();
-                tick.setDateTime(new Date(oldTick.getDateTime().getTime() + i * dateTimeOffset));
+                tick.setDateTime(new Date(oldTick.getDateTime().getTime() + startHour + i * offsetHour));
                 tick.setLast(RoundUtil.getBigDecimal(oldTick.getLast().doubleValue() + i * lastOffset));
-                tick.setLastDateTime(new Date(oldTick.getLastDateTime().getTime() + i * lastDateTimeOffset));
-                tick.setVol(oldTick.getVol() + i * volOffset);
-                tick.setVolBid(oldTick.getVolBid() + i * volBidOffset);
-                tick.setVolAsk(oldTick.getVolAsk() + i * volAskOffset);
-                tick.setBid(RoundUtil.getBigDecimal(oldTick.getBid().doubleValue() + i * bidOffset));
-                tick.setAsk(RoundUtil.getBigDecimal(oldTick.getAsk().doubleValue() + i * askOffset));
-                tick.setOpenIntrest(oldTick.getOpenIntrest() + i * openIntrestOffset);
-                tick.setSettlement(RoundUtil.getBigDecimal(oldTick.getSettlement().doubleValue()));
+                tick.setLastDateTime(oldTick.getLastDateTime());
+
+                tick.setVol(0);
+                tick.setVolBid(0);
+                tick.setVolAsk(0);
+                tick.setBid(new BigDecimal(0));
+                tick.setAsk(new BigDecimal(0));
+                tick.setOpenIntrest(0);
+                tick.setSettlement(new BigDecimal(0));
+
                 csvWriter.writeTick(tick);
             }
             oldTick = newTick;
         }
-        csvWriter.writeTick(oldTick);
 
         csvWriter.close();
     }
