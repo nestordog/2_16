2ee74dbe387d3c576143672b1fcdad9430140e1a
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-03-16 12:15:40 +0000

    sort by id display positionId
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@2740 19eef36c-86f0-de11-afab-0004ac968479

--- sql/saldo.sql
+++ sql/saldo.sql
@@ -1,19 +1,15 @@
-/*
-    SQL Manager 2005 for MySQL
-    SQL Editor queries
- */
-
-/*  Page 1  */
-
 SELECT t1.date_time,
        t1.type,
        s1.symbol,
+       s1.position_fk,
        t1.quantity,
        t1.price,
        t1.commission,
-       (SELECT sum(-t2.quantity * t2.price - t2.commission) from transaction t2 where t2.date_time <= t1.date_time) as saldo
+       (SELECT sum(-t2.quantity * t2.price - t2.commission)
+        from transaction t2
+        where t2.id <= t1.id) as saldo
 FROM transaction t1
 left outer join security s1 on t1.security_fk = s1.id
-order by t1.date_time ASC;
+order by t1.id ASC;
 
 
