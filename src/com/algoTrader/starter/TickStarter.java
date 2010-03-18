9fba83adb4bee626a433f9ab9fd12fb557f6d2fb
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-03-18 17:17:26 +0000

    run recorded ticks through stochastics before starting
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@2768 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/starter/TickStarter.java
+++ src/com/algoTrader/starter/TickStarter.java
@@ -1,11 +1,31 @@
 package com.algoTrader.starter;
 
 import com.algoTrader.ServiceLocator;
+import com.algoTrader.enumeration.RuleName;
+import com.algoTrader.service.RuleService;
+import com.algoTrader.service.SimulationService;
 
 public class TickStarter {
 
     public static void main(String[] args) {
 
-        ServiceLocator.instance().getRuleService().activateAll();
+        start();
+    }
+
+    public static void start() {
+        RuleService ruleService = ServiceLocator.instance().getRuleService();
+        SimulationService simulationService = ServiceLocator.instance().getSimulationService();
+
+        // run all the recorded ticks through to initialize the oscilators
+        ruleService.activate(RuleName.CREATE_K_FAST);
+        ruleService.activate(RuleName.CREATE_K_SLOW);
+        ruleService.activate(RuleName.CREATE_D_SLOW);
+        //ruleService.activate(RuleName.PRINT_STOCHASTIC);
+
+        simulationService.run();
+
+        // switch to internalClock and activate the rest of the rules
+        ruleService.setInternalClock();
+        ruleService.activateAll();
     }
 }
