6715cb852c14e70e447b76d1975f29e0ddd219d6
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-03-30 13:09:08 +0000

    stop but don't restart
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@2807 19eef36c-86f0-de11-afab-0004ac968479

--- build.sh
+++ build.sh
@@ -2,9 +2,10 @@
 
 cd /usr/local/AlgoTrader
 
+/usr/local/etc/rc.d/algotrader.sh stop
+
 svn update
 /usr/local/bin/maven mda
 /usr/local/bin/ant compile
 
-/usr/local/etc/rc.d/algotrader.sh restart
 
