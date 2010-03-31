759d18a4e97356c484e39eebe48dcf38bdb1680b
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-03-31 21:00:11 +0000

    get last nearest 10% at the market
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@2818 19eef36c-86f0-de11-afab-0004ac968479

--- sql/market-size-front-month-at-the-money.sql
+++ sql/market-size-front-month-at-the-money.sql
@@ -1,21 +1,14 @@
-select * from (
-  select u.id,
-  u.symbol,
-  u.market,
-  ut.last as underlayingLast,
-  o.strike,
-  o.expiration,
-  ot.last * ot.`OPEN_INTREST` * o.`CONTRACT_SIZE` * r.value as marketSize
-  from stock_option as o
-  join security as s on o.id = s.id
-  join security as u on s.UNDERLAYING_FK = u.id
-  join tick as ot on ot.`SECURITY_FK` = o.`ID`
-  join tick as ut on ut.`SECURITY_FK` = u.`ID`
-  join rate as r on r.`CURRENCY` = s.CURRENCY
-  where o.strike <= ut.last
-  and o.expiration >= '2010-04-01'
-  order by u.symbol asc, o.strike desc, o.expiration asc, market desc
-) as list
-group by id
+select u.symbol,
+sum(ot.last * ot.`VOL` * o.`CONTRACT_SIZE` * r.value) as marketSize
+from stock_option as o
+join security as s on o.id = s.id
+join security as u on s.UNDERLAYING_FK = u.id
+join tick as ot on ot.`SECURITY_FK` = o.`ID`
+join tick as ut on ut.`SECURITY_FK` = u.`ID`
+join rate as r on r.`CURRENCY` = s.CURRENCY
+where o.strike <= ut.last
+and o.strike > ut.last * 0.9
+and o.expiration = '2010-04-01'
+group by u.symbol
 order by marketSize desc
 
