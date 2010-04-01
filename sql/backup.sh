507453d43f66ff8fc236326d3747dee4b6e8f8ed
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-04-01 10:47:55 +0000

    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@2846 19eef36c-86f0-de11-afab-0004ac968479

--- sql/backup.sh
+++ sql/backup.sh
@@ -1,3 +1,7 @@
+#!/bin/sh
+
 cd /usr/local/AlgoTrader/sql
+
 mysqldump -u @localhost algotrader > backup.sql
+
 smbclient -U aflury -c "cd aflury/Algotrader/backup;put backup.sql" //bubba/home franklin91
