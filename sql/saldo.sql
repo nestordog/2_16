7cd9cd0f8d42884862499475ed35a74e9565618a
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-04-12 13:57:08 +0000

    output strike and exp-date
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@2864 19eef36c-86f0-de11-afab-0004ac968479

--- sql/saldo.sql
+++ sql/saldo.sql
@@ -1,15 +1,21 @@
-SELECT t1.date_time,
-       t1.type,
-       s1.symbol,
-       s1.position_fk,
-       t1.quantity,
-       t1.price,
-       t1.commission,
-       (SELECT sum(-t2.quantity * t2.price - t2.commission)
-        from transaction t2
-        where t2.id <= t1.id) as saldo
-FROM transaction t1
-left outer join security s1 on t1.security_fk = s1.id
-order by t1.id ASC;
-
-
+  select
+    `t1`.`DATE_TIME` AS `date_time`,
+    `t1`.`TYPE` AS `type`,
+    `s1`.`SYMBOL` AS `symbol`,
+    `s1`.`POSITION_FK` AS `POSITION_FK`,
+    `o1`.`STRIKE` AS `STRIKE`,
+    `o1`.`EXPIRATION` AS `expiration`,
+    `t1`.`QUANTITY` AS `quantity`,
+    `t1`.`PRICE` AS `price`,
+    `t1`.`COMMISSION` AS `commission`,
+    (
+  select
+    sum(((-(`t2`.`QUANTITY`) * `t2`.`PRICE`) - `t2`.`COMMISSION`)) AS `sum(-t2.quantity * t2.price - t2.commission)`
+  from
+    `transaction` `t2`
+  where
+    (`t2`.`id` <= `t1`.`id`)) AS `saldo`
+  from
+    ((`transaction` `t1` left join `security` `s1` on((`t1`.`SECURITY_FK` = `s1`.`id`))) left join `stock_option` `o1` on((`o1`.`ID` = `s1`.`id`)))
+  order by
+    `t1`.`id`;
