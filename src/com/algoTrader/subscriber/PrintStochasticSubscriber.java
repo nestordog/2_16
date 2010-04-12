81559a52f8b17f04eeaca8163038390fdb9849cb
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-04-12 14:54:11 +0000

    fix imports
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@2882 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/subscriber/PrintStochasticSubscriber.java
+++ src/com/algoTrader/subscriber/PrintStochasticSubscriber.java
@@ -1,9 +1,6 @@
 package com.algoTrader.subscriber;
 
-import java.io.IOException;
-
 import org.apache.log4j.Logger;
-import org.supercsv.exception.SuperCSVException;
 
 import com.algoTrader.entity.Characteristic;
 import com.algoTrader.util.MyLogger;
@@ -12,7 +9,7 @@ public class PrintStochasticSubscriber {
 
     private static Logger logger = MyLogger.getLogger(PrintStochasticSubscriber.class.getName());
 
-    public void update(Characteristic kFast, Characteristic kSlow, Characteristic dSlow) throws SuperCSVException, IOException {
+    public void update(Characteristic kFast, Characteristic kSlow, Characteristic dSlow)  {
 
         logger.info(kFast.getSecurity().getSymbol() + " kFast=" + kFast.getValue() + " kSlow=" + kSlow.getValue() + " dSlow=" + dSlow.getValue());
     }
