3d12d8bfb2d82974939e9dc2a834a4ee03d72f66
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-04-12 14:02:44 +0000

    getCurrentValue (ignore lastTransactionAge)
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@2872 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/entity/TickImpl.java
+++ src/com/algoTrader/entity/TickImpl.java
@@ -2,31 +2,30 @@ package com.algoTrader.entity;
 
 import java.math.BigDecimal;
 
-import com.algoTrader.util.EsperService;
 import com.algoTrader.util.PropertiesUtil;
 import com.algoTrader.util.RoundUtil;
 
 public class TickImpl extends com.algoTrader.entity.Tick {
 
-    private static int lastTransactionAge = PropertiesUtil.getIntProperty("lastTransactionAge");
     private static int minVol = PropertiesUtil.getIntProperty("minVol");
     private static double spreadSlope = PropertiesUtil.getDoubleProperty("spreadSlope");
     private static double maxSpreadConstant = PropertiesUtil.getDoubleProperty("maxSpreadConstant");
+    private static boolean simulation = PropertiesUtil.getBooleanProperty("simulation");
 
     private static final long serialVersionUID = 7518020445322413106L;
 
     public BigDecimal getCurrentValue() {
 
-        if (getLastDateTime() == null ||
-                EsperService.getCurrentTime() - getLastDateTime().getTime() > lastTransactionAge) {
-            if (getVolAsk() > minVol && getVolBid() > minVol) {
-                return RoundUtil.getBigDecimal((getAsk().doubleValue() + getBid().doubleValue()) / 2d);
-            } else {
-                return getLast();
-            }
-        } else {
+        if (simulation) {
             return getLast();
+        } else {
+            return RoundUtil.getBigDecimal((getAsk().doubleValue() + getBid().doubleValue()) / 2.0);
         }
+    }
+
+    public double getCurrentValueDouble() {
+
+        return getCurrentValue().doubleValue();
     }
 
     public BigDecimal getSettlement() {
@@ -56,10 +55,5 @@ public class TickImpl extends com.algoTrader.entity.Tick {
         }
 
         return true;
-    }
-
-    public double getCurrentValueDouble() {
-
-        return getCurrentValue().doubleValue();
     }
 }
