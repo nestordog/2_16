41f53d72c570c324ec0998997d17e21e7a3be5f3
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-04-13 15:37:35 +0000

    method comment
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@2886 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/entity/TickImpl.java
+++ src/com/algoTrader/entity/TickImpl.java
@@ -14,6 +14,11 @@ public class TickImpl extends com.algoTrader.entity.Tick {
 
     private static final long serialVersionUID = 7518020445322413106L;
 
+    /**
+     * 1. in simulation only "last" is used
+     * 2. on indexes (smi & vsmi) there is no bid and ask (only last)
+     * Note: ticks that are not valid (i.e. low volume) are not fed into esper, so we don't need to check
+     */
     public BigDecimal getCurrentValue() {
 
         if (simulation) {
