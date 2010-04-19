ea4919cd3d8d85b7c654ed248cf781b301c53e66
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-04-19 15:30:46 +0000

    getLastEvent
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@2894 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/util/EsperService.java
+++ src/com/algoTrader/util/EsperService.java
@@ -1,10 +1,13 @@
 package com.algoTrader.util;
 
+import java.util.Iterator;
+
 import com.algoTrader.enumeration.RuleName;
 import com.espertech.esper.client.Configuration;
 import com.espertech.esper.client.EPServiceProvider;
 import com.espertech.esper.client.EPServiceProviderManager;
 import com.espertech.esper.client.EPStatement;
+import com.espertech.esper.client.EventBean;
 import com.espertech.esper.client.time.CurrentTimeEvent;
 
 public class EsperService {
@@ -44,5 +47,17 @@ public class EsperService {
     public static void sendEvent(Object obj) {
 
         getEPServiceInstance().getEPRuntime().sendEvent(obj);
+    }
+
+    public static Object getLastEvent(RuleName ruleName) {
+
+        EPStatement statement = getStatement(ruleName);
+        if (statement != null && statement.isStarted()) {
+            for (Iterator<EventBean> it = statement.iterator(); it.hasNext(); ) {
+                EventBean bean = it.next();
+                return bean.getUnderlying();
+            }
+        }
+        return null;
     }
 }
