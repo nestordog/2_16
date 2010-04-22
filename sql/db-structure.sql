b5ffb01652420fa74f8f1c71c917c57041c224f8
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-04-22 19:19:58 +0000

    strike and expiration in view saldo
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@2907 19eef36c-86f0-de11-afab-0004ac968479

--- sql/db-structure.sql
+++ sql/db-structure.sql
@@ -82,7 +82,9 @@ SET character_set_client = utf8;
   `date_time` datetime,
   `type` enum('BUY','SELL','DIVIDEND','DEBIT','CREDIT','FEES','INTREST','EXPIRATION'),
   `symbol` varchar(30),
-  `position_fk` int(11),
+  `POSITION_FK` int(11),
+  `STRIKE` decimal(9,2),
+  `expiration` datetime,
   `quantity` bigint(20),
   `price` decimal(9,2),
   `commission` decimal(17,2),
@@ -179,7 +181,7 @@ CREATE TABLE `transaction` (
 /*!50001 SET collation_connection      = latin1_swedish_ci */;
 /*!50001 CREATE ALGORITHM=UNDEFINED */
 /*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `saldo` AS select `t1`.`DATE_TIME` AS `date_time`,`t1`.`TYPE` AS `type`,`s1`.`SYMBOL` AS `symbol`,`s1`.`POSITION_FK` AS `position_fk`,`t1`.`QUANTITY` AS `quantity`,`t1`.`PRICE` AS `price`,`t1`.`COMMISSION` AS `commission`,(select sum(((-(`t2`.`QUANTITY`) * `t2`.`PRICE`) - `t2`.`COMMISSION`)) AS `sum(-t2.quantity * t2.price - t2.commission)` from `transaction` `t2` where (`t2`.`id` <= `t1`.`id`)) AS `saldo` from (`transaction` `t1` left join `security` `s1` on((`t1`.`SECURITY_FK` = `s1`.`id`))) order by `t1`.`id` */;
+/*!50001 VIEW `saldo` AS select `t1`.`DATE_TIME` AS `date_time`,`t1`.`TYPE` AS `type`,`s1`.`SYMBOL` AS `symbol`,`s1`.`POSITION_FK` AS `POSITION_FK`,`o1`.`STRIKE` AS `STRIKE`,`o1`.`EXPIRATION` AS `expiration`,`t1`.`QUANTITY` AS `quantity`,`t1`.`PRICE` AS `price`,`t1`.`COMMISSION` AS `commission`,(select sum(((-(`t2`.`QUANTITY`) * `t2`.`PRICE`) - `t2`.`COMMISSION`)) AS `sum(-t2.quantity * t2.price - t2.commission)` from `transaction` `t2` where (`t2`.`id` <= `t1`.`id`)) AS `saldo` from ((`transaction` `t1` left join `security` `s1` on((`t1`.`SECURITY_FK` = `s1`.`id`))) left join `stock_option` `o1` on((`o1`.`ID` = `s1`.`id`))) order by `t1`.`id` */;
 /*!50001 SET character_set_client      = @saved_cs_client */;
 /*!50001 SET character_set_results     = @saved_cs_results */;
 /*!50001 SET collation_connection      = @saved_col_connection */;
