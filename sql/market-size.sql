9c78fd83b46defa5cb56ed571c81db018464c8d0
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-07-01 11:29:07 +0000

    improve market size scripts
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@2964 19eef36c-86f0-de11-afab-0004ac968479

--- sql/market-size.sql
+++
@@ -1,9 +0,0 @@
-select u.symbol,
-sum(o.`CONTRACT_SIZE` * t.`LAST` * t.`VOL` *r.value) as market
-from stock_option as o
-join tick as t on t.`SECURITY_FK` = o.`ID`
-join security as s on o.id = s.id
-join security as u on s.UNDERLAYING_FK = u.id
-join rate as r on r.`CURRENCY` = s.CURRENCY
-group by u.id
-order by market desc
