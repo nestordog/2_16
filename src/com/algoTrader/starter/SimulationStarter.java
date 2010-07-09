965da0610a551e59f43e7a3960219252853c2c6f
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-07-09 14:24:31 +0000

    print: monthlyPerformances sharpRatio maxDrawDownM bestMonthlyPerformance maxDrawDown maxDrawDownPeriod colmarRatio
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@2987 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/starter/SimulationStarter.java
+++ src/com/algoTrader/starter/SimulationStarter.java
@@ -1,9 +1,14 @@
 package com.algoTrader.starter;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import com.algoTrader.ServiceLocator;
+import com.algoTrader.entity.MonthlyPerformance;
+import com.algoTrader.util.RoundUtil;
 import com.algoTrader.vo.InterpolationVO;
+import com.algoTrader.vo.MaxDrawDownVO;
+import com.algoTrader.vo.PerformanceKeysVO;
 
 public class SimulationStarter {
 
@@ -12,6 +17,7 @@ public class SimulationStarter {
         start();
     }
 
+    @SuppressWarnings("unchecked")
     public static void start() {
 
         ServiceLocator.instance().getSimulationService().init();
@@ -19,15 +25,45 @@ public class SimulationStarter {
         ServiceLocator.instance().getRuleService().activateAll();
         ServiceLocator.instance().getSimulationService().run();
 
+        BigDecimal totalValue = ServiceLocator.instance().getManagementService().getAccountTotalValue();
+        System.out.println("totalValue=" + totalValue);
+
         InterpolationVO interpolation = ServiceLocator.instance().getSimulationService().getInterpolation();
 
         if (interpolation != null) {
-            System.out.print("a=" + interpolation.getA());
-            System.out.print(" b=" + interpolation.getB());
-            System.out.print(" r=" + interpolation.getR());
+            System.out.print("a=" + RoundUtil.getBigDecimal(interpolation.getA()));
+            System.out.print(" b=" + RoundUtil.getBigDecimal(interpolation.getB()));
+            System.out.println(" r=" + RoundUtil.getBigDecimal(interpolation.getR()));
         }
 
-        BigDecimal totalValue = ServiceLocator.instance().getManagementService().getAccountTotalValue();
-        System.out.println(" totalValue=" + totalValue);
+        List<MonthlyPerformance> monthlyPerformances = ServiceLocator.instance().getSimulationService().getMonthlyPerformances();
+
+        System.out.print("monthlyPerformance: ");
+        double maxDrawDownM = 0d;
+        double bestMonthlyPerformance = 0d;
+        for (MonthlyPerformance monthlyPerformance : monthlyPerformances) {
+            maxDrawDownM = Math.min(maxDrawDownM, monthlyPerformance.getValue());
+            bestMonthlyPerformance = Math.max(bestMonthlyPerformance, monthlyPerformance.getValue());
+            System.out.print(RoundUtil.getBigDecimal(monthlyPerformance.getValue() * 100) + "% " );
+        }
+        System.out.println();
+
+        PerformanceKeysVO performanceKeys = ServiceLocator.instance().getSimulationService().getPerformanceKeys();
+        MaxDrawDownVO maxDrawDownVO = ServiceLocator.instance().getSimulationService().getMaxDrawDown();
+
+        if (performanceKeys != null && maxDrawDownVO != null) {
+            System.out.print("n=" + RoundUtil.getBigDecimal(performanceKeys.getN()));
+            System.out.print(" avgM=" + RoundUtil.getBigDecimal(performanceKeys.getAvgM() * 100) + "%");
+            System.out.print(" stdM=" + RoundUtil.getBigDecimal(performanceKeys.getStdM() * 100) + "%");
+            System.out.print(" avgY=" + RoundUtil.getBigDecimal(performanceKeys.getAvgY() * 100) + "%");
+            System.out.print(" stdY=" + RoundUtil.getBigDecimal(performanceKeys.getStdY() * 100) + "%");
+            System.out.println(" sharpRatio=" + RoundUtil.getBigDecimal(performanceKeys.getSharpRatio()));
+
+            System.out.print("maxDrawDownM: " + RoundUtil.getBigDecimal(-maxDrawDownM * 100) + "%");
+            System.out.print(" bestMonthlyPerformance: " + RoundUtil.getBigDecimal(bestMonthlyPerformance * 100) + "%");
+            System.out.print(" maxDrawDown: " + RoundUtil.getBigDecimal(maxDrawDownVO.getAmount() * 100) + "%");
+            System.out.print(" maxDrawDownPeriod: " + RoundUtil.getBigDecimal(maxDrawDownVO.getPeriod() / 86400000) + "days");
+            System.out.println(" colmarRatio: " + RoundUtil.getBigDecimal(performanceKeys.getAvgY() / maxDrawDownVO.getAmount()));
+        }
     }
 }
