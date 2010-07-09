b129c4b650ff01fedc5d899f7975493458ebe39b
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-07-09 14:11:34 +0000

    changed path
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@2978 19eef36c-86f0-de11-afab-0004ac968479

--- sql/dump-db.bat
+++ sql/dump-db.bat
@@ -1,3 +1,3 @@
-"c:\Program Files\MySQL\MySQL Server 5.1\bin\mysqldump.exe" -X --hex-blob -t -u @localhost -r D:\AlgoTrader\sql\rules.xml algotrader rule
-"c:\Program Files\MySQL\MySQL Server 5.1\bin\mysqldump.exe" -t -u @localhost -r D:\AlgoTrader\sql\rules.sql algotrader rule
-"c:\Program Files\MySQL\MySQL Server 5.1\bin\mysqldump.exe" -d -u @localhost -r D:\AlgoTrader\sql\db-structure.sql algotrader
+"D:\MySQL Server 5.1\bin\mysqldump.exe" -X --hex-blob -t -u @localhost -r D:\AlgoTrader\sql\rules.xml algotrader rule
+"D:\MySQL Server 5.1\bin\mysqldump.exe" -t -u @localhost -r D:\AlgoTrader\sql\rules.sql algotrader rule
+"D:\MySQL Server 5.1\bin\mysqldump.exe" -d -u @localhost -r D:\AlgoTrader\sql\db-structure.sql algotrader
