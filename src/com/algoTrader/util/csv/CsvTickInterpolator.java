243112187094818eb044447bf0a180a07a9d5027
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-07-22 20:46:38 +0000

    use same terminologies as HLOCInterpolator
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3002 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/util/csv/CsvTickInterpolator.java
+++ src/com/algoTrader/util/csv/CsvTickInterpolator.java
@@ -1,27 +1,29 @@
 package com.algoTrader.util.csv;
 
+import java.io.File;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.supercsv.exception.SuperCSVException;
 
 import com.algoTrader.entity.Tick;
 import com.algoTrader.entity.TickImpl;
+import com.algoTrader.util.PropertiesUtil;
 import com.algoTrader.util.RoundUtil;
 
 public class CsvTickInterpolator {
 
-    private static int factor = 8;
+    private static String dataSet = PropertiesUtil.getProperty("simulation.dataSet");
 
-    private static long startHour = 8;
-    private static long offsetHour = 1;
-
-    private static SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yy");
-    private static SimpleDateFormat dateTimeFormat = new SimpleDateFormat("dd-MM-yy_k:mm:ss");
+    private static double recordsPerDay = 17.0;
+     private static double recordsPerHour = 2.0;
+     private static double startHour = 9.0;
 
     public static void main(String[] args) throws SuperCSVException, IOException, ParseException {
+
+        (new File("results/tickdata/" + dataSet + "/" + args[1] + ".csv")).delete();
 
         CsvTickReader csvReader = new CsvTickReader(args[0]);
         CsvTickWriter csvWriter = new CsvTickWriter(args[1]);
@@ -31,19 +33,15 @@ public class CsvTickInterpolator {
         Tick newTick;
         while ((newTick = csvReader.readTick()) != null) {
 
-            for (int i=0; i <= factor; i++) {
+            for (int currentHour=0; currentHour < recordsPerDay; currentHour++) {
 
-                double lastOffset = (newTick.getLast().doubleValue() - oldTick.getLast().doubleValue()) / (double)factor;
-                long hour = startHour + i * offsetHour;
-                String dateTime = dateFormat.format(newTick.getDateTime()) + "_" +  + hour + ":01:00";
+                double lastOffset = (newTick.getLast().doubleValue() - oldTick.getLast().doubleValue()) / (recordsPerDay - 1.0);
 
                 Tick tick = new TickImpl();
 
-                tick.setDateTime(dateTimeFormat.parse(dateTime));
-                tick.setLast(RoundUtil.getBigDecimal(oldTick.getLast().doubleValue() + i * lastOffset));
-
+                tick.setDateTime(new Date(newTick.getDateTime().getTime() + (int)((currentHour / recordsPerHour + startHour) * 60 * 60 * 1000)));
+                tick.setLast(RoundUtil.getBigDecimal(oldTick.getLast().doubleValue() + currentHour * lastOffset));
                 tick.setLastDateTime(tick.getDateTime());
-
                 tick.setVol(0);
                 tick.setVolBid(0);
                 tick.setVolAsk(0);
