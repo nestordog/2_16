2be876190d74fbd3a4481de79d59057dae0b52f1
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-07-22 20:46:06 +0000

    complete redo. random time for high and low
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3001 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/util/csv/CsvHlocInterpolator.java
+++ src/com/algoTrader/util/csv/CsvHlocInterpolator.java
@@ -1,23 +1,30 @@
 package com.algoTrader.util.csv;
 
+import java.io.File;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.NavigableMap;
+import java.util.TreeMap;
 
 import org.supercsv.exception.SuperCSVException;
 
 import com.algoTrader.entity.Tick;
 import com.algoTrader.entity.TickImpl;
+import com.algoTrader.util.PropertiesUtil;
 import com.algoTrader.util.RoundUtil;
 import com.algoTrader.vo.HlocVO;
 
 public class CsvHlocInterpolator {
 
-    private static int factor = 8;
-    private static long startHour = 9 * 60 * 60 * 1000;
-    private static long offsetHour = 8 * 60 * 60 * 1000 / factor;
+    private static String dataSet = PropertiesUtil.getProperty("simulation.dataSet");
+    private static double recordsPerDay = 17.0;
+     private static double recordsPerHour = 2.0;
+     private static double startHour = 9.0;
 
     public static void main(String[] args) throws SuperCSVException, IOException {
+
+        (new File("results/tickdata/" + dataSet + "/" + args[1] + ".csv")).delete();
 
         CsvHlocReader csvReader = new CsvHlocReader(args[0]);
         CsvTickWriter csvWriter = new CsvTickWriter(args[1]);
@@ -25,43 +32,50 @@ public class CsvHlocInterpolator {
         HlocVO hloc;
         while ((hloc = csvReader.readHloc()) != null) {
 
-            for (int i=0; i <= factor; i++) {
+            double open = hloc.getOpen().doubleValue();
+            double close = hloc.getClose().doubleValue();
+            double high = hloc.getHigh().doubleValue();
+            double low = hloc.getLow().doubleValue();
 
-                double high = hloc.getHigh().doubleValue();
-                double low = hloc.getLow().doubleValue();
-                double open = hloc.getOpen().doubleValue();
-                double close = hloc.getClose().doubleValue();
+            NavigableMap<Integer, Double> map = new TreeMap<Integer, Double>();
 
-                double last = 0;;
-                if (open > close) {
-                    switch(i) {
-                        case (0) : last = open; break;
-                        case (1) : last = (open + high) / 2.0; break;
-                        case (2) : last = high; break;
-                        case (3) : last = (high * 3 + low) / 4.0; break;
-                        case (4) : last =  (high + low) / 2.0; break;
-                        case (5) : last = (high + low * 3.0) / 4.0; break;
-                        case (6) : last = low; break;
-                        case (7) : last = (low + close) / 2.0; break;
-                        case (8) : last = close; break;
-                    }
+            int lowHour = (int)(Math.random() * (recordsPerDay - 2.0));
+            int highHour = (int)(Math.random() * (recordsPerDay - 3.0));
+
+            if (highHour >= lowHour) highHour++;
+
+            if ((open < close) && (lowHour > highHour) ||
+                (open > close) && (lowHour < highHour)) {
+                int tempHour = lowHour;
+                lowHour = highHour;
+                highHour = tempHour;
+            }
+
+            map.put(0, open);
+            map.put((int)(recordsPerDay), close);
+            map.put(highHour, high);
+            map.put(lowHour, low);
+
+            for (int currentHour = 0; currentHour < recordsPerDay; currentHour++) {
+
+                int prevHour = map.floorKey(currentHour);
+                double prevValue = map.get(prevHour);
+
+                double value = 0.0;
+                if (currentHour == prevHour) {
+                    value = prevValue;
                 } else {
-                    switch(i) {
-                    case (0) : last = open; break;
-                    case (1) : last = (open + low) / 2.0; break;
-                    case (2) : last = low; break;
-                    case (3) : last = (low * 3 + high) / 4.0; break;
-                    case (4) : last =  (low + high) / 2.0; break;
-                    case (5) : last = (low + high * 3.0) / 4.0; break;
-                    case (6) : last = high; break;
-                    case (7) : last = (high + close) / 2.0; break;
-                    case (8) : last = close; break;
-                    }
+
+                    int nextHour = map.ceilingKey(currentHour);
+                    double nextValue = map.get(nextHour);
+
+                    double factor = (double)(currentHour - prevHour) / (double)(nextHour - prevHour);
+                    value = (nextValue - prevValue) * factor + prevValue;
                 }
 
                 Tick tick = new TickImpl();
-                tick.setDateTime(new Date(hloc.getDateTime().getTime() + startHour + i * offsetHour));
-                tick.setLast(RoundUtil.getBigDecimal(last));
+                tick.setDateTime(new Date(hloc.getDateTime().getTime() + (int)((currentHour / recordsPerHour + startHour) * 60 * 60 * 1000)));
+                tick.setLast(RoundUtil.getBigDecimal(value));
                 tick.setLastDateTime(tick.getDateTime());
 
                 tick.setVol(0);
