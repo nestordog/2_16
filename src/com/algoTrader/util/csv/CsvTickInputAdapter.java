b99bde2eee84dc6956e73405eac0ef18c841a990
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-07-23 10:33:31 +0000

    rename
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3012 19eef36c-86f0-de11-afab-0004ac968479

---
+++ src/com/algoTrader/util/csv/CsvTickInputAdapter.java
@@ -0,0 +1,34 @@
+package com.algoTrader.util.csv;
+
+import com.algoTrader.ServiceLocator;
+import com.algoTrader.entity.Security;
+import com.algoTrader.entity.Tick;
+import com.espertech.esper.client.EPException;
+import com.espertech.esper.client.EPServiceProvider;
+import com.espertech.esperio.SendableBeanEvent;
+import com.espertech.esperio.SendableEvent;
+import com.espertech.esperio.csv.CSVInputAdapter;
+import com.espertech.esperio.csv.CSVInputAdapterSpec;
+
+public class CsvTickInputAdapter extends CSVInputAdapter {
+
+    private int securityId;
+
+    public CsvTickInputAdapter(EPServiceProvider epService, CSVInputAdapterSpec spec, int id) {
+
+        super(epService, spec);
+        securityId = id;
+    }
+
+    public SendableEvent read() throws EPException {
+        SendableBeanEvent event = (SendableBeanEvent)super.read();
+
+        if (event != null) {
+            Tick tick = (Tick)event.getBeanToSend();
+            Security security = ServiceLocator.instance().getLookupService().getSecurity(securityId);
+            tick.setSecurity(security);
+        }
+
+        return event;
+    }
+}
