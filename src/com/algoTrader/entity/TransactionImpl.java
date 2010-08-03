afbb280d861e94f6222655c6084a743bfc9a5562
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-08-03 15:09:48 +0000

    cache getValueDouble because getValue get's called very often
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3049 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/entity/TransactionImpl.java
+++ src/com/algoTrader/entity/TransactionImpl.java
@@ -9,6 +9,8 @@ public class TransactionImpl extends Transaction {
 
     private static final long serialVersionUID = -1528408715199422753L;
 
+    private Double value = null; // cache getValueDouble because getValue get's called very often
+
     public BigDecimal getValue() {
 
         return RoundUtil.getBigDecimal(getValueDouble());
@@ -16,24 +18,21 @@ public class TransactionImpl extends Transaction {
 
     public double getValueDouble() {
 
-        if (getType().equals(TransactionType.BUY) ||
-            getType().equals(TransactionType.SELL) ||
-            getType().equals(TransactionType.EXPIRATION)) {
-
-            return -getPrice().doubleValue() * (double)getQuantity() - getCommission().doubleValue();
-
-        } else if (getType().equals(TransactionType.CREDIT) ||
-            getType().equals(TransactionType.DIVIDEND) ||
-            getType().equals(TransactionType.INTREST)) {
-
-            return getPrice().doubleValue();
-
-        } else if (getType().equals(TransactionType.DEBIT) ||
-            getType().equals(TransactionType.FEES)) {
-
-            return -getPrice().doubleValue();
-        } else {
-            throw new IllegalArgumentException("unsupported transactionType: " + getType());
+        if (value == null) {
+            if (getType().equals(TransactionType.BUY) ||
+                    getType().equals(TransactionType.SELL) ||
+                    getType().equals(TransactionType.EXPIRATION)) {
+                value = -getPrice().doubleValue() * (double) getQuantity() - getCommission().doubleValue();
+            } else if (getType().equals(TransactionType.CREDIT) ||
+                    getType().equals(TransactionType.INTREST)) {
+                value = getPrice().doubleValue();
+            } else if (getType().equals(TransactionType.DEBIT) ||
+                    getType().equals(TransactionType.FEES)) {
+                value = -getPrice().doubleValue();
+            } else {
+                throw new IllegalArgumentException("unsupported transactionType: " + getType());
+            }
         }
+        return value;
     }
 }
