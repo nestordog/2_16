50d68b16a5ac15bd9c28b539bf3c2be71e3e69d3
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-08-03 15:05:27 +0000

    cache stockOptionsOnWatchlist, because it gets called very often
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3048 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/entity/StockOptionDaoImpl.java
+++ src/com/algoTrader/entity/StockOptionDaoImpl.java
@@ -1,5 +1,18 @@
 package com.algoTrader.entity;
 
+import java.util.List;
+
 public class StockOptionDaoImpl extends StockOptionDaoBase {
 
+    private List<StockOption> stockOptionsOnWatchlist; // cache stockOptionsOnWatchlist because it gets called very often
+
+    @SuppressWarnings("unchecked")
+    public List<StockOption> findStockOptionsOnWatchlist() {
+
+        if (stockOptionsOnWatchlist == null) {
+            stockOptionsOnWatchlist = super.findStockOptionsOnWatchlist();
+        }
+
+        return stockOptionsOnWatchlist;
+    }
 }
