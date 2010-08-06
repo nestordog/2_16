d082befa396b1f95c539d9d58c7469c1e3b3655a
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-08-06 08:46:47 +0000

    getStockOptionsOnWatchlist(forceReload)
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3068 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/entity/StockOptionDaoImpl.java
+++ src/com/algoTrader/entity/StockOptionDaoImpl.java
@@ -7,9 +7,9 @@ public class StockOptionDaoImpl extends StockOptionDaoBase {
     private List<StockOption> stockOptionsOnWatchlist; // cache stockOptionsOnWatchlist because it gets called very often
 
     @SuppressWarnings("unchecked")
-    public List<StockOption> findStockOptionsOnWatchlist() {
+    public List<StockOption> handleGetStockOptionsOnWatchlist(boolean forceReload) {
 
-        if (stockOptionsOnWatchlist == null) {
+        if (stockOptionsOnWatchlist == null || forceReload) {
             stockOptionsOnWatchlist = super.findStockOptionsOnWatchlist();
         }
 
