9af36bf92babc00ad0d696cd9b7c899fa7821315
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-08-11 08:01:22 +0000

    output call and put
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3085 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/subscriber/PrintStochasticSubscriber.java
+++ src/com/algoTrader/subscriber/PrintStochasticSubscriber.java
@@ -2,15 +2,19 @@ package com.algoTrader.subscriber;
 
 import org.apache.log4j.Logger;
 
-import com.algoTrader.entity.Characteristic;
+import com.algoTrader.entity.DSlow;
+import com.algoTrader.entity.KFast;
+import com.algoTrader.entity.KSlow;
 import com.algoTrader.util.MyLogger;
 
 public class PrintStochasticSubscriber {
 
     private static Logger logger = MyLogger.getLogger(PrintStochasticSubscriber.class.getName());
 
-    public void update(Characteristic kFast, Characteristic kSlow, Characteristic dSlow)  {
+    public void update(KFast kFast, KSlow kSlow, DSlow dSlow)  {
 
-        logger.debug(kFast.getSecurity().getSymbol() + "," + kFast.getValue() + "," + kSlow.getValue() + "," + dSlow.getValue());
+        logger.debug(kFast.getSecurity().getSymbol() + "," +
+                kFast.getCall() + "," + kSlow.getCall() + "," + dSlow.getCall() + "," +
+                kFast.getPut() + "," + kSlow.getPut() + "," + dSlow.getPut());
     }
 }
