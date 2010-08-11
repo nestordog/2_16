d1c6d4aaea45a53a9da8f7a9fca72e93c77f23db
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-08-11 08:27:59 +0000

    handle diffrent optimization strategies
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3092 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/starter/SimulationStarter.java
+++ src/com/algoTrader/starter/SimulationStarter.java
@@ -5,20 +5,58 @@ import org.apache.commons.math.FunctionEvaluationException;
 import org.apache.log4j.Logger;
 
 import com.algoTrader.ServiceLocator;
+import com.algoTrader.service.SimulationServiceImpl;
 import com.algoTrader.util.MyLogger;
 
 public class SimulationStarter {
 
-    public static Logger logger = MyLogger.getLogger(SimulationStarter.class.getName());
+    public static Logger logger = MyLogger.getLogger(SimulationServiceImpl.class.getName());
 
     public static void main(String[] args) throws ConvergenceException, FunctionEvaluationException {
 
         if (args[0].equals("simulateByUnderlayings")) {
+
             ServiceLocator.instance().getSimulationService().simulateByUnderlayings();
         } else if (args[0].equals("simulateByActualOrders")) {
+
             ServiceLocator.instance().getSimulationService().simulateByActualTransactions();
-        } else if (args[0].equals("optimize")) {
-            ServiceLocator.instance().getSimulationService().optimize(args[1], Double.valueOf(args[2]), Double.valueOf(args[3]), Double.valueOf(args[4]));
+        } else if (args[0].equals("optimizeSingle")) {
+
+            String[] parameters = new String[args.length -1];
+            double[] mins = new double[args.length -1];
+            double[] maxs = new double[args.length -1];
+            double[] accuracies = new double[args.length -1];
+            for (int i = 1; i < args.length; i++) {
+                String[] params = args[i].split(":");
+                parameters[i-1] = params[0];
+                mins[i-1] = Double.valueOf(params[1]);
+                maxs[i-1] = Double.valueOf(params[2]);
+                accuracies[i-1] = Double.valueOf(params[3]);
+            }
+
+            ServiceLocator.instance().getSimulationService().optimizeSingles(parameters, mins, maxs, accuracies);
+        } else if (args[0].equals("optimizeMulti")) {
+
+            String[] parameters = new String[args.length - 1];
+            double[] starts = new double[args.length - 1];
+            for (int i = 1; i < args.length; i++) {
+
+                String[] params = args[i].split(":");
+                String parameter = params[0];
+                double start = Double.valueOf(params[1]);
+                parameters[i-1] = parameter;
+                starts[i-1] = start;
+            }
+
+            ServiceLocator.instance().getSimulationService().optimizeMulti(parameters, starts);
+        } else if (args[0].equals("optimizeLinear")) {
+
+            String param = args[1];
+            double min = Double.parseDouble(args[2]);
+            double max = Double.parseDouble(args[3]);
+            double increment = Double.parseDouble(args[4]);
+
+            ServiceLocator.instance().getSimulationService().optimizeLinear(param, min, max, increment);
         } else {
             logger.info("please specify simulateByUnderlayings or simulateByActualOrders on the commandline");
         }
