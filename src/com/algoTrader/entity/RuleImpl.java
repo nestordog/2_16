a560ac617eb118b0f4b05fafc9c45708d596a6ed
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-08-11 08:00:53 +0000

    prepared / pattern: field -> method
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3084 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/entity/RuleImpl.java
+++ src/com/algoTrader/entity/RuleImpl.java
@@ -4,6 +4,22 @@ public class RuleImpl extends com.algoTrader.entity.Rule {
 
     private static final long serialVersionUID = -8481295095357769646L;
 
+    public boolean isPattern() {
+
+        if (getDefinition().startsWith("every")) {
+            return true;
+        } else if (getDefinition().startsWith("select") || (getDefinition().startsWith("insert"))) {
+            return false;
+        } else {
+            throw new IllegalArgumentException("unknown rule definition");
+        }
+    }
+
+    public boolean isPrepared() {
+
+        return getDefinition().contains("?");
+    }
+
     public String getPrioritisedDefinition() {
 
         if (getPriority() != 0) {
