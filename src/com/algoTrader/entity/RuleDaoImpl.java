8ad119be29e967b2a4e2f5d0aee64231e75cdf39
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-08-11 08:00:20 +0000

    formating
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3083 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/entity/RuleDaoImpl.java
+++ src/com/algoTrader/entity/RuleDaoImpl.java
@@ -1,13 +1,5 @@
-// license-header java merge-point
-/**
- * This is only generated once! It will never be overwritten.
- * You can (and have to!) safely modify it by hand.
- */
 package com.algoTrader.entity;
-/**
- * @see com.algoTrader.entity.Rule
- */
-public class RuleDaoImpl
-    extends com.algoTrader.entity.RuleDaoBase
-{
+
+public class RuleDaoImpl extends RuleDaoBase {
+
 }
