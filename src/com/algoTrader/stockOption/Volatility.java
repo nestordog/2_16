c3b847c88a39c669bb702a0675ac6266243f11c0
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-08-11 11:08:25 +0000

    remove sabr properties stop iterating if factor is less than 0.01% of factorSum
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3107 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/stockOption/Volatility.java
+++ src/com/algoTrader/stockOption/Volatility.java
@@ -14,11 +14,8 @@ public class Volatility {
     private static int strikeDistance = PropertiesUtil.getIntProperty("simulation.strikeDistance");
     private static double intrest = PropertiesUtil.getDoubleProperty("intrest");
     private static double dividend = PropertiesUtil.getDoubleProperty("dividend");
-    private static double beta = PropertiesUtil.getDoubleProperty("beta");
-    private static double volVol = PropertiesUtil.getDoubleProperty("volVol");
-    private static double correlation = -PropertiesUtil.getDoubleProperty("correlation");
 
-    public static double getIndexVola(double underlayingSpot, double atmVola, double years, double intrest, double dividend, double beta, double volVol, double correlation) {
+    public static double getIndexVola(double underlayingSpot, double atmVola, double years) {
 
         double accumulation = Math.exp(years * intrest);
         double forward = underlayingSpot * (1 - years * dividend) * Math.exp(years * intrest);
@@ -28,7 +25,7 @@ public class Volatility {
 
         // process atm strike
         {
-            double sabrVola = Sabr.getSabrVolatility(atmStrike, forward, years, atmVola, beta, volVol, correlation);
+            double sabrVola = Sabr.getSabrVolatility(atmStrike, forward, years, atmVola);
             double call = StockOptionUtil.getOptionPriceBS(underlayingSpot, atmStrike, sabrVola, years, intrest, dividend, OptionType.CALL);
             double put = StockOptionUtil.getOptionPriceBS(underlayingSpot, atmStrike, sabrVola, years, intrest, dividend, OptionType.PUT);
             double outOfTheMoneyPrice = (put + call) / 2;
@@ -39,10 +36,13 @@ public class Volatility {
         // process strikes below atm
         double strike = atmStrike - strikeDistance;
         while (true) {
-            double sabrVola = Sabr.getSabrVolatility(strike, forward, years, atmVola, beta, volVol, correlation);
+            double sabrVola = Sabr.getSabrVolatility(strike, forward, years, atmVola);
             double put = StockOptionUtil.getOptionPriceBS(underlayingSpot, strike, sabrVola, years, intrest, dividend, OptionType.PUT);
-            if (put < 0.5) break;
+            if (put < 0.5)
+                break;
             double factor = getFactor(strike, accumulation, strikeDistance, put);
+            if ((factor / factorSum) < 0.0001)
+                break;
             factorSum += factor;
             strike -= strikeDistance;
         }
@@ -50,10 +50,13 @@ public class Volatility {
         // process strikes above atm
         strike = atmStrike + strikeDistance;
         while (true) {
-            double sabrVola = Sabr.getSabrVolatility(strike, forward, years, atmVola, beta, volVol, correlation);
+            double sabrVola = Sabr.getSabrVolatility(strike, forward, years, atmVola);
             double call = StockOptionUtil.getOptionPriceBS(underlayingSpot, strike, sabrVola, years, intrest, dividend, OptionType.CALL);
-            if (call < 0.5) break;
+            if (call < 0.5)
+                break;
             double factor = getFactor(strike, accumulation, strikeDistance, call);
+            if ((factor / factorSum) < 0.0001)
+                break;
             factorSum += factor;
             strike += strikeDistance;
         }
@@ -61,11 +64,11 @@ public class Volatility {
         return Math.sqrt((factorSum * 2 - Math.pow(forward / atmStrike-1 , 2)) / years);
     }
 
-    public static double getAtmVola(final double underlayingSpot, final double indexVola, final double years, final double intrest, final double dividend, final double beta, final double volVol, final double correlation) throws ConvergenceException, FunctionEvaluationException, IllegalArgumentException {
+    public static double getAtmVola(final double underlayingSpot, final double indexVola, final double years) throws ConvergenceException, FunctionEvaluationException, IllegalArgumentException {
 
         UnivariateRealFunction function = new UnivariateRealFunction () {
             public double value(double atmVola) throws FunctionEvaluationException {
-                double currentIndexVola = getIndexVola(underlayingSpot, atmVola, years, intrest, dividend, beta, volVol, correlation);
+                double currentIndexVola = getIndexVola(underlayingSpot, atmVola, years);
                 double difference = currentIndexVola - indexVola;
                 return difference;
             }};
@@ -75,11 +78,6 @@ public class Volatility {
         solver.setAbsoluteAccuracy(0.0001);
 
         return solver.solve(function, indexVola * 0.7 , indexVola * 1.1, indexVola);
-    }
-
-    public static double getAtmVola(final double underlayingSpot, final double indexVola, final double years) throws ConvergenceException, FunctionEvaluationException, IllegalArgumentException {
-
-        return getAtmVola(underlayingSpot, indexVola, years, intrest, dividend, beta, volVol, correlation);
     }
 
     private static double getFactor(double strike, double accumulation, int strikeDistance, double outOfTheMoneyPrice) {
