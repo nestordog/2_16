a5ec97374957c18a2b026639ce1f7b95ed3d527a
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-09-08 19:27:56 +0000

    update getCommission
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3176 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/entity/StockOptionImpl.java
+++ src/com/algoTrader/entity/StockOptionImpl.java
@@ -3,20 +3,19 @@ package com.algoTrader.entity;
 import java.math.BigDecimal;
 
 import com.algoTrader.enumeration.TransactionType;
+import com.algoTrader.util.PropertiesUtil;
 import com.algoTrader.util.RoundUtil;
 
 public class StockOptionImpl extends com.algoTrader.entity.StockOption {
 
     private static final long serialVersionUID = -3168298592370987085L;
 
+    private static final double commission = PropertiesUtil.getDoubleProperty("strategie.commission");
+
     public BigDecimal getCommission(long quantity, TransactionType transactionType) {
 
         if (TransactionType.SELL.equals(transactionType) || TransactionType.BUY.equals(transactionType)) {
-            if (quantity < 4) {
-                return RoundUtil.getBigDecimal(quantity * 1.5 + 5);
-            } else {
-                return RoundUtil.getBigDecimal(quantity * 3);
-            }
+            return RoundUtil.getBigDecimal(quantity * commission);
         } else {
             return new BigDecimal(0);
         }
