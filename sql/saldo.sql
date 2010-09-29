8501512d0740e697bb1fe632a1b10d8a61179334
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-09-29 18:08:25 +0000

    handle DEBIT and CREDIT correctly
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3241 19eef36c-86f0-de11-afab-0004ac968479

--- sql/saldo.sql
+++ sql/saldo.sql
@@ -1,21 +1,16 @@
   select
-    `t1`.`DATE_TIME` AS `date_time`,
-    `t1`.`TYPE` AS `type`,
-    `s1`.`SYMBOL` AS `symbol`,
-    `s1`.`POSITION_FK` AS `POSITION_FK`,
-    `o1`.`STRIKE` AS `STRIKE`,
-    `o1`.`EXPIRATION` AS `expiration`,
-    `t1`.`QUANTITY` AS `quantity`,
-    `t1`.`PRICE` AS `price`,
-    `t1`.`COMMISSION` AS `commission`,
-    (
-  select
-    sum(((-(`t2`.`QUANTITY`) * `t2`.`PRICE`) - `t2`.`COMMISSION`)) AS `sum(-t2.quantity * t2.price - t2.commission)`
-  from
-    `transaction` `t2`
-  where
-    (`t2`.`id` <= `t1`.`id`)) AS `saldo`
-  from
-    ((`transaction` `t1` left join `security` `s1` on((`t1`.`SECURITY_FK` = `s1`.`id`))) left join `stock_option` `o1` on((`o1`.`ID` = `s1`.`id`)))
-  order by
-    `t1`.`id`;
+    t1.DATE_TIME AS date_time,
+    t1.TYPE AS type,
+    s1.SYMBOL AS symbol,
+    s1.ISIN AS isin,
+    s1.POSITION_FK AS position_fk,
+    o1.STRIKE AS STRIKE,
+    o1.EXPIRATION AS expiration,
+    t1.QUANTITY AS quantity,
+    t1.PRICE AS price,
+    t1.COMMISSION AS commission,
+    (select sum(case t2.TYPE when 'CREDIT' then t2.PRICE when 'DEBIT' then t2.PRICE else (-t2.QUANTITY * t2.PRICE - t2.COMMISSION) end) from transaction as t2 where t2.id <= t1.id) AS saldo
+  from transaction t1
+  left join security s1 on t1.SECURITY_FK = s1.id
+  left join stock_option o1 on o1.ID = s1.id
+  order by t1.id;
