e011526f46d2ba02ef5417226092f06126de6e6a
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-09-30 11:40:12 +0000

    use receiveFA to get accounts
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3242 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/service/ib/IbAccountServiceImpl.java
+++ src/com/algoTrader/service/ib/IbAccountServiceImpl.java
@@ -1,14 +1,25 @@
 package com.algoTrader.service.ib;
 
+import java.io.StringReader;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
 import org.apache.log4j.Logger;
+import org.apache.xpath.XPathAPI;
 import org.springframework.beans.factory.InitializingBean;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.traversal.NodeIterator;
+import org.xml.sax.InputSource;
 
 import com.algoTrader.util.MyLogger;
 import com.algoTrader.util.PropertiesUtil;
@@ -21,7 +32,6 @@ public class IbAccountServiceImpl extends IbAccountServiceBase implements Initia
     private static boolean ibEnabled = "IB".equals(PropertiesUtil.getProperty("marketChannel"));
 
     private static int retrievalTimeout = PropertiesUtil.getIntProperty("ib.retrievalTimeout");
-    private static String[] accounts = PropertiesUtil.getStringArrayProperty("ib.accounts");
 
     private DefaultClientSocket client;
     private DefaultWrapper wrapper;
@@ -30,6 +40,7 @@ public class IbAccountServiceImpl extends IbAccountServiceBase implements Initia
     private Condition condition = this.lock.newCondition();
 
     private Map<String, Map<String, String>> allAccountValues;
+    private String fa;
 
     private static int clientId = 2;
 
@@ -53,6 +64,21 @@ public class IbAccountServiceImpl extends IbAccountServiceBase implements Initia
 
                     Map<String, String> values = IbAccountServiceImpl.this.allAccountValues.get(accountName);
                     values.put(key, value);
+
+                    IbAccountServiceImpl.this.condition.signalAll();
+
+                } finally {
+                    IbAccountServiceImpl.this.lock.unlock();
+                }
+            }
+
+            public void receiveFA(int faDataType, String xml) {
+
+                IbAccountServiceImpl.this.lock.lock();
+
+                try {
+
+                    IbAccountServiceImpl.this.fa = xml;
 
                     IbAccountServiceImpl.this.condition.signalAll();
 
@@ -103,10 +129,46 @@ public class IbAccountServiceImpl extends IbAccountServiceBase implements Initia
         return this.allAccountValues.get(accountName).get(key);
     }
 
+    private Set<String> getAccounts() throws Exception {
+
+        IbAccountServiceImpl.this.lock.lock();
+
+        try {
+
+            IbAccountServiceImpl.this.fa = null;
+
+            this.client.requestFA(1);
+
+            while (this.fa == null) {
+
+                if (!this.condition.await(retrievalTimeout, TimeUnit.SECONDS)) {
+                    throw new IbAccountServiceException("could not get FA ");
+                }
+            }
+        } finally {
+            IbAccountServiceImpl.this.lock.unlock();
+        }
+
+        // get the xml-document
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(new InputSource(new StringReader(this.fa)));
+
+        // parse the document using XPath
+        NodeIterator iterator = XPathAPI.selectNodeIterator(document, "//Group[name='AllClients']/ListOfAccts/String");
+        Node node;
+        Set<String> accounts = new HashSet<String>();
+        while ((node = iterator.nextNode()) != null) {
+            accounts.add(node.getFirstChild().getNodeValue());
+        }
+
+        return accounts;
+    }
+
     protected long handleGetNumberOfContractsByMargin(double initialMarginPerContract) throws Exception {
 
         long numberOfContractsByMargin = 0;
-        for (String account : accounts) {
+        for (String account : getAccounts()) {
             double availableAmount = Double.parseDouble(retrieveAccountValue(account, "CHF", "AvailableFunds"));
             long numberOfContracts = (long) (availableAmount / initialMarginPerContract);
             numberOfContractsByMargin += numberOfContracts;
