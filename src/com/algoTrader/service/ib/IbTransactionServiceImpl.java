a10895991fda12c6025957865a312d7f7425bbe3
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-10-08 17:09:13 +0000

    24h time format
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3260 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/service/ib/IbTransactionServiceImpl.java
+++ src/com/algoTrader/service/ib/IbTransactionServiceImpl.java
@@ -36,7 +36,7 @@ public class IbTransactionServiceImpl extends IbTransactionServiceBase implement
 
     private static Logger logger = MyLogger.getLogger(IbTransactionServiceImpl.class.getName());
 
-    private static SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd  hh:mm:ss");
+    private static SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd  HH:mm:ss");
 
     private static boolean simulation = PropertiesUtil.getBooleanProperty("simulation");
     private static boolean ibEnabled = "IB".equals(PropertiesUtil.getProperty("marketChannel"));
