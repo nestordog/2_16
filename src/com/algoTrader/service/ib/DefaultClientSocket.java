937dd1896bfde3227a7b8cfe812def0a3eff8bf0
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-10-08 14:13:53 +0000

    connectionAvailable uniform timeouts set ConnectionState
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3256 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/service/ib/DefaultClientSocket.java
+++ src/com/algoTrader/service/ib/DefaultClientSocket.java
@@ -1,12 +1,22 @@
 package com.algoTrader.service.ib;
 
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.Socket;
+
+import org.apache.log4j.Logger;
+
+import com.algoTrader.enumeration.ConnectionState;
+import com.algoTrader.util.MyLogger;
 import com.algoTrader.util.PropertiesUtil;
 import com.ib.client.EClientSocket;
 
 public class DefaultClientSocket extends EClientSocket {
 
-    private static final long reconnectTimeout = PropertiesUtil.getIntProperty("ib.reconnectTimeout");
+    private static final long connectionTimeout = PropertiesUtil.getIntProperty("ib.connectionTimeout");
     private static int port = PropertiesUtil.getIntProperty("ib.port");
+
+    private static Logger logger = MyLogger.getLogger(DefaultClientSocket.class.getName());
 
     private DefaultWrapper wrapper;
 
@@ -20,35 +30,54 @@ public class DefaultClientSocket extends EClientSocket {
 
         if (isConnected()) {
             eDisconnect();
+
             try {
-                Thread.sleep(1000);
+                Thread.sleep(connectionTimeout);
             } catch (InterruptedException e1) {
                 try {
                     // during eDisconnect this thread get's interrupted so sleep again
-                    Thread.sleep(1000);
+                    Thread.sleep(connectionTimeout);
                 } catch (InterruptedException e2) {
-                    e2.printStackTrace();
+                    logger.error(e2);
                 }
             }
         }
 
         this.wrapper.setRequested(false);
 
-        while (!isConnected()) {
-            eConnect(null, port, clientId);
+        while (!connectionAvailable()) {
 
-            if (!isConnected()) {
+            try {
+                Thread.sleep(connectionTimeout);
+            } catch (InterruptedException e1) {
                 try {
-                    Thread.sleep(reconnectTimeout);
-                } catch (InterruptedException e1) {
-                    try {
-                        // during eDisconnect this thread get's interrupted so sleep again
-                        Thread.sleep(reconnectTimeout);
-                    } catch (InterruptedException e2) {
-                        e2.printStackTrace();
-                    }
+                    // during eDisconnect this thread get's interrupted so sleep again
+                    Thread.sleep(connectionTimeout);
+                } catch (InterruptedException e2) {
+                    logger.error(e2);
                 }
             }
+        }
+
+        eConnect(null, port, clientId);
+
+        if (isConnected()) {
+            this.wrapper.setState(ConnectionState.CONNECTED);
+            logger.debug("connectionState: " + this.wrapper.getState());
+        }
+    }
+
+    private static synchronized boolean connectionAvailable() {
+        try {
+            Socket socket = new Socket("127.0.0.1", port);
+            socket.close();
+            return true;
+        } catch (ConnectException e) {
+            // do nothing, gateway is down
+            return false;
+        } catch (IOException e) {
+            logger.error(e);
+            return false;
         }
     }
 }
