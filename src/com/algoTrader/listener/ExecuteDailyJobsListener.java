7c5d509388db33bf7aff4d267192f7c8c39c95af
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-10-09 13:04:41 +0000

    renamed processCashTransactions
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3267 19eef36c-86f0-de11-afab-0004ac968479

---
+++ src/com/algoTrader/listener/ExecuteDailyJobsListener.java
@@ -0,0 +1,18 @@
+package com.algoTrader.listener;
+
+import com.algoTrader.ServiceLocator;
+import com.espertech.esper.client.EPServiceProvider;
+import com.espertech.esper.client.EPStatement;
+import com.espertech.esper.client.EventBean;
+import com.espertech.esper.client.StatementAwareUpdateListener;
+
+public class ExecuteDailyJobsListener  implements StatementAwareUpdateListener {
+
+    public void update(EventBean[] newEvents, EventBean[] oldEvents, EPStatement statement,
+            EPServiceProvider epServiceProvider) {
+
+        ServiceLocator.instance().getActionService().setMargins();
+
+        ServiceLocator.instance().getDispatcherService().getAccountService().processCashTransactions();
+    }
+}
