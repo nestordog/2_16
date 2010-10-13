bc026a0f775c0d870002b4a7668cb15c2b75f171
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-10-13 18:08:04 +0000

    use unix2dos
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3281 19eef36c-86f0-de11-afab-0004ac968479

--- sql/dump-db.bat
+++ sql/dump-db.bat
@@ -1,3 +1,5 @@
 "D:\MySQL Server 5.1\bin\mysqldump.exe" -X --hex-blob -t -u @localhost -r D:\AlgoTrader\sql\rules.xml algotrader rule
 "D:\MySQL Server 5.1\bin\mysqldump.exe" -t -u @localhost -r D:\AlgoTrader\sql\rules.sql algotrader rule
 "D:\MySQL Server 5.1\bin\mysqldump.exe" -d -u @localhost -r D:\AlgoTrader\sql\db-structure.sql algotrader
+"D:\AlgoTrader\sql\unix2dos.exe" D:\AlgoTrader\sql\rules.xml
+"D:\AlgoTrader\sql\unix2dos.exe" D:\AlgoTrader\sql\db-structure.sql
