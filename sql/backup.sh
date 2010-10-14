4cc58c39d50981a8c3a319693fea78ee0801d80e
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-10-14 17:31:24 +0000

    backup tickdata
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3291 19eef36c-86f0-de11-afab-0004ac968479

--- sql/backup.sh
+++ sql/backup.sh
@@ -3,7 +3,10 @@ set -v off
 
 cd /usr/local/AlgoTrader/sql
 
-mysqldump -u @localhost algotrader > backup.sql
+/usr/local/bin/mysqldump -u @localhost algotrader > backup.sql
 
-smbclient -U aflury -c "cd aflury/Algotrader/backup;put backup.sql" //bubba/home franklin91
+/usr/local/bin/smbclient -U aflury -c "cd aflury/Algotrader/backup;put backup.sql" //bubba/home franklin91
 
+cd /usr/local/AlgoTrader/results/tickdata
+
+/usr/local/bin/smbclient -U aflury -c "cd aflury/Algotrader/backup/tickdata;recurse;prompt;mput *" //bubba/home franklin91
