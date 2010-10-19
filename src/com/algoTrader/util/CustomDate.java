f0d1303bb534eb1cf8574942abf601d18cf31a05
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-10-19 14:32:02 +0000

    handle empty string
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3311 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/util/CustomDate.java
+++ src/com/algoTrader/util/CustomDate.java
@@ -4,8 +4,14 @@ import java.util.Date;
 
 public class CustomDate extends Date {
 
+    private static final long serialVersionUID = -1910877406220278376L;
+
     public CustomDate(String date) {
-        super(Long.parseLong(date));
+        super();
+
+        if (!"".equals(date)) {
+            setTime(Long.parseLong(date));
+        }
     }
 
     public CustomDate(long date) {
