b6d6d55a98d176f200c2ba6deaf3302bd2ed9845
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-11-23 16:07:38 +0000

    separate rules and db-structure
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3387 19eef36c-86f0-de11-afab-0004ac968479

--- sql/dump-db.bat
+++ sql/dump-db.bat
@@ -1,5 +1,6 @@
 "D:\MySQL Server 5.1\bin\mysqldump.exe" -X --hex-blob -t -u @localhost -r D:\AlgoTrader\sql\rules.xml algotrader rule
 "D:\MySQL Server 5.1\bin\mysqldump.exe" -t -u @localhost -r D:\AlgoTrader\sql\rules.sql algotrader rule
-"D:\MySQL Server 5.1\bin\mysqldump.exe" -d -u @localhost -r D:\AlgoTrader\sql\db-structure.sql algotrader
 "D:\AlgoTrader\sql\unix2dos.exe" D:\AlgoTrader\sql\rules.xml
+
+"D:\MySQL Server 5.1\bin\mysqldump.exe" -d -u @localhost -r D:\AlgoTrader\sql\db-structure.sql algotraderlive
 "D:\AlgoTrader\sql\unix2dos.exe" D:\AlgoTrader\sql\db-structure.sql
