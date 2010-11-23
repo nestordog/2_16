36ebb1ba408c180661318a753863d66d3d171848
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-11-23 16:17:36 +0000

    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3388 19eef36c-86f0-de11-afab-0004ac968479

--- sql/dump-db.bat
+++ sql/dump-db.bat
@@ -2,5 +2,5 @@
 "D:\MySQL Server 5.1\bin\mysqldump.exe" -t -u @localhost -r D:\AlgoTrader\sql\rules.sql algotrader rule
 "D:\AlgoTrader\sql\unix2dos.exe" D:\AlgoTrader\sql\rules.xml
 
-"D:\MySQL Server 5.1\bin\mysqldump.exe" -d -u @localhost -r D:\AlgoTrader\sql\db-structure.sql algotraderlive
+"D:\MySQL Server 5.1\bin\mysqldump.exe" -d -u @localhost -r D:\AlgoTrader\sql\db-structure.sql algotrader
 "D:\AlgoTrader\sql\unix2dos.exe" D:\AlgoTrader\sql\db-structure.sql
