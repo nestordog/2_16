52953979e02aec3b3bfcd109117a447a57fe8992
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-12-16 16:04:44 +0000

    move prefeeTicks -> ThetaService handleProcessSecuritiesOnWatchlist -> handleRetrieveTicks retrieve RawTickVO add handleCompleteRawTick add handlePropagateTick putOn/removeFrom watchlist: use watchlist / watchers / pass strategyname handleCalculateSabr -> StockOptionRetrieverService add RetrieveTickListener
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3476 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/service/TickServiceImpl.java
+++ src/com/algoTrader/service/TickServiceImpl.java
@@ -3,12 +3,8 @@ package com.algoTrader.service;
 import java.io.File;
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Collection;
-import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,88 +12,58 @@ import java.util.Map;
 import org.apache.log4j.Logger;
 import org.supercsv.exception.SuperCSVException;
 
+import com.algoTrader.ServiceLocator;
 import com.algoTrader.entity.Security;
 import com.algoTrader.entity.StockOption;
+import com.algoTrader.entity.Strategy;
+import com.algoTrader.entity.StrategyImpl;
 import com.algoTrader.entity.Tick;
-import com.algoTrader.enumeration.OptionType;
-import com.algoTrader.sabr.SABRCalibration;
-import com.algoTrader.sabr.SABRCalibrationParams;
+import com.algoTrader.entity.TickImpl;
 import com.algoTrader.stockOption.StockOptionUtil;
 import com.algoTrader.util.ConfigurationUtil;
 import com.algoTrader.util.DateUtil;
-import com.algoTrader.util.EsperService;
 import com.algoTrader.util.MyLogger;
 import com.algoTrader.util.RoundUtil;
 import com.algoTrader.util.io.CsvTickReader;
 import com.algoTrader.util.io.CsvTickWriter;
-import com.algoTrader.util.io.DBTickInputAdapter;
-import com.espertech.esperio.AdapterCoordinator;
-import com.espertech.esperio.AdapterCoordinatorImpl;
+import com.algoTrader.vo.RawTickVO;
+import com.espertech.esper.client.EventBean;
+import com.espertech.esper.client.UpdateListener;
 
 public abstract class TickServiceImpl extends TickServiceBase {
 
     private static Logger logger = MyLogger.getLogger(TickServiceImpl.class.getName());
-
-    private static String isin = ConfigurationUtil.getBaseConfig().getString("strategie.isin");
     private static String dataSet = ConfigurationUtil.getBaseConfig().getString("dataSource.dataSet");
-    private static double intrest = ConfigurationUtil.getBaseConfig().getDouble("strategie.intrest");
-    private static double dividend = ConfigurationUtil.getBaseConfig().getDouble("strategie.dividend");
-    private static double beta = ConfigurationUtil.getBaseConfig().getDouble("strategie.beta");
-    private static int strikeDistance = ConfigurationUtil.getBaseConfig().getInt("strategie.strikeDistance");
-
-    private static SimpleDateFormat inputFormat = new SimpleDateFormat("yyyyMMdd-kkmmss");
-    private static SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy.MM.dd kk:mm:ss");
-
-    private static double MILLISECONDS_PER_YEAR = 31536000000l;
-    private static int advanceMinutes = 10;
+    private static boolean simulation = ConfigurationUtil.getBaseConfig().getBoolean("simulation");
 
     private Map<Security, CsvTickWriter> csvWriters = new HashMap<Security, CsvTickWriter>();
+    private Map<Integer, Tick> securities = new HashMap<Integer, Tick>();
 
     @SuppressWarnings("unchecked")
-    protected void handlePrefeedTicks() throws SuperCSVException, IOException {
-
-        Security underlaying = getSecurityDao().findByISIN(isin);
-
-        double kFastDays = Math.max(ConfigurationUtil.getBaseConfig().getDouble("callKFastDays"), ConfigurationUtil.getBaseConfig().getDouble("putKFastDays"));
-        double kSlowDays = Math.max(ConfigurationUtil.getBaseConfig().getDouble("callKSlowDays"), ConfigurationUtil.getBaseConfig().getDouble("putKSlowDays"));
-        double dSlowDays = Math.max(ConfigurationUtil.getBaseConfig().getDouble("callDSlowDays"), ConfigurationUtil.getBaseConfig().getDouble("putDSlowDays"));
-        int numberOfTicks = (int) Math.ceil((kFastDays + kSlowDays + dSlowDays) * ConfigurationUtil.getBaseConfig().getLong("simulation.eventsPerDay"));
-
-        // we need to get 2 x numberOfTicks so that KEEP_STOCHASTIC_VO has the
-        // keeps that same number of ticks we need to initialize stochastic
-        Collection<Tick> recentTicks = getTickDao().findLastNTicksForSecurity(2 * numberOfTicks, underlaying.getId());
-        Tick firstTick = recentTicks.iterator().next();
-
-        Collection<Tick> ticks = getTickDao().findEndOfDayTicks(firstTick.getDateTime(), underlaying.getId());
-
-        ticks.addAll(recentTicks);
-
-        AdapterCoordinator coordinator = new AdapterCoordinatorImpl(EsperService.getEPServiceInstance(), true, true);
-        coordinator.coordinate(new DBTickInputAdapter(ticks));
-        coordinator.start();
-    }
-
-    @SuppressWarnings("unchecked")
-    protected void handleProcessSecuritiesOnWatchlist() throws SuperCSVException, IOException  {
+    protected void handleRetrieveTicks() throws SuperCSVException, IOException  {
 
         List<Security> securities = getSecurityDao().findSecuritiesOnWatchlist();
         for (Security security : securities) {
 
             // retrieve ticks only between marketOpen & close
-            if (DateUtil.compareToTime(security.getMarketOpen()) >= 0 &&
-                DateUtil.compareToTime(security.getMarketClose()) <= 0) {
+            if (DateUtil.compareToTime(security.getSecurityFamily().getMarketOpen()) >= 0 &&
+                DateUtil.compareToTime(security.getSecurityFamily().getMarketClose()) <= 0) {
 
-                Tick tick = retrieveTick(security);
+                RawTickVO rawTick = retrieveTick(security);
 
                 // if we hit a timeout, we get null
-                if (tick != null) {
+                if (rawTick != null) {
+
+                    Tick tick = completeRawTick(rawTick);
 
                     try {
                         tick.validate();
-                        EsperService.sendEvent(tick);
                     } catch (Exception e) {
                         // do nothing, just ignore invalideTicks
                     }
+
+                    propagateTick(tick);
+                    createSimulatedTicks(tick);
 
                     // write the tick to file (even if not valid)
                     CsvTickWriter csvWriter = this.csvWriters.get(security);
@@ -114,44 +80,120 @@ public abstract class TickServiceImpl extends TickServiceBase {
         }
     }
 
-    protected void handlePutOnWatchlist(int stockOptionId) throws Exception {
+    protected Tick handleCompleteRawTick(RawTickVO rawTick) {
 
-        StockOption stockOption = (StockOption)getStockOptionDao().load(stockOptionId);
-        putOnWatchlist(stockOption);
+        return getTickDao().rawTickVOToEntity(rawTick);
     }
 
     @SuppressWarnings("unchecked")
-    protected void handlePutOnWatchlist(StockOption stockOption) throws Exception {
+    protected void handleCreateSimulatedTicks(Tick tick) throws Exception {
 
-        if (!stockOption.isOnWatchlist()) {
+        if (!simulation)
+            return;
 
-            putOnExternalWatchlist(stockOption);
+        List<StockOption> stockOptions = getStockOptionDao().findStockOptionsOnWatchlist();
+        for (StockOption stockOption : stockOptions) {
 
-            stockOption.setOnWatchlist(true);
-            getStockOptionDao().update(stockOption);
-            getStockOptionDao().getStockOptionsOnWatchlist(false).add(stockOption);
+            Security underlaying = stockOption.getSecurityFamily().getUnderlaying();
+            if (tick.getSecurity().getId() == underlaying.getId()) {
 
-            logger.info("put stockOption on watchlist " + stockOption.getSymbol());
+                // save the underlyingTick for later
+                this.securities.put(tick.getSecurity().getId(), tick);
+
+            } else if (tick.getSecurity().getId() == underlaying.getVolatility().getId()) {
+
+                // only create StockOptionTicks when a volaTick arrives
+                Tick underlayingTick = this.securities.get(underlaying.getId());
+
+                if (underlayingTick != null) {
+
+                    double lastDouble = StockOptionUtil.getOptionPrice(stockOption, underlayingTick.getCurrentValueDouble(), tick.getCurrentValueDouble() / 100);
+                    BigDecimal last = RoundUtil.getBigDecimal(lastDouble);
+
+                    Tick stockOptionTick = new TickImpl();
+                    stockOptionTick.setDateTime(tick.getDateTime());
+                    stockOptionTick.setLast(last);
+                    stockOptionTick.setLastDateTime(tick.getDateTime());
+                    stockOptionTick.setVol(0);
+                    stockOptionTick.setVolBid(0);
+                    stockOptionTick.setVolAsk(0);
+                    stockOptionTick.setBid(new BigDecimal(0));
+                    stockOptionTick.setAsk(new BigDecimal(0));
+                    stockOptionTick.setOpenIntrest(0);
+                    stockOptionTick.setSettlement(new BigDecimal(0));
+                    stockOptionTick.setSecurity(stockOption);
+
+                    propagateTick(stockOptionTick);
+                }
+            }
         }
     }
 
-    protected void handleRemoveFromWatchlist(int stockOptionId) throws Exception {
+    @SuppressWarnings("unchecked")
+    protected void handlePropagateTick(Tick tick) {
 
-        StockOption stockOption = (StockOption)getStockOptionDao().load(stockOptionId);
-        removeFromWatchlist(stockOption);
+        logger.debug(tick.getSecurity().getSymbol() + " " + tick);
+
+        getRuleService().sendEvent(StrategyImpl.BASE, tick);
+
+        Collection<Strategy> strategies = tick.getSecurity().getWatchers();
+        for (Strategy strategy : strategies) {
+            getRuleService().sendEvent(strategy.getName(), tick);
+        }
     }
 
-    protected void handleRemoveFromWatchlist(StockOption stockOption) throws Exception {
+    protected void handlePutOnWatchlist(String strategyName, int securityId) throws Exception {
 
-        if (stockOption.isOnWatchlist()) {
+        Strategy strategy = getStrategyDao().findByName(strategyName);
+        Security security = getSecurityDao().load(securityId);
+        putOnWatchlist(strategy, security);
+    }
 
-            removeFromExternalWatchlist(stockOption);
+    @SuppressWarnings("unchecked")
+    protected void handlePutOnWatchlist(Strategy strategy, Security security) throws Exception {
 
-            stockOption.setOnWatchlist(false);
-            getStockOptionDao().update(stockOption);
-            getStockOptionDao().getStockOptionsOnWatchlist(false).remove(stockOption);
+        if (!security.getWatchers().contains(strategy)) {
 
-            logger.info("removed stockOption from watchlist " + stockOption.getSymbol());
+            // only put on external watchlist if nobody was watching this security so far
+            if (security.getWatchers().size() == 0) {
+                putOnExternalWatchlist(security);
+            }
+
+            // update links
+            security.getWatchers().add(strategy);
+            getSecurityDao().update(security);
+            strategy.getWatchlist().add(security);
+            getStrategyDao().update(strategy);
+
+            logger.info("put security on watchlist " + security.getSymbol());
+        }
+    }
+
+    protected void handleRemoveFromWatchlist(String strategyName, int securityId) throws Exception {
+
+        Strategy strategy = getStrategyDao().findByName(strategyName);
+        Security security = getSecurityDao().load(securityId);
+
+        removeFromWatchlist(strategy, security);
+    }
+
+    protected void handleRemoveFromWatchlist(Strategy strategy, Security security) throws Exception {
+
+        if (security.getWatchers().contains(strategy)) {
+
+            // update links
+            security.getWatchers().remove(strategy);
+            getSecurityDao().update(security);
+            strategy.getWatchlist().remove(security);
+            getStrategyDao().update(strategy);
+
+            // only remove from external watchlist if nobody is watch this security anymore
+            if (security.getWatchers().size() == 0) {
+                removeFromExternalWatchlist(security);
+            }
+
+
+            logger.info("removed security from watchlist " + security.getSymbol());
         }
     }
 
@@ -165,7 +207,7 @@ public abstract class TickServiceImpl extends TickServiceBase {
 
         if (file.exists()) {
 
-            Security security = getSecurityDao().findByISIN(isin);
+            Security security = getSecurityDao().findByIsin(isin);
             CsvTickReader reader = new CsvTickReader(isin);
 
             Tick tick;
@@ -191,96 +233,11 @@ public abstract class TickServiceImpl extends TickServiceBase {
         }
     }
 
-    @SuppressWarnings("unchecked")
-    protected void handleCalculateSabr(String isin, String startDateString, String expirationDateString, String optionType) throws Exception {
+    public static class RetrieveTickListener implements UpdateListener {
 
-        Security underlaying = getSecurityDao().findByISIN(isin);
-        OptionType type = OptionType.fromString(optionType);
-        Date startDate = inputFormat.parse(startDateString);
-        Date expirationDate = inputFormat.parse(expirationDateString);
-        Date closeHour = (new SimpleDateFormat("kkmmss")).parse("172000");
+        public void update(EventBean[] newEvents, EventBean[] oldEvents) {
 
-        GregorianCalendar cal = new GregorianCalendar();
-        cal.setTime(startDate);
-
-        SABRCalibration sabr = SABRCalibration.getInstance();
-
-        while (cal.getTime().compareTo(expirationDate) < 0) {
-
-            if ((cal.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY) || cal.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY) {
-                cal.add(Calendar.DAY_OF_YEAR, 1);
-                continue;
-            }
-
-            Date date = cal.getTime();
-
-            while (DateUtil.compareTime(cal.getTime(), closeHour) <= 0) {
-
-                date = cal.getTime();
-
-                double years = (expirationDate.getTime() - date.getTime()) / MILLISECONDS_PER_YEAR;
-
-                Tick underlayingTick = getTickDao().findByDateAndSecurity(date, underlaying);
-                if (underlayingTick == null || underlayingTick.getLast() == null) {
-                    cal.add(Calendar.MINUTE, advanceMinutes);
-                    continue;
-                }
-
-                BigDecimal underlayingSpot = underlayingTick.getLast();
-                double forward = StockOptionUtil.getForward(underlayingSpot.doubleValue(), years, intrest, dividend);
-                double atmStrike = RoundUtil.roundToNextN(underlayingSpot, strikeDistance, type).doubleValue();
-
-                List<Tick> ticks = getTickDao().findByDateTypeAndExpiration(date, type, expirationDate);
-                List<Double> strikes = new ArrayList<Double>();
-                List<Double> volatilities = new ArrayList<Double>();
-                double atmVola = 0;
-                for (Tick tick : ticks) {
-
-                    StockOption stockOption = (StockOption) tick.getSecurity();
-
-                    try {
-                        stockOption.validateTickSpread(tick);
-                    } catch (Exception ex) {
-                        continue;
-                    }
-                    double strike = stockOption.getStrike().doubleValue();
-                    double optionValue = tick.getCurrentValueDouble();
-
-                    try {
-                        double volatility = StockOptionUtil.getVolatility(underlayingSpot.doubleValue(), strike, optionValue, years, type);
-                        strikes.add(strike);
-                        volatilities.add(volatility);
-
-                        if (atmStrike == strike) {
-                            atmVola = volatility;
-                        }
-                    } catch (Exception e) {
-                        // do nothing
-                    }
-                }
-
-                if (strikes.size() < 10 || atmVola == 0) {
-                    cal.add(Calendar.MINUTE, advanceMinutes);
-                    continue;
-                }
-
-                Double[] strikesArray = strikes.toArray(new Double[0]);
-                Double[] volatilitiesArray = volatilities.toArray(new Double[0]);
-
-                SABRCalibrationParams params = sabr.calibrate(strikesArray, volatilitiesArray, atmVola, forward, years, beta);
-
-                if (params.getA() < 100) {
-                    System.out.println(outputFormat.format(date) + " " + params.getA() + " " + params.getR() + " " + params.getV());
-                }
-
-                cal.add(Calendar.MINUTE, advanceMinutes);
-            }
-
-            cal.add(Calendar.DAY_OF_YEAR, 1);
-            cal.set(Calendar.HOUR_OF_DAY, 9);
-            cal.set(Calendar.MINUTE, 00);
+            ServiceLocator.serverInstance().getDispatcherService().getTickService().retrieveTicks();
         }
-
-        sabr.dispose();
     }
 }
