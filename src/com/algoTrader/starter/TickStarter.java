1df65041a637b79ef354f5850bcdbd1067cf7e89
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-12-16 14:07:52 +0000

    only start BASE strategy
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3452 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/starter/TickStarter.java
+++ src/com/algoTrader/starter/TickStarter.java
@@ -1,10 +1,9 @@
 package com.algoTrader.starter;
 
 import com.algoTrader.ServiceLocator;
-import com.algoTrader.enumeration.RuleName;
+import com.algoTrader.entity.StrategyImpl;
 import com.algoTrader.service.RuleService;
-import com.algoTrader.service.TickService;
-import com.algoTrader.util.EsperService;
+
 
 public class TickStarter {
 
@@ -14,30 +13,15 @@ public class TickStarter {
     }
 
     public static void start() {
-        RuleService ruleService = ServiceLocator.instance().getRuleService();
-        TickService tickService = ServiceLocator.instance().getDispatcherService().getTickService();
 
-        // run all the recorded ticks through to initialize MACD and stochastic
-        ruleService.activate(RuleName.CREATE_END_OF_DAY_TICK);
-        ruleService.activate(RuleName.CREATE_MACD);
-        ruleService.activate(RuleName.CREATE_MACD_SIGNAL);
-        ruleService.activate(RuleName.SET_TREND);
-        ruleService.activate(RuleName.CREATE_MACD_VO);
-        ruleService.activate(RuleName.KEEP_MACD_VO);
+        // initialitze IB services
+        ServiceLocator.serverInstance().getIbService().init();
 
-        ruleService.activate(RuleName.CREATE_K_FAST);
-        ruleService.activate(RuleName.CREATE_K_SLOW);
-        ruleService.activate(RuleName.CREATE_D_SLOW);
-        ruleService.activate(RuleName.CREATE_STOCHASTIC_VO);
-        ruleService.activate(RuleName.KEEP_STOCHASTIC_VO);
-
-        tickService.prefeedTicks();
-
-        // switch to internalClock
-        EsperService.setInternalClock(true);
-        EsperService.enableJmx();
-
-        //activate the rest of the rules
-        ruleService.activateAll();
+        // start all BASE rules
+        RuleService ruleService = ServiceLocator.serverInstance().getRuleService();
+        ruleService.initServiceProvider(StrategyImpl.BASE);
+        ruleService.setInternalClock(StrategyImpl.BASE, true);
+        ruleService.activateAll(StrategyImpl.BASE);
+        ruleService.enableManagement(StrategyImpl.BASE);
     }
 }
