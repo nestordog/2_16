947e591ac948427997afff4c9e4b14faed55b0c5
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-12-16 14:56:42 +0000

    moved logic from ManagementService
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3469 19eef36c-86f0-de11-afab-0004ac968479

---
+++ src/com/algoTrader/service/ReportingServiceImpl.java
@@ -0,0 +1,122 @@
+package com.algoTrader.service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import com.algoTrader.entity.PositionDao;
+import com.algoTrader.entity.Security;
+import com.algoTrader.entity.Strategy;
+import com.algoTrader.entity.Tick;
+import com.algoTrader.entity.TransactionDao;
+import com.algoTrader.vo.PositionVO;
+import com.algoTrader.vo.TransactionVO;
+
+public class ReportingServiceImpl extends ReportingServiceBase {
+
+    protected BigDecimal handleGetStrategyCashBalance(String strategyName) throws Exception {
+
+        Strategy strategy = getStrategyDao().findByName(strategyName);
+        if (strategy.isBase()) {
+            return getStrategyDao().getPortfolioCashBalance();
+        } else {
+            return strategy.getCashBalance();
+        }
+    }
+
+    protected BigDecimal handleGetStrategySecuritiesCurrentValue(String strategyName) throws Exception {
+
+        Strategy strategy = getStrategyDao().findByName(strategyName);
+        if (strategy.isBase()) {
+            return getStrategyDao().getPortfolioSecuritiesCurrentValue();
+        } else {
+            return strategy.getSecuritiesCurrentValue();
+        }
+
+    }
+
+    protected BigDecimal handleGetStrategyMaintenanceMargin(String strategyName) throws Exception {
+
+        Strategy strategy = getStrategyDao().findByName(strategyName);
+        if (strategy.isBase()) {
+            return getStrategyDao().getPortfolioMaintenanceMargin();
+        } else {
+            return strategy.getMaintenanceMargin();
+        }
+    }
+
+    protected BigDecimal handleGetStrategyNetLiqValue(String strategyName) throws Exception {
+
+        Strategy strategy = getStrategyDao().findByName(strategyName);
+        if (strategy.isBase()) {
+            return getStrategyDao().getPortfolioNetLiqValue();
+        } else {
+            return strategy.getNetLiqValue();
+        }
+    }
+
+    protected BigDecimal handleGetStrategyAvailableFunds(String strategyName) throws Exception {
+
+        Strategy strategy = getStrategyDao().findByName(strategyName);
+        if (strategy.isBase()) {
+            return getStrategyDao().getPortfolioAvailableFunds();
+        } else {
+            return strategy.getAvailableFunds();
+        }
+    }
+
+    protected double handleGetStrategyLeverage(String strategyName) throws Exception {
+
+        Strategy strategy = getStrategyDao().findByName(strategyName);
+        if (strategy.isBase()) {
+            return getStrategyDao().getPortfolioLeverageDouble();
+        } else {
+            return strategy.getLeverage();
+        }
+    }
+
+    protected BigDecimal handleGetStrategyUnderlaying(String strategyName) throws Exception {
+
+        Security underlaying = getStrategyDao().findByName(strategyName).getUnderlaying();
+        if (underlaying != null) {
+            Tick tick = underlaying.getLastTick();
+            if (tick != null) {
+                return tick.getLast();
+            }
+        }
+        return null;
+    }
+
+    protected BigDecimal handleGetStrategyVolatility(String strategyName) throws Exception {
+
+        Security underlaying = getStrategyDao().findByName(strategyName).getUnderlaying();
+        if (underlaying != null) {
+            Tick tick = underlaying.getVolatility().getLastTick();
+            if (tick != null) {
+                return tick.getLast();
+            }
+        }
+        return null;
+    }
+
+    @SuppressWarnings("unchecked")
+    protected List<PositionVO> handleGetDataOpenPositions(String strategyName) throws Exception {
+
+        Strategy strategy = getStrategyDao().findByName(strategyName);
+        if (strategy.isBase()) {
+            return getPositionDao().findOpenPositions(PositionDao.TRANSFORM_POSITIONVO);
+        } else {
+            return getPositionDao().findOpenPositionsByStrategy(PositionDao.TRANSFORM_POSITIONVO, strategyName);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    protected List<TransactionVO> handleGetDataTransactions(String strategyName) throws Exception {
+
+        Strategy strategy = getStrategyDao().findByName(strategyName);
+        if (strategy.isBase()) {
+            return getTransactionDao().findLastNTransactions(TransactionDao.TRANSFORM_TRANSACTIONVO, 10);
+        } else {
+            return getTransactionDao().findLastNTransactionsByStrategy(TransactionDao.TRANSFORM_TRANSACTIONVO, 10, strategyName);
+        }
+    }
+}
