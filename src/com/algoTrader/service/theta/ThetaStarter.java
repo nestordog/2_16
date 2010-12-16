b93b1286c08f60a5b8e2042b4c5c79bac90193df
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-12-16 14:35:59 +0000

    use ruleService.activateInit
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3458 19eef36c-86f0-de11-afab-0004ac968479

---
+++ src/com/algoTrader/service/theta/ThetaStarter.java
@@ -0,0 +1,39 @@
+package com.algoTrader.service.theta;
+
+import com.algoTrader.ServiceLocator;
+import com.algoTrader.service.RuleService;
+import com.algoTrader.service.StrategyService;
+import com.algoTrader.util.ConfigurationUtil;
+
+public class ThetaStarter {
+
+    public static void main(String[] args) {
+
+        start();
+    }
+
+    public static void start() {
+
+        String strategyName = ConfigurationUtil.getBaseConfig().getString("strategyName");
+
+        RuleService ruleService = ServiceLocator.commonInstance().getRuleService();
+        ThetaService thetaService = ServiceLocator.commonInstance().getThetaService();
+        StrategyService strategyService = ServiceLocator.commonInstance().getStrategyService();
+
+        ruleService.initServiceProvider(strategyName);
+
+        // run all the recorded ticks through to initialize MACD and stochastic
+        ruleService.activateInit(strategyName);
+        thetaService.prefeedTicks(strategyName);
+
+        // switch to internalClock
+        ruleService.setInternalClock(strategyName, true);
+        ruleService.enableManagement(strategyName);
+
+        //activate the rest of the rules
+        ruleService.activateAll(strategyName);
+
+        // register the strategy with BASE so we can receive events
+        strategyService.registerStrategy(strategyName);
+    }
+}
