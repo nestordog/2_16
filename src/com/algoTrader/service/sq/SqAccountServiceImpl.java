7b79757d5e6cce8870d3ab3e013806ca2096a42b
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-12-16 13:14:19 +0000

    Account -> Strategy
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3435 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/service/sq/SqAccountServiceImpl.java
+++ src/com/algoTrader/service/sq/SqAccountServiceImpl.java
@@ -1,17 +1,13 @@
 package com.algoTrader.service.sq;
 
-import com.algoTrader.entity.Account;
-import com.algoTrader.enumeration.Currency;
-import com.algoTrader.util.ConfigurationUtil;
+import com.algoTrader.entity.Strategy;
 
 public class SqAccountServiceImpl extends SqAccountServiceBase {
 
-    private static Currency currency = Currency.fromString(ConfigurationUtil.getBaseConfig().getString("strategie.currency"));
+    protected long handleGetNumberOfContractsByMargin(String strategyName, double initialMarginPerContract) {
 
-    protected long handleGetNumberOfContractsByMargin(double initialMarginPerContract) {
-
-        Account account = getAccountDao().findByCurrency(currency);
-        return (long) (account.getAvailableFundsDouble() / initialMarginPerContract);
+        Strategy strategy = getStrategyDao().findByName(strategyName);
+        return (long) (strategy.getAvailableFundsDouble() / initialMarginPerContract);
     }
 
     protected void handleProcessCashTransactions() throws Exception {
