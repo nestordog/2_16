41cfcdb830c3eb6e79e56a7f69c083a7e815220e
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-12-16 14:52:33 +0000

    IndicatorService -> ThetaManagementService add buySignal & sellSignal check strategyGroup THETA
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3463 19eef36c-86f0-de11-afab-0004ac968479

---
+++ src/com/algoTrader/service/theta/ThetaManagementServiceImpl.java
@@ -0,0 +1,62 @@
+package com.algoTrader.service.theta;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import com.algoTrader.entity.Security;
+import com.algoTrader.entity.Strategy;
+import com.algoTrader.entity.StrategyImpl;
+import com.algoTrader.util.StrategyUtil;
+import com.algoTrader.vo.MacdVO;
+import com.algoTrader.vo.StochasticVO;
+
+public class ThetaManagementServiceImpl extends ThetaManagementServiceBase {
+
+    @SuppressWarnings("unchecked")
+    protected List<MacdVO> handleGetMACD() throws Exception {
+
+        Strategy strategy = StrategyUtil.getStartedStrategy();
+        if (StrategyImpl.THETA.equals(strategy.getGroup())) {
+            return getRuleService().getAllEvents(strategy.getName(), "KEEP_MACD_VO");
+        } else {
+            throw new ThetaManagementServiceException("GetMACD can only be called from THETA strategy group");
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    protected List<StochasticVO> handleGetStochastic() throws Exception {
+
+        Strategy strategy = StrategyUtil.getStartedStrategy();
+        if (StrategyImpl.THETA.equals(strategy.getGroup())) {
+            return getRuleService().getAllEvents(strategy.getName(), "KEEP_STOCHASTIC_VO");
+        } else {
+            throw new ThetaManagementServiceException("GetStochastic can only be called from THETA strategy group");
+        }
+    }
+
+    protected void handleBuySignal() {
+
+        Strategy strategy = StrategyUtil.getStartedStrategy();
+        if (StrategyImpl.THETA.equals(strategy.getGroup())) {
+            Security underlaying = strategy.getUnderlaying();
+            BigDecimal underlayingSpot = underlaying.getLastTick().getCurrentValue();
+
+            getThetaService().buySignal(strategy.getName(), underlaying.getId(), underlayingSpot);
+        } else {
+            throw new ThetaManagementServiceException("BuySignal can only be called from THETA strategy group");
+        }
+    }
+
+    protected void handleSellSignal() {
+
+        Strategy strategy = StrategyUtil.getStartedStrategy();
+        if (StrategyImpl.THETA.equals(strategy.getGroup())) {
+            Security underlaying = strategy.getUnderlaying();
+            BigDecimal underlayingSpot = underlaying.getLastTick().getCurrentValue();
+
+            getThetaService().sellSignal(strategy.getName(), underlaying.getId(), underlayingSpot);
+        } else {
+            throw new ThetaManagementServiceException("SellSignal can only be called from THETA strategy group");
+        }
+    }
+}
