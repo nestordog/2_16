6279c2e23457d03776fd83d90607c8e969c7190a
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-12-16 13:52:58 +0000

    remove handleGetStockOptionsOnWatchlist
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3443 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/entity/StockOptionDaoImpl.java
+++ src/com/algoTrader/entity/StockOptionDaoImpl.java
@@ -1,18 +1,4 @@
 package com.algoTrader.entity;
 
-import java.util.List;
-
 public class StockOptionDaoImpl extends StockOptionDaoBase {
-
-    private List<StockOption> stockOptionsOnWatchlist; // cache stockOptionsOnWatchlist because it gets called very often
-
-    @SuppressWarnings("unchecked")
-    public List<StockOption> handleGetStockOptionsOnWatchlist(boolean forceReload) {
-
-        if (this.stockOptionsOnWatchlist == null || forceReload) {
-            this.stockOptionsOnWatchlist = super.findStockOptionsOnWatchlist();
-        }
-
-        return this.stockOptionsOnWatchlist;
-    }
 }
