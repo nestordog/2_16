465b4733b1bd04682a88205909719517d1961e99
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-12-16 13:51:59 +0000

    add isStrategyUnderlaying use RuleService for lastTick add getDummyBid / getDummyAsk
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3442 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/entity/SecurityImpl.java
+++ src/com/algoTrader/entity/SecurityImpl.java
@@ -1,56 +1,70 @@
 package com.algoTrader.entity;
 
-import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+
+import org.hibernate.Hibernate;
 
 import com.algoTrader.ServiceLocator;
-import com.algoTrader.enumeration.RuleName;
-import com.algoTrader.enumeration.TransactionType;
-import com.algoTrader.util.EsperService;
-import com.espertech.esper.client.EPStatement;
-import com.espertech.esper.client.EventBean;
-import com.espertech.esper.client.SafeIterator;
+import com.algoTrader.util.StrategyUtil;
+import com.espertech.esper.event.bean.BeanEventBean;
 
-public class SecurityImpl extends com.algoTrader.entity.Security {
+public class SecurityImpl extends Security {
 
     private static final long serialVersionUID = -6631052475125813394L;
 
+    public boolean isStrategyUnderlaying() {
+
+        return Hibernate.isInitialized(getStrategies()) && (getStrategies().size() != 0);
+    }
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
     public Tick getLastTick() {
 
-        // try to see if the rule GET_LAST_TICK has the tick
-        EPStatement statement = EsperService.getStatement(RuleName.GET_LAST_TICK);
-        if (statement != null && statement.isStarted()) {
+        List<Map> events = ServiceLocator.commonInstance().getRuleService().getAllEvents(StrategyUtil.getStartedStrategyName(), "GET_LAST_TICK");
 
-            SafeIterator<EventBean> it = statement.safeIterator();
-            try {
-                while (it.hasNext()) {
-                    EventBean bean = it.next();
-                    Integer securityId = (Integer) bean.get("securityId");
-                    if (securityId.equals(getId())) {
-                        return (Tick)bean.get("tick");
-                    }
-                }
-            } finally {
-                it.close();
+        // try to see if the rule GET_LAST_TICK has the tick
+        for (Map event : events) {
+            Integer securityId = (Integer) event.get("securityId");
+            if (securityId.equals(getId())) {
+                return (Tick) ((BeanEventBean) event.get("tick")).getUnderlying();
             }
         }
 
         // if we did not get the tick up to now go to the db an get the last tick
-        Tick tick = ServiceLocator.instance().getLookupService().getLastTick(getId());
+        Tick tick = ServiceLocator.commonInstance().getLookupService().getLastTick(getId());
         return tick;
     }
 
-    public boolean hasOpenPositions() {
-        return getPosition().isOpen();
+    /**
+     * spread depends on the pricePerContract (i.e. spread should be the same
+     * for 12.- à contractSize 10 as for 1.20 à contractSize 100)
+     *
+     * @return price per option
+     */
+    public double getDummyBid(double price) {
+
+        double pricePerContract = price * getSecurityFamily().getContractSize();
+        double spread = pricePerContract * getSecurityFamily().getSpreadSlope() + getSecurityFamily().getSpreadConstant();
+        return (pricePerContract - (spread / 2.0)) / getSecurityFamily().getContractSize();
     }
 
-    public BigDecimal getCommission(long quantity, TransactionType transactionType) {
+    /**
+     * spread depends on the pricePerContract (i.e. spread should be the same
+     * for 12.- à contractSize 10 as for 1.20 à contractSize 100)
+     *
+     * @return price per option
+     */
+    public double getDummyAsk(double price) {
 
-        return new BigDecimal(0);
+        double pricePerContract = price * getSecurityFamily().getContractSize();
+        double spread = pricePerContract * getSecurityFamily().getSpreadSlope() + getSecurityFamily().getSpreadConstant();
+        return (pricePerContract + (spread / 2.0)) / getSecurityFamily().getContractSize();
     }
 
-    public int getContractSize() {
+    public boolean isOnWatchlist() {
 
-        return 1;
+        return Hibernate.isInitialized(getWatchers()) && (getWatchers().size() != 0);
     }
 
     public void validateTick(Tick tick) {
