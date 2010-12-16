022bc6a51c1b4954b6e6bf3e433d94c04362616c
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-12-16 14:53:40 +0000

    source format / remove comments
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3466 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/service/HistoricalDataServiceImpl.java
+++ src/com/algoTrader/service/HistoricalDataServiceImpl.java
@@ -1,22 +1,5 @@
-// license-header java merge-point
-/**
- * This is only generated once! It will never be overwritten.
- * You can (and have to!) safely modify it by hand.
- */
 package com.algoTrader.service;
 
-/**
- * @see com.algoTrader.service.HistoricalDataService
- */
-public class HistoricalDataServiceImpl
-    extends com.algoTrader.service.HistoricalDataServiceBase
-{
-
-
-    @Override
-    protected void handleRequestHistoricalData(int[] securityIds, String[] whatToShow, String startDate, String endDate) throws Exception {
-        // TODO Auto-generated method stub
-
-    }
+public abstract class HistoricalDataServiceImpl extends HistoricalDataServiceBase {
 
 }
