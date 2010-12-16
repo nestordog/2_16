88fd69f908a8ca0ff2d266614114b8a1213179ea
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-12-16 13:09:58 +0000

    format source and remove comment
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3430 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/service/sq/SqHistoricalDataServiceImpl.java
+++ src/com/algoTrader/service/sq/SqHistoricalDataServiceImpl.java
@@ -1,15 +1,11 @@
-// license-header java merge-point
-/**
- * This is only generated once! It will never be overwritten.
- * You can (and have to!) safely modify it by hand.
- */
 package com.algoTrader.service.sq;
 
-/**
- * @see com.algoTrader.service.sq.SqHistoricalDataService
- */
-public class SqHistoricalDataServiceImpl
-    extends com.algoTrader.service.sq.SqHistoricalDataServiceBase
-{
+import javax.naming.OperationNotSupportedException;
 
+public class SqHistoricalDataServiceImpl extends SqHistoricalDataServiceBase {
+
+    protected void handleRequestHistoricalData(int[] securityIds, String[] whatToShow, String startDate, String endDate) throws Exception {
+
+        throw new OperationNotSupportedException("handleRequestHistoricalData not implemented yet");
+    }
 }
