853c9c9c1f588717f90505b003acb321704ed572
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-12-16 14:08:31 +0000

    ServiceLocator serverInstance add strategyName parameter
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3453 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/starter/SimulationStarter.java
+++ src/com/algoTrader/starter/SimulationStarter.java
@@ -16,49 +16,54 @@ public class SimulationStarter {
 
         if (args[0].equals("simulateByUnderlayings")) {
 
-            ServiceLocator.instance().getSimulationService().simulateByUnderlayings();
+            ServiceLocator.serverInstance().getSimulationService().simulateByUnderlayings();
         } else if (args[0].equals("simulateByActualOrders")) {
 
-            ServiceLocator.instance().getSimulationService().simulateByActualTransactions();
+            ServiceLocator.serverInstance().getSimulationService().simulateByActualTransactions();
         } else if (args[0].equals("optimizeSingle")) {
 
-            String[] parameters = new String[args.length -1];
-            double[] mins = new double[args.length -1];
-            double[] maxs = new double[args.length -1];
-            double[] accuracies = new double[args.length -1];
-            for (int i = 1; i < args.length; i++) {
+            String strategyName = args[0];
+            String[] parameters = new String[args.length - 2];
+            double[] mins = new double[args.length - 2];
+            double[] maxs = new double[args.length - 2];
+            double[] accuracies = new double[args.length - 2];
+            for (int i = 2; i < args.length; i++) {
                 String[] params = args[i].split(":");
-                parameters[i-1] = params[0];
-                mins[i-1] = Double.valueOf(params[1]);
-                maxs[i-1] = Double.valueOf(params[2]);
-                accuracies[i-1] = Double.valueOf(params[3]);
+                parameters[i - 2] = params[0];
+                mins[i - 2] = Double.valueOf(params[1]);
+                maxs[i - 2] = Double.valueOf(params[2]);
+                accuracies[i - 2] = Double.valueOf(params[3]);
             }
 
-            ServiceLocator.instance().getSimulationService().optimizeSingles(parameters, mins, maxs, accuracies);
+            ServiceLocator.serverInstance().getSimulationService().optimizeSingles(strategyName, parameters, mins, maxs, accuracies);
+
         } else if (args[0].equals("optimizeMulti")) {
 
-            String[] parameters = new String[args.length - 1];
-            double[] starts = new double[args.length - 1];
-            for (int i = 1; i < args.length; i++) {
+            String strategyName = args[0];
+            String[] parameters = new String[args.length - 2];
+            double[] starts = new double[args.length - 2];
+            for (int i = 2; i < args.length; i++) {
 
                 String[] params = args[i].split(":");
                 String parameter = params[0];
                 double start = Double.valueOf(params[1]);
-                parameters[i-1] = parameter;
-                starts[i-1] = start;
+                parameters[i - 2] = parameter;
+                starts[i - 2] = start;
             }
 
-            ServiceLocator.instance().getSimulationService().optimizeMulti(parameters, starts);
+            ServiceLocator.serverInstance().getSimulationService().optimizeMulti(strategyName, parameters, starts);
+
         } else if (args[0].equals("optimizeLinear")) {
 
-            for (int i = 1; i < args.length; i++) {
+            String strategyName = args[0];
+            for (int i = 2; i < args.length; i++) {
                 String[] params = args[i].split(":");
                 String parameter = params[0];
                 double min = Double.parseDouble(params[1]);
                 double max = Double.parseDouble(params[2]);
                 double increment = Double.parseDouble(params[3]);
 
-                ServiceLocator.instance().getSimulationService().optimizeLinear(parameter, min, max, increment);
+                ServiceLocator.serverInstance().getSimulationService().optimizeLinear(strategyName, parameter, min, max, increment);
             }
         } else {
             logger.info("please specify simulateByUnderlayings or simulateByActualOrders on the commandline");
