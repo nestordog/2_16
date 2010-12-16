09e3da22d3642a40cf255a203f5679c7ca803357
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-12-16 11:33:58 +0000

    AspectJ
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3414 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/service/LookupServiceImpl.java
+++ src/com/algoTrader/service/LookupServiceImpl.java
@@ -1,43 +1,89 @@
 package com.algoTrader.service;
 
-import com.algoTrader.entity.Account;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
 import com.algoTrader.entity.Position;
 import com.algoTrader.entity.Rule;
 import com.algoTrader.entity.Security;
+import com.algoTrader.entity.SecurityFamily;
 import com.algoTrader.entity.StockOption;
+import com.algoTrader.entity.StockOptionFamily;
+import com.algoTrader.entity.Strategy;
 import com.algoTrader.entity.Tick;
 import com.algoTrader.entity.Transaction;
 import com.algoTrader.vo.PortfolioValueVO;
 
 @SuppressWarnings("unchecked")
-public class LookupServiceImpl extends com.algoTrader.service.LookupServiceBase {
+public class LookupServiceImpl extends LookupServiceBase {
 
-    protected com.algoTrader.entity.Security handleGetSecurity(int id) throws java.lang.Exception {
+    protected Security handleGetSecurity(int id) throws java.lang.Exception {
 
         return getSecurityDao().load(id);
     }
 
+    protected Security handleGetSecurityByStrategyName(String strategyName) throws Exception {
+
+        return getSecurityDao().findByStrategyName(strategyName);
+    }
+
     protected Security handleGetSecurityByIsin(String isin) throws Exception {
 
-        return getSecurityByIsin(isin);
+        return getSecurityDao().findByIsin(isin);
     }
 
-    protected com.algoTrader.entity.Account handleGetAccount(int id) throws java.lang.Exception {
+    protected Security handleGetSecurityFetched(int stockOptionId) throws Exception {
 
-        return getAccountDao().load(id);
+        return getSecurityDao().findSecurityFetched(stockOptionId);
     }
 
-    protected com.algoTrader.entity.Position handleGetPosition(int id) throws java.lang.Exception {
+    protected StockOption handleGetNearestStockOption(int underlayingId, Date expirationDate, BigDecimal underlayingSpot, String optionTypeString) throws Exception {
+
+        return getStockOptionDao().findNearestStockOption(underlayingId, expirationDate, underlayingSpot, optionTypeString);
+    }
+
+    protected Strategy handleGetStrategy(int id) throws java.lang.Exception {
+
+        return getStrategyDao().load(id);
+    }
+
+    protected Strategy handleGetStrategyByName(String name) throws Exception {
+
+        return getStrategyDao().findByName(name);
+    }
+
+    protected Strategy handleGetStrategyByNameFetched(String name) throws Exception {
+
+        return getStrategyDao().findByNameFetched(name);
+    }
+
+    protected SecurityFamily handleGetSecurityFamily(int id) throws Exception {
+
+        return getSecurityFamilyDao().load(id);
+    }
+
+    protected StockOptionFamily handleGetStockOptionFamilyByUnderlaying(int id) throws Exception {
+
+        return getStockOptionFamilyDao().findByUnderlaying(id);
+    }
+
+    protected Position handleGetPosition(int id) throws java.lang.Exception {
 
         return getPositionDao().load(id);
     }
 
-    protected com.algoTrader.entity.Rule handleGetRule(int id) throws java.lang.Exception {
+    protected Position handleGetPositionFetched(int id) throws Exception {
+
+        return getPositionDao().findByIdFetched(id);
+    }
+
+    protected Rule handleGetRule(int id) throws java.lang.Exception {
 
         return getRuleDao().load(id);
     }
 
-    protected com.algoTrader.entity.Transaction handleGetTransaction(int id) throws java.lang.Exception {
+    protected Transaction handleGetTransaction(int id) throws java.lang.Exception {
 
         return getTransactionDao().load(id);
     }
@@ -47,9 +93,9 @@ public class LookupServiceImpl extends com.algoTrader.service.LookupServiceBase 
         return (Security[])getSecurityDao().loadAll().toArray(new Security[0]);
     }
 
-    protected Account[] handleGetAllAccounts() throws Exception {
+    protected Strategy[] handleGetAllStrategies() throws Exception {
 
-        return (Account[])getAccountDao().loadAll().toArray(new Account[0]);
+        return (Strategy[]) getStrategyDao().loadAll().toArray(new Strategy[0]);
     }
 
     protected Position[] handleGetAllPositions() throws Exception {
@@ -73,6 +119,11 @@ public class LookupServiceImpl extends com.algoTrader.service.LookupServiceBase 
         return (Transaction[])getTransactionDao().findAllTrades().toArray(new Transaction[0]);
     }
 
+    protected Transaction[] handleGetAllCashFlows() throws Exception {
+
+        return (Transaction[]) getTransactionDao().findAllCashflows().toArray(new Transaction[0]);
+    }
+
     protected Security[] handleGetAllSecuritiesInPortfolio() throws Exception {
 
         return (Security[])getSecurityDao().findSecuritesInPortfolio().toArray(new Security[0]);
@@ -80,7 +131,7 @@ public class LookupServiceImpl extends com.algoTrader.service.LookupServiceBase 
 
     protected StockOption[] handleGetStockOptionsOnWatchlist() throws Exception {
 
-        return (StockOption[])getStockOptionDao().getStockOptionsOnWatchlist(false).toArray(new StockOption[0]);
+        return (StockOption[]) getStockOptionDao().findStockOptionsOnWatchlist().toArray(new StockOption[0]);
     }
 
     protected Position[] handleGetOpenPositions() throws Exception {
@@ -91,21 +142,57 @@ public class LookupServiceImpl extends com.algoTrader.service.LookupServiceBase 
     protected PortfolioValueVO handleGetPortfolioValue() throws Exception {
 
         PortfolioValueVO portfolioValueVO = new PortfolioValueVO();
-        portfolioValueVO.setSecuritiesCurrentValue(getAccountDao().getSecuritiesCurrentValueAllAccountsDouble());
-        portfolioValueVO.setCashBalance(getAccountDao().getCashBalanceAllAccountsDouble());
-        portfolioValueVO.setMaintenanceMargin(getAccountDao().getMaintenanceMarginAllAccountsDouble());
-        portfolioValueVO.setNetLiqValue(getAccountDao().getNetLiqValueAllAccountsDouble());
+        portfolioValueVO.setSecuritiesCurrentValue(getStrategyDao().getPortfolioSecuritiesCurrentValueDouble());
+        portfolioValueVO.setCashBalance(getStrategyDao().getPortfolioCashBalanceDouble());
+        portfolioValueVO.setMaintenanceMargin(getStrategyDao().getPortfolioMaintenanceMarginDouble());
+        portfolioValueVO.setNetLiqValue(getStrategyDao().getPortfolioNetLiqValueDouble());
+        portfolioValueVO.setLeverage(getStrategyDao().getPortfolioLeverageDouble());
 
         return portfolioValueVO;
     }
 
-    protected boolean handleHasLastTicks() throws Exception {
-
-        return getTickDao().hasLastTicks();
-    }
-
     protected Tick handleGetLastTick(int securityId) throws Exception {
 
-        return (Tick) getTickDao().findLastNTicksForSecurity(1, securityId).get(0);
+        List<Tick> list = getTickDao().findLastNTicksForSecurity(securityId, 1);
+        if (list.size() > 0) {
+            return list.get(0);
+        } else {
+            return null;
+        }
+    }
+
+    protected List<Tick> handleGetPreFeedTicks(int securityId, int numberOfTicks) {
+
+        getTickDao().findLastNTicksForSecurity(securityId, numberOfTicks);
+        List<Integer> recentIds = getTickDao().findLastNTickIdsForSecurity(securityId, numberOfTicks);
+        List<Integer> ids = getTickDao().findEndOfDayTickIds(securityId, recentIds.get(0));
+
+        ids.addAll(recentIds);
+
+        return getTickDao().findByIdsFetched(ids);
+    }
+
+    protected Rule handleGetRuleByName(String name) throws Exception {
+
+        return getRuleDao().findByName(name);
+    }
+
+    protected List<Rule> handleGetAutoActivateRules(String strategyName) throws Exception {
+
+        return getRuleDao().findAutoActivateRules(strategyName);
+    }
+
+    protected List<Strategy> handleGetAutoActivateStrategies() throws Exception {
+
+        return getStrategyDao().findAutoActivateStrategies();
+    }
+
+    protected Object handleTest(Object object) throws Exception {
+
+        Security security = (Security) object;
+        getSecurityDao().update(security);
+        security.getIsin();
+
+        return null;
     }
 }
