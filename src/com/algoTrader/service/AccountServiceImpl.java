9972e4c9adb87a82daea87ab7c26f9bc0e7fe506
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2010-12-16 14:47:14 +0000

    add ProcessCashTransactionsListener
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3460 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/service/AccountServiceImpl.java
+++ src/com/algoTrader/service/AccountServiceImpl.java
@@ -1,5 +1,27 @@
 package com.algoTrader.service;
 
-public abstract class AccountServiceImpl extends com.algoTrader.service.AccountServiceBase {
+import org.apache.log4j.Logger;
 
+import com.algoTrader.ServiceLocator;
+import com.algoTrader.util.MyLogger;
+import com.espertech.esper.client.EventBean;
+import com.espertech.esper.client.UpdateListener;
+
+public abstract class AccountServiceImpl extends AccountServiceBase {
+
+    private static Logger logger = MyLogger.getLogger(AccountServiceImpl.class.getName());
+
+    public static class ProcessCashTransactionsListener implements UpdateListener {
+
+        public void update(EventBean[] newEvents, EventBean[] oldEvents) {
+
+            long startTime = System.currentTimeMillis();
+            logger.debug("retrieveTicks start");
+
+            ServiceLocator.serverInstance().getDispatcherService().getAccountService().processCashTransactions();
+
+            logger.debug("retrieveTicks end (" + (System.currentTimeMillis() - startTime) + "ms execution)");
+
+        }
+    }
 }
