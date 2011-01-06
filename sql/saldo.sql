dfff37ba13fd5c77e839c189656592ca404b9d17
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2011-01-06 17:44:12 +0000

    formating
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3507 19eef36c-86f0-de11-afab-0004ac968479

--- sql/saldo.sql
+++ sql/saldo.sql
@@ -1,16 +1,20 @@
-  select
-    t1.DATE_TIME AS date_time,
-    t1.TYPE AS type,
-    s1.SYMBOL AS symbol,
-    s1.ISIN AS isin,
-    s1.POSITION_FK AS position_fk,
-    o1.STRIKE AS STRIKE,
-    o1.EXPIRATION AS expiration,
-    t1.QUANTITY AS quantity,
-    t1.PRICE AS price,
-    t1.COMMISSION AS commission,
-    (select sum(case t2.TYPE when 'CREDIT' then t2.PRICE when 'DEBIT' then t2.PRICE else (-t2.QUANTITY * t2.PRICE - t2.COMMISSION) end) from transaction as t2 where t2.id <= t1.id) AS saldo
-  from transaction t1
-  left join security s1 on t1.SECURITY_FK = s1.id
-  left join stock_option o1 on o1.ID = s1.id
-  order by t1.id;
+CREATE OR REPLACE VIEW saldo AS
+SELECT
+  t1.DATE_TIME AS date_time,
+  t1.TYPE AS type,
+  s1.SYMBOL AS symbol,
+  s1.ISIN AS isin,
+  s1.POSITION_FK AS position_fk,
+  o1.STRIKE AS STRIKE,
+  o1.EXPIRATION AS expiration,
+  t1.QUANTITY AS quantity,
+  t1.PRICE AS price,
+  t1.COMMISSION AS commission,
+  (SELECT SUM(CASE t2.TYPE WHEN 'CREDIT' THEN t2.PRICE WHEN 'DEBIT' THEN t2.PRICE ELSE (-t2.QUANTITY * t2.PRICE - t2.COMMISSION) END)
+    FROM transaction AS t2
+    WHERE t2.id <= t1.id
+  ) AS saldo
+FROM transaction t1
+LEFT JOIN security s1 ON t1.SECURITY_FK = s1.id
+LEFT JOIN stock_option o1 ON o1.ID = s1.id
+ORDER BY t1.id;
