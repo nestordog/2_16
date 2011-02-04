a8c4dc753d8c6811d77e56e5ec46b2833a1578c5
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2011-02-04 19:01:35 +0000

    use jmx agent multiline arguments
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3622 19eef36c-86f0-de11-afab-0004ac968479

--- bin/run-base.sh
+++ bin/run-base.sh
@@ -7,4 +7,13 @@ for name in lib/*.jar ; do
   CP=$CP:$name
 done
 
-nohup java -cp $CP -DstrategyName=BASE -agentlib:jdwp=transport=dt_socket,suspend=n,server=y,address=localhost:8000 -Dcom.sun.management.jmxremote.port=1099 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false com.algoTrader.starter.TickStarter  > log/nohup.log 2>&1 &
+nohup java \
+-cp $CP \
+-DstrategyName=BASE \
+-Dcom.algoTrarder.rmi.registryPort=1099 \
+-Dcom.algoTrarder.rmi.serverPort=1098 \
+-Djava.rmi.server.hostname=127.0.0.1 \
+-javaagent:lib/agent.jar \
+-agentlib:jdwp=transport=dt_socket,suspend=n,server=y,address=localhost:8000
+com.algoTrader.starter.TickStarter \
+> log/nohup.log 2>&1 &
--- bin/run-smi.sh
+++ bin/run-smi.sh
@@ -7,4 +7,14 @@ for name in lib/*.jar ; do
   CP=$CP:$name
 done
 
-nohup java -cp $CP -DstrategyName=SMI -agentlib:jdwp=transport=dt_socket,suspend=n,server=y,address=localhost:8001 -Dcom.sun.management.jmxremote.port=1098 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -javaagent:lib/org.springframework.instrument-3.0.5.RELEASE.jar com.algoTrader.service.theta.ThetaStarter > log/nohup.log 2>&1 &
+nohup java \
+-cp $CP \
+-DstrategyName=SMI \
+-Dcom.algoTrarder.rmi.registryPort=1097 \
+-Dcom.algoTrarder.rmi.serverPort=1096 \
+-Djava.rmi.server.hostname=127.0.0.1 \
+-javaagent:lib/agent.jar \
+-javaagent:lib/org.springframework.instrument-3.0.5.RELEASE.jar \
+-agentlib:jdwp=transport=dt_socket,suspend=n,server=y,address=localhost:8001 \
+com.algoTrader.service.theta.ThetaStarter \
+> log/nohup.log 2>&1 &
