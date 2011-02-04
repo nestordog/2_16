e2d630d458b3a3595352a97fb2ced712b9c413b6
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2011-02-04 17:14:02 +0000

    add nohup
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3619 19eef36c-86f0-de11-afab-0004ac968479

--- bin/run-smi.sh
+++ bin/run-smi.sh
@@ -7,4 +7,4 @@ for name in lib/*.jar ; do
   CP=$CP:$name
 done
 
-java -cp $CP -DstrategyName=SMI -agentlib:jdwp=transport=dt_socket,suspend=n,server=y,address=localhost:8001 -Dcom.sun.management.jmxremote.port=1097 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -javaagent:lib/org.springframework.instrument-3.0.5.RELEASE.jar com.algoTrader.service.theta.ThetaStarter
+nohup java -cp $CP -DstrategyName=SMI -agentlib:jdwp=transport=dt_socket,suspend=n,server=y,address=localhost:8001 -Dcom.sun.management.jmxremote.port=1098 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -javaagent:lib/org.springframework.instrument-3.0.5.RELEASE.jar com.algoTrader.service.theta.ThetaStarter > log/nohup.log 2>&1 &
