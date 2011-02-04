51cb68fc0f8aaa16343ead114fc669ca71addfc1
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2011-02-04 11:41:39 +0000

    eol-style: native mime-type: text/plain
    run-simulation: remove dataSource
    
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3615 19eef36c-86f0-de11-afab-0004ac968479

--- run-simulation.sh
+++ run-simulation.sh
@@ -7,5 +7,5 @@ for name in lib/*.jar ; do
   CP=$CP:$name
 done
 
-java -cp $CP -Dsimulation=true -DdataSource.dataSet=1year -DdataSource.url=jdbc:mysql://127.0.0.1:3306/algotrader com.algoTrader.starter.SimulationStarter simulateWithCurrentParams
+java -cp $CP -Dsimulation=true -DdataSource.dataSet=1year com.algoTrader.starter.SimulationStarter simulateWithCurrentParams
 
