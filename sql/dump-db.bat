f1021aa9fac018f2c803c50de6e8f6e2fc0b0201
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2011-02-25 12:24:47 +0000

    remove dump rules
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3656 19eef36c-86f0-de11-afab-0004ac968479

--- sql/dump-db.bat
+++ sql/dump-db.bat
@@ -1,6 +1,2 @@
-"D:\MySQL Server 5.1\bin\mysqldump.exe" -X --hex-blob -t -u @localhost -r D:\AlgoTrader\sql\rules.xml algotrader rule
-"D:\MySQL Server 5.1\bin\mysqldump.exe" -t -u @localhost -r D:\AlgoTrader\sql\rules.sql algotrader rule
-"D:\AlgoTrader\sql\unix2dos.exe" D:\AlgoTrader\sql\rules.xml
-
 "D:\MySQL Server 5.1\bin\mysqldump.exe" -d -u @localhost -r D:\AlgoTrader\sql\db-structure.sql algotrader
 "D:\AlgoTrader\sql\unix2dos.exe" D:\AlgoTrader\sql\db-structure.sql
