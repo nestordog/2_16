cb0931e9f8f6b2b1e2fba926c9765e4097863554
(no author) <(no author)@19eef36c-86f0-de11-afab-0004ac968479>
2011-02-27 13:20:14 +0000

    does not extend IIbService any more add getAllConnectionStates
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3686 19eef36c-86f0-de11-afab-0004ac968479

--- src/com/algoTrader/service/ib/IbServiceImpl.java
+++ src/com/algoTrader/service/ib/IbServiceImpl.java
@@ -1,7 +1,13 @@
 package com.algoTrader.service.ib;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+
+import org.springframework.util.ClassUtils;
+
+import com.algoTrader.enumeration.ConnectionState;
 
 public class IbServiceImpl extends IbServiceBase {
 
@@ -15,9 +21,19 @@ public class IbServiceImpl extends IbServiceBase {
     }
 
     protected void handleConnect() {
+
         for (IIbService service : this.getAllIbServices()) {
             service.connect();
         }
+    }
+
+    protected Map<String, ConnectionState> handleGetAllConnectionStates() {
+
+        Map<String, ConnectionState> connectionStates = new HashMap<String, ConnectionState>();
+        for (IIbService service : this.getAllIbServices()) {
+            connectionStates.put(ClassUtils.getShortName(service.getClass().getInterfaces()[0]), service.getConnectionState());
+        }
+        return connectionStates;
     }
 
     private Set<IIbService> getAllIbServices() {
@@ -28,6 +44,7 @@ public class IbServiceImpl extends IbServiceBase {
             this.services.add(getIbTickService());
             this.services.add(getIbTransactionService());
             this.services.add(getIbAccountService());
+            this.services.add(getIbHistoricalDataService());
             // this.services.add(getIbStockOptionRetrieverService());
         }
 
