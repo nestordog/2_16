06e94dd6e1e5762c07f9b92e4974a48ec39d00db
aflury <aflury@19eef36c-86f0-de11-afab-0004ac968479>
2011-03-23 11:36:09 +0000

    multiline
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3742 19eef36c-86f0-de11-afab-0004ac968479

--- bin/run-expiredOptions.sh
+++ bin/run-expiredOptions.sh
@@ -7,9 +7,18 @@ for name in lib/*.jar ; do
   CP=$CP:$name
 done
 
-## java -cp $CP -DstrategyName=BASE -DdataSource.dataSet=expiredOptions -Dib.historicalDataServiceEnabled=true -Dib.accountServiceEnabled=false -Dib.stockOptionRetrieverServiceEnabled=false -Dib.tickServiceEnabled=false -Dib.transactionServiceEnabled=false -Dlog4j.configuration=log4j-console.xml com.algoTrader.starter.HistoricalDataStarter 20101018 20110119 BID:ASK 11087:11089:11091:11093:11095:11097:11099:11101:11103:11105
-## java -cp $CP -DstrategyName=BASE -DdataSource.dataSet=expiredOptions -Dib.historicalDataServiceEnabled=true -Dib.accountServiceEnabled=false -Dib.stockOptionRetrieverServiceEnabled=false -Dib.tickServiceEnabled=false -Dib.transactionServiceEnabled=false -Dlog4j.configuration=log4j-console.xml com.algoTrader.starter.HistoricalDataStarter 20101220 20110119 TRADES 4:5
+java -cp $CP \
+-DstrategyName=BASE \
+-DdataSource.dataSet=expiredOptions \
+-Dib.historicalDataServiceEnabled=true \
+-Dib.accountServiceEnabled=false \
+-Dib.stockOptionRetrieverServiceEnabled=false \
+-Dib.tickServiceEnabled=false \
+-Dib.transactionServiceEnabled=false \
+-Dlog4j.configuration=log4j-console.xml \
+com.algoTrader.starter.HistoricalDataStarter \
+20101215 20110317 BID:ASK 11658:11625:11627:11629:11631:11633:11635:11637:11639:11641:11643:11645:11647:11649:11651:11653:11655:11657:11659:11661:11663:11665:11667:11669:11671:11673
 
-## java -cp $CP -DstrategyName=BASE -DdataSource.dataSet=expiredOptions -Dib.historicalDataServiceEnabled=true -Dib.accountServiceEnabled=false -Dib.stockOptionRetrieverServiceEnabled=false -Dib.tickServiceEnabled=false -Dib.transactionServiceEnabled=false -Dlog4j.configuration=log4j-console.xml com.algoTrader.starter.HistoricalDataStarter 20101018 20110120 BID:ASK 11439:11441:11443:11445:11447:11449:11451:11453
-java -cp $CP -DstrategyName=BASE -DdataSource.dataSet=expiredOptions -Dib.historicalDataServiceEnabled=true -Dib.accountServiceEnabled=false -Dib.stockOptionRetrieverServiceEnabled=false -Dib.tickServiceEnabled=false -Dib.transactionServiceEnabled=false -Dlog4j.configuration=log4j-console.xml com.algoTrader.starter.HistoricalDataStarter 20101220 20110120 TRADES 2:3
+# 20110121 20110218 TRADES 2:3:4:5
+# 20101115 20110216 BID:ASK 11534:11536:11538
 
