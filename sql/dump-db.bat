bfc0471ec8db39ef246f86d35864614faae281bd
aflury <aflury@19eef36c-86f0-de11-afab-0004ac968479>
2011-04-21 14:01:27 +0000

    point to algotraderlive
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3842 19eef36c-86f0-de11-afab-0004ac968479

--- sql/dump-db.bat
+++ sql/dump-db.bat
@@ -1,2 +1,2 @@
-"D:\MySQL Server 5.1\bin\mysqldump.exe" -d -u @localhost -r D:\AlgoTrader\sql\db-structure.sql algotrader
+"D:\MySQL Server 5.1\bin\mysqldump.exe" -d -u @localhost -r D:\AlgoTrader\sql\db-structure.sql algotraderlive
 "D:\AlgoTrader\sql\unix2dos.exe" D:\AlgoTrader\sql\db-structure.sql
