ce687a6ec6d9dcec5a5551b6711f59bc92f75382
aflury <aflury@19eef36c-86f0-de11-afab-0004ac968479>
2011-04-27 13:39:45 +0000

    mvn classpath
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3904 19eef36c-86f0-de11-afab-0004ac968479

--- code/bin/run-base.sh
+++ code/bin/run-base.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
-cd $ALGOTRADER_HOME
+cd $ALGOTRADER_HOME/code
 
-CP=classes
-for name in lib/*.jar ; do
-  CP=$CP:$name
-done
+mvn -o -q dependency:build-classpath -Dmdep.outputFile=cp.txt
 
 nohup java \
--cp $CP \
+-cp `cat cp.txt` \
 -DstrategyName=BASE \
 -Dcom.algoTrarder.rmi.registryPort=1099 \
 -Dcom.algoTrarder.rmi.serverPort=1098 \
@@ -18,3 +15,4 @@ nohup java \
 -Xmx100m \
 com.algoTrader.starter.TickStarter \
 > log/nohup.log 2>&1 &
+
