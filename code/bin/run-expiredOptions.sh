bd9cb4642a885dc53194c5280a40381de5058c2b
aflury <aflury@19eef36c-86f0-de11-afab-0004ac968479>
2011-05-20 06:53:13 +0000

    remove nohup remove log4j-console
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@3965 19eef36c-86f0-de11-afab-0004ac968479

--- code/bin/run-expiredOptions.sh
+++ code/bin/run-expiredOptions.sh
@@ -6,7 +6,7 @@ mvn -o -q -f bin/pom.xml \
 dependency:build-classpath \
 -Dmdep.outputFile=cp.txt
 
-nohup java \
+java \
 -cp `cat bin/cp.txt` \
 -DstrategyName=BASE \
 -DdataSource.dataSet=expiredOptions \
@@ -15,7 +15,6 @@ nohup java \
 -Dib.stockOptionRetrieverServiceEnabled=false \
 -Dib.tickServiceEnabled=false \
 -Dib.transactionServiceEnabled=false \
--Dlog4j.configuration=log4j-console.xml \
 com.algoTrader.starter.HistoricalDataStarter \
 20101215 20110317 BID:ASK 11658:11625:11627:11629:11631:11633:11635:11637:11639:11641:11643:11645:11647:11649:11651:11653:11655:11657:11659:11661:11663:11665:11667:11669:11671:11673
 
--- code/bin/run-retrieveOptions.sh
+++ code/bin/run-retrieveOptions.sh
@@ -6,12 +6,10 @@ mvn -o -q -f bin/pom.xml \
 dependency:build-classpath \
 -Dmdep.outputFile=cp.txt
 
-nohup java \
+java \
 -cp `cat bin/cp.txt` \
 -DstrategyName=BASE \
 -DmarketChannel=SQ \
 -DdataSource.url=jdbc:mysql://127.0.0.1:3306/AlgoTrader \
--Dlog4j.configuration=log4j-console.xml \
--agentlib:jdwp=transport=dt_socket,suspend=n,server=y,address=localhost:8000 \
 com.algoTrader.starter.StockOptionRetrievalStarter \
 2 4
