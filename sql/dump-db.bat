c57f1a45656e056610aafa07a3d96aa88996a421
aflury <aflury@19eef36c-86f0-de11-afab-0004ac968479>
2011-06-24 13:39:32 +0000

    --skip-extended-insert
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@4102 19eef36c-86f0-de11-afab-0004ac968479

--- sql/dump-db.bat
+++ sql/dump-db.bat
@@ -1,5 +1,5 @@
 "D:\MySQL Server 5.1\bin\mysqldump.exe" -d -u @localhost -r D:\AlgoTrader\sql\db-structure.sql algotrader
-"D:\MySQL Server 5.1\bin\mysqldump.exe" -c -t -u @localhost -r D:\AlgoTrader\sql\db-data.sql algotrader
+"D:\MySQL Server 5.1\bin\mysqldump.exe" -c --skip-extended-insert -t -u @localhost -r D:\AlgoTrader\sql\db-data.sql algotrader
 
 "D:\AlgoTrader\sql\unix2dos.exe" D:\AlgoTrader\sql\db-structure.sql
 "D:\AlgoTrader\sql\unix2dos.exe" D:\AlgoTrader\sql\db-data.sql
