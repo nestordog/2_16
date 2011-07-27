2f0ecc5bd9ec1de307d077ecbdc14a0e939e1d9e
aflury <aflury@19eef36c-86f0-de11-afab-0004ac968479>
2011-07-27 18:27:33 +0000

    move sql to code
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@4218 19eef36c-86f0-de11-afab-0004ac968479

--- sql/IB-transactions.sql
+++
@@ -1,4 +0,0 @@
-select t.currency, s.symbol, t.date_time, t.quantity, t.price, t.commission, s.security_family_fk
-from transaction as t
-join security as s on t.security_fk = s.id
-order by s.security_family_fk, t.currency, t.date_time desc
