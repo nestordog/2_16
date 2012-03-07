169d4d14b5a6f4cfefcb64636325fdf0871709b6
aflury <aflury@19eef36c-86f0-de11-afab-0004ac968479>
2012-03-07 16:03:55 +0000

    move download1MinTicks to HistoricalDataService
    HistoricalDataService.getHistoricalBars mark as abstract
    
    Periodicity -> Period
    
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@4696 19eef36c-86f0-de11-afab-0004ac968479

--- core/src/main/java/com/algoTrader/service/HistoricalDataServiceImpl.java
+++ core/src/main/java/com/algoTrader/service/HistoricalDataServiceImpl.java
@@ -1,5 +1,158 @@
 package com.algoTrader.service;
 
+import java.math.BigDecimal;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
+import org.apache.log4j.Logger;
+
+import com.algoTrader.entity.marketData.Bar;
+import com.algoTrader.entity.marketData.Tick;
+import com.algoTrader.entity.marketData.TickImpl;
+import com.algoTrader.entity.security.Security;
+import com.algoTrader.enumeration.BarType;
+import com.algoTrader.enumeration.Period;
+import com.algoTrader.util.DateUtil;
+import com.algoTrader.util.MyLogger;
+import com.algoTrader.util.io.CsvTickWriter;
+
 public abstract class HistoricalDataServiceImpl extends HistoricalDataServiceBase {
+
+    private static Logger logger = MyLogger.getLogger(HistoricalDataServiceImpl.class.getName());
+
+    @Override
+    protected void handleDownload1MinTicks(int[] securityIds, BarType[] barTypes, Date startDate, Date endDate) throws Exception {
+
+        for (int securityId : securityIds) {
+
+            Security security = getSecurityDao().load(securityId);
+
+            CsvTickWriter writer = new CsvTickWriter(security.getIsin());
+
+            download1MinTicksForSecurity(security, barTypes, startDate, endDate, writer);
+
+            writer.close();
+        }
+    }
+
+    private void download1MinTicksForSecurity(Security security, BarType[] barTypes, Date startDate, Date endDate, CsvTickWriter writer) throws Exception {
+
+        GregorianCalendar cal = new GregorianCalendar();
+        cal.setTime(startDate);
+
+        Date date;
+        while ((date = cal.getTime()).compareTo(endDate) <= 0) {
+
+            if ((cal.get(Calendar.DAY_OF_WEEK) != Calendar.SUNDAY) && (cal.get(Calendar.DAY_OF_WEEK) != Calendar.MONDAY)) {
+
+                download1MinTicksForDate(security, date, barTypes, writer);
+            }
+            cal.add(Calendar.DAY_OF_YEAR, 1);
+        }
+    }
+
+    private void download1MinTicksForDate(Security security, Date date, BarType[] barTypes, CsvTickWriter writer) throws Exception {
+
+        TreeMap<Date, Tick> timeTickMap = new TreeMap<Date, Tick>();
+
+        // run all barTypes and get the ticks
+        for (BarType barType : barTypes) {
+
+            List<Bar> bars = getHistoricalBars(security.getId(), date, 1, Period.DAY, 1, Period.MINUTE, barType);
+
+            // filter Bars by date
+            for (Iterator<Bar> it = bars.iterator(); it.hasNext();) {
+
+                Bar bar = it.next();
+
+                // retrieve bars only between marketOpen & close
+                if (DateUtil.compareTime(bar.getDateTime(), security.getSecurityFamily().getMarketClose()) > 0
+                        || DateUtil.compareTime(bar.getDateTime(), security.getSecurityFamily().getMarketOpen()) < 0) {
+
+                    it.remove();
+                    continue;
+                }
+
+                // only accept bars within the last 24h
+                if (date.getTime() - bar.getDateTime().getTime() > 86400000) {
+
+                    it.remove();
+                    continue;
+                }
+            }
+
+            // to make sure we don't get a pacing error
+            Thread.sleep(10000);
+
+            for (Bar bar : bars) {
+
+                Tick tick = timeTickMap.get(bar.getDateTime());
+
+                if (tick == null) {
+
+                    tick = new TickImpl();
+                    tick.setSecurity(security);
+                    tick.setLast(new BigDecimal(0));
+                    tick.setDateTime(bar.getDateTime());
+                    tick.setVol(0);
+                    tick.setVolBid(0);
+                    tick.setVolAsk(0);
+                    tick.setBid(new BigDecimal(0));
+                    tick.setAsk(new BigDecimal(0));
+                    tick.setOpenIntrest(0);
+                    tick.setSettlement(new BigDecimal(0));
+
+                    timeTickMap.put(bar.getDateTime(), tick);
+                }
+
+                if (BarType.TRADES.equals(barType)) {
+                    Entry<Date, Tick> entry = timeTickMap.lowerEntry(bar.getDateTime());
+                    if (entry != null) {
+                        Tick lastTick = entry.getValue();
+                        if (bar.getVol() > 0) {
+                            tick.setVol(lastTick.getVol() + bar.getVol());
+                            tick.setLastDateTime(bar.getDateTime());
+                        } else {
+                            tick.setVol(lastTick.getVol());
+                            tick.setLastDateTime(lastTick.getLastDateTime());
+                        }
+                    } else {
+                        if (bar.getVol() > 0) {
+                            tick.setVol(bar.getVol());
+                            tick.setLastDateTime(bar.getDateTime());
+                        }
+                    }
+                    BigDecimal last = bar.getClose();
+                    tick.setLast(last);
+                } else if (BarType.BID.equals(barType)) {
+                    BigDecimal bid = bar.getClose();
+                    tick.setBid(bid);
+                } else if (BarType.ASK.equals(barType)) {
+                    BigDecimal ask = bar.getClose();
+                    tick.setAsk(ask);
+                } else if (BarType.BID_ASK.equals(barType)) {
+                    BigDecimal bid = bar.getOpen();
+                    tick.setBid(bid);
+                    BigDecimal ask = bar.getClose();
+                    tick.setAsk(ask);
+                }
+            }
+        }
+
+        // write the ticks to the csvWriter
+        for (Map.Entry<Date, Tick> entry : timeTickMap.entrySet()) {
+
+            Tick tick = entry.getValue();
+            writer.write(tick);
+        }
+
+        logger.debug("wrote " + timeTickMap.entrySet().size() + " ticks for: " + security.getSymbol() + " on date: " + date);
+    }
 
 }
--- core/src/main/java/com/algoTrader/service/ib/IBHistoricalDataServiceImpl.java
+++ core/src/main/java/com/algoTrader/service/ib/IBHistoricalDataServiceImpl.java
@@ -1,16 +1,9 @@
 package com.algoTrader.service.ib;
 
-import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.TreeMap;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -20,17 +13,13 @@ import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.annotation.Value;
 
 import com.algoTrader.entity.marketData.Bar;
-import com.algoTrader.entity.marketData.Tick;
-import com.algoTrader.entity.marketData.TickImpl;
 import com.algoTrader.entity.security.Security;
 import com.algoTrader.enumeration.BarType;
 import com.algoTrader.enumeration.ConnectionState;
 import com.algoTrader.enumeration.Period;
 import com.algoTrader.service.HistoricalDataServiceException;
-import com.algoTrader.util.DateUtil;
 import com.algoTrader.util.MyLogger;
 import com.algoTrader.util.RoundUtil;
-import com.algoTrader.util.io.CsvTickWriter;
 import com.ib.client.Contract;
 
 public class IBHistoricalDataServiceImpl extends IBHistoricalDataServiceBase implements DisposableBean {
@@ -259,135 +248,6 @@ public class IBHistoricalDataServiceImpl extends IBHistoricalDataServiceBase imp
         }
 
         return this.barList;
-    }
-
-    @Override
-    protected void handleDownload1MinTicks(int[] securityIds, BarType[] barTypes, Date startDate, Date endDate) throws Exception {
-
-        for (int securityId : securityIds) {
-
-            Security security = getSecurityDao().load(securityId);
-
-            CsvTickWriter writer = new CsvTickWriter(security.getIsin());
-
-            download1MinTicksForSecurity(security, barTypes, startDate, endDate, writer);
-
-            writer.close();
-        }
-    }
-
-    private void download1MinTicksForSecurity(Security security, BarType[] barTypes, Date startDate, Date endDate, CsvTickWriter writer) throws Exception {
-
-        GregorianCalendar cal = new GregorianCalendar();
-        cal.setTime(startDate);
-
-        Date date;
-        while ((date = cal.getTime()).compareTo(endDate) <= 0) {
-
-            if ((cal.get(Calendar.DAY_OF_WEEK) != Calendar.SUNDAY) && (cal.get(Calendar.DAY_OF_WEEK) != Calendar.MONDAY)) {
-
-                download1MinTicksForDate(security, date, barTypes, writer);
-            }
-            cal.add(Calendar.DAY_OF_YEAR, 1);
-        }
-    }
-
-    private void download1MinTicksForDate(Security security, Date date, BarType[] barTypes, CsvTickWriter writer) throws Exception {
-
-        TreeMap<Date, Tick> timeTickMap = new TreeMap<Date, Tick>();
-
-        // run all barTypes and get the ticks
-        for (BarType barType : barTypes) {
-
-            List<Bar> bars = getHistoricalBars(security.getId(), date, 1, Period.DAY, 1, Period.MINUTE, barType);
-
-            // filter Bars by date
-            for (Iterator<Bar> it = this.barList.iterator(); it.hasNext();) {
-
-                Bar bar = it.next();
-
-                // retrieve bars only between marketOpen & close
-                if (DateUtil.compareTime(bar.getDateTime(), security.getSecurityFamily().getMarketClose()) > 0
-                        || DateUtil.compareTime(bar.getDateTime(), security.getSecurityFamily().getMarketOpen()) < 0) {
-
-                    it.remove();
-                    continue;
-                }
-
-                // only accept bars within the last 24h
-                if (date.getTime() - bar.getDateTime().getTime() > 86400000) {
-
-                    it.remove();
-                    continue;
-                }
-            }
-
-            // to make sure we don't get a pacing error
-            Thread.sleep(10000);
-
-            for (Bar bar : bars) {
-
-                Tick tick = timeTickMap.get(bar.getDateTime());
-
-                if (tick == null) {
-
-                    tick = new TickImpl();
-                    tick.setSecurity(security);
-                    tick.setLast(new BigDecimal(0));
-                    tick.setDateTime(bar.getDateTime());
-                    tick.setVol(0);
-                    tick.setVolBid(0);
-                    tick.setVolAsk(0);
-                    tick.setBid(new BigDecimal(0));
-                    tick.setAsk(new BigDecimal(0));
-                    tick.setOpenIntrest(0);
-                    tick.setSettlement(new BigDecimal(0));
-
-                    timeTickMap.put(bar.getDateTime(), tick);
-                }
-
-                if (BarType.TRADES.equals(barType)) {
-                    Entry<Date, Tick> entry = timeTickMap.lowerEntry(bar.getDateTime());
-                    if (entry != null) {
-                        Tick lastTick = entry.getValue();
-                        if (bar.getVol() > 0) {
-                            tick.setVol(lastTick.getVol() + bar.getVol());
-                            tick.setLastDateTime(bar.getDateTime());
-                        } else {
-                            tick.setVol(lastTick.getVol());
-                            tick.setLastDateTime(lastTick.getLastDateTime());
-                        }
-                    } else {
-                        if (bar.getVol() > 0) {
-                            tick.setVol(bar.getVol());
-                            tick.setLastDateTime(bar.getDateTime());
-                        }
-                    }
-                    BigDecimal last = bar.getClose();
-                    tick.setLast(last);
-                } else if (BarType.BID.equals(barType)) {
-                    BigDecimal bid = bar.getClose();
-                    tick.setBid(bid);
-                } else if (BarType.ASK.equals(barType)) {
-                    BigDecimal ask = bar.getClose();
-                    tick.setAsk(ask);
-                } else if (BarType.BID_ASK.equals(barType)) {
-                    BigDecimal bid = bar.getOpen();
-                    tick.setBid(bid);
-                    BigDecimal ask = bar.getClose();
-                    tick.setAsk(ask);
-                }
-            }
-        }
-
-        // write the ticks to the csvWriter
-        for (Map.Entry<Date, Tick> entry : timeTickMap.entrySet()) {
-
-            Tick tick = entry.getValue();
-            writer.write(tick);
-        }
-
-        logger.debug("wrote " + timeTickMap.entrySet().size() + " ticks for: " + security.getSymbol() + " on date: " + date);
     }
 
     @Override
