cec11c9165de0e9bb1c6b6ee5fbcfc868d3ed263
aflury <aflury@19eef36c-86f0-de11-afab-0004ac968479>
2012-05-23 19:46:13 +0000

    db-structure: security: add key SYMBOLE_UNIQUE security: add key ISIN_UNIQUE stategy: add key NAME_UNIQUE
    db-data:
    remove table property
    remove security CMFVIX
    security_family: update market open / close
    
    db-triggers: update entire file
    
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@4896 19eef36c-86f0-de11-afab-0004ac968479

--- core/sql/db-triggers.sql
+++ core/sql/db-triggers.sql
@@ -1,37 +1,30 @@
-
-      /*
-      * Script created by Quest Schema Compare at 22.11.2011 12:55:28.
-      * Please back up your database before running this script.
-      *
-      * Synchronizing objects from algotradertest to algotrader.
-      */
-    USE `algotrader`;
-
-
-/* Header line. Object: allocation_after_update. Script date: 22.11.2011 12:55:28. */
-CREATE TRIGGER `component_after_update`
-  AFTER UPDATE ON `component`
-  FOR EACH ROW BEGIN
-     IF NOT NEW.QUANTITY = OLD.QUANTITY OR (NEW.QUANTITY IS NULL XOR OLD.QUANTITY IS NULL) THEN
-        INSERT INTO history (TBL, REF_ID, TIME, COL, VALUE)
-        VALUES ('component', NEW.id, NOW(), 'QUANTITY', NEW.QUANTITY);
-     END IF;
-END;
-
-/* Header line. Object: cash_balance_after_update. Script date: 22.11.2011 12:55:28. */
+/* Header line. Object: cash_balance_after_update. Script date: 23.05.2012 20:55:33. */
 CREATE TRIGGER `cash_balance_after_update`
   AFTER UPDATE ON `cash_balance`
-  FOR EACH ROW BEGIN
+  FOR EACH ROW
+BEGIN
      IF NOT NEW.AMOUNT = OLD.AMOUNT OR (NEW.AMOUNT IS NULL XOR OLD.AMOUNT IS NULL) THEN
         INSERT INTO history (TBL, REF_ID, TIME, COL, VALUE)
         VALUES ('cash_balance', NEW.id, NOW(), 'AMOUNT', NEW.AMOUNT);
      END IF;
 END;
 
-/* Header line. Object: position_after_update. Script date: 22.11.2011 12:55:28. */
+/* Header line. Object: component_after_update. Script date: 23.05.2012 20:55:33. */
+CREATE TRIGGER `component_after_update`
+  AFTER UPDATE ON `component`
+  FOR EACH ROW
+BEGIN
+     IF NOT NEW.QUANTITY = OLD.QUANTITY OR (NEW.QUANTITY IS NULL XOR OLD.QUANTITY IS NULL) THEN
+        INSERT INTO history (TBL, REF_ID, TIME, COL, VALUE)
+        VALUES ('component', NEW.id, NOW(), 'QUANTITY', NEW.QUANTITY);
+     END IF;
+END;
+
+/* Header line. Object: position_after_update. Script date: 23.05.2012 20:55:33. */
 CREATE TRIGGER `position_after_update`
   AFTER UPDATE ON `position`
-  FOR EACH ROW BEGIN
+  FOR EACH ROW
+BEGIN
      IF NOT NEW.QUANTITY = OLD.QUANTITY OR (NEW.QUANTITY IS NULL XOR OLD.QUANTITY IS NULL) THEN
         INSERT INTO history (TBL, REF_ID, TIME, COL, VALUE)
         VALUES ('position', NEW.id, NOW(), 'QUANTITY', NEW.QUANTITY);
@@ -40,36 +33,50 @@ CREATE TRIGGER `position_after_update`
         INSERT INTO history (TBL, REF_ID, TIME, COL, VALUE)
         VALUES ('position', NEW.id, NOW(), 'EXIT_VALUE', NEW.EXIT_VALUE);
      END IF;
-     IF NOT NEW.PROFIT_TARGET = OLD.PROFIT_TARGET OR (NEW.PROFIT_TARGET IS NULL XOR OLD.PROFIT_TARGET IS NULL) THEN
-        INSERT INTO history (TBL, REF_ID, TIME, COL, VALUE)
-        VALUES ('position', NEW.id, NOW(), 'PROFIT_TARGET', NEW.PROFIT_TARGET);
-     END IF;
      IF NOT NEW.MAINTENANCE_MARGIN = OLD.MAINTENANCE_MARGIN OR (NEW.MAINTENANCE_MARGIN IS NULL XOR OLD.MAINTENANCE_MARGIN IS NULL) THEN
         INSERT INTO history (TBL, REF_ID, TIME, COL, VALUE)
         VALUES ('position', NEW.id, NOW(), 'MAINTENANCE_MARGIN', NEW.MAINTENANCE_MARGIN);
      END IF;
 END;
 
-/* Header line. Object: strategy_after_update. Script date: 22.11.2011 12:55:28. */
-CREATE TRIGGER `strategy_after_update`
-  AFTER UPDATE ON `strategy`
-  FOR EACH ROW BEGIN
-     IF NOT NEW.ALLOCATION = OLD.ALLOCATION OR (NEW.ALLOCATION IS NULL XOR OLD.ALLOCATION IS NULL) THEN
+/* Header line. Object: property_after_upd_tr. Script date: 23.05.2012 20:55:33. */
+CREATE TRIGGER `property_after_update`
+  AFTER UPDATE ON `property`
+  FOR EACH ROW
+BEGIN
+     IF NOT NEW.INT_VALUE = OLD.INT_VALUE OR (NEW.INT_VALUE IS NULL XOR OLD.INT_VALUE IS NULL) THEN
         INSERT INTO history (TBL, REF_ID, TIME, COL, VALUE)
-        VALUES ('strategy', NEW.id, NOW(), 'ALLOCATION', NEW.ALLOCATION);
+        VALUES ('property', NEW.id, NOW(), 'INT_VALUE', NEW.INT_VALUE);
+     END IF;
+     IF NOT NEW.DOUBLE_VALUE = OLD.DOUBLE_VALUE OR (NEW.DOUBLE_VALUE IS NULL XOR OLD.DOUBLE_VALUE IS NULL) THEN
+        INSERT INTO history (TBL, REF_ID, TIME, COL, VALUE)
+        VALUES ('property', NEW.id, NOW(), 'DOUBLE_VALUE', NEW.DOUBLE_VALUE);
+     END IF;
+     IF NOT NEW.MONEY_VALUE = OLD.MONEY_VALUE OR (NEW.MONEY_VALUE IS NULL XOR OLD.MONEY_VALUE IS NULL) THEN
+        INSERT INTO history (TBL, REF_ID, TIME, COL, VALUE)
+        VALUES ('property', NEW.id, NOW(), 'MONEY_VALUE', NEW.MONEY_VALUE);
+     END IF;
+     IF NOT NEW.TEXT_VALUE = OLD.TEXT_VALUE OR (NEW.TEXT_VALUE IS NULL XOR OLD.TEXT_VALUE IS NULL) THEN
+        INSERT INTO history (TBL, REF_ID, TIME, COL, VALUE)
+        VALUES ('property', NEW.id, NOW(), 'TEXT_VALUE', NEW.TEXT_VALUE);
+     END IF;
+     IF NOT NEW.DATE_VALUE = OLD.DATE_VALUE OR (NEW.DATE_VALUE IS NULL XOR OLD.DATE_VALUE IS NULL) THEN
+        INSERT INTO history (TBL, REF_ID, TIME, COL, VALUE)
+        VALUES ('property', NEW.id, NOW(), 'DATE_VALUE', NEW.DATE_VALUE);
+     END IF;
+     IF NOT NEW.BOOLEAN_VALUE = OLD.BOOLEAN_VALUE OR (NEW.BOOLEAN_VALUE IS NULL XOR OLD.BOOLEAN_VALUE IS NULL) THEN
+        INSERT INTO history (TBL, REF_ID, TIME, COL, VALUE)
+        VALUES ('property', NEW.id, NOW(), 'BOOLEAN_VALUE', NEW.BOOLEAN_VALUE);
      END IF;
 END;
 
-/* Header line. Object: watch_list_item_after_update. Script date: 22.11.2011 12:55:28. */
-CREATE TRIGGER `subscription_after_update`
-  AFTER UPDATE ON `subscription`
-  FOR EACH ROW BEGIN
-     IF NOT NEW.UPPER_ALERT_VALUE = OLD.UPPER_ALERT_VALUE OR (NEW.UPPER_ALERT_VALUE IS NULL XOR OLD.UPPER_ALERT_VALUE IS NULL) THEN
+/* Header line. Object: strategy_after_update. Script date: 23.05.2012 20:55:33. */
+CREATE TRIGGER `strategy_after_update`
+  AFTER UPDATE ON `strategy`
+  FOR EACH ROW
+BEGIN
+     IF NOT NEW.ALLOCATION = OLD.ALLOCATION OR (NEW.ALLOCATION IS NULL XOR OLD.ALLOCATION IS NULL) THEN
         INSERT INTO history (TBL, REF_ID, TIME, COL, VALUE)
-        VALUES ('subscription', NEW.id, NOW(), 'UPPER_ALERT_VALUE', NEW.UPPER_ALERT_VALUE);
-     END IF;
-     IF NOT NEW.LOWER_ALERT_VALUE = OLD.LOWER_ALERT_VALUE OR (NEW.LOWER_ALERT_VALUE IS NULL XOR OLD.LOWER_ALERT_VALUE IS NULL) THEN
-        INSERT INTO history (TBL, REF_ID, TIME, COL, VALUE)
-        VALUES ('subscription', NEW.id, NOW(), 'LOWER_ALERT_VALUE', NEW.LOWER_ALERT_VALUE);
+        VALUES ('strategy', NEW.id, NOW(), 'ALLOCATION', NEW.ALLOCATION);
      END IF;
 END;
