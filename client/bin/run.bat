b1bc0c491f3b043a9c1cb05278d6b92df822591e
aflury <aflury@19eef36c-86f0-de11-afab-0004ac968479>
2012-06-21 09:25:57 +0000

    use ssl
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@5026 19eef36c-86f0-de11-afab-0004ac968479

--- client/bin/run.bat
+++ client/bin/run.bat
@@ -7,9 +7,14 @@ start javaw.exe ^
 %ALGOTRADER_CLIENT_HOME%\lib\indicator-plugin.jar;^
 %JAVA_HOME%\lib\jconsole.jar;^
 %JAVA_HOME%\lib\tools.jar ^
+-Djavax.net.ssl.keyStore=%ALGOTRADER_CLIENT_HOME%/bin/keystore ^
+-Djavax.net.ssl.keyStorePassword=Zermatt11 ^
+-Djavax.net.ssl.trustStore=%ALGOTRADER_CLIENT_HOME%/bin/truststore ^
+-Djavax.net.ssl.trustStorePassword=Zermatt11 ^
 sun.tools.jconsole.JConsole ^
 -interval=60 ^
 -debug ^
-localhost:1099 ^
-localhost:1097 ^
-localhost:1095
+quant.linardcapital.com:1099 ^
+quant.linardcapital.com:1097 ^
+vola.linardcapital.com:1099 ^
+vola.linardcapital.com:1097
--- core/bin/run-base.sh
+++ core/bin/run-base.sh
@@ -8,6 +8,14 @@ nohup java \
 -Dcom.algoTrarder.rmi.registryPort=1099 \
 -Dcom.algoTrarder.rmi.serverPort=1098 \
 -Djava.rmi.server.hostname=127.0.0.1 \
+-Dcom.sun.management.jmxremote.authenticate=false \
+-Dcom.sun.management.jmxremote.ssl=true \
+-Dcom.sun.management.jmxremote.ssl.need.client.auth=true \
+-Dcom.sun.management.jmxremote.registry.ssl=true \
+-Djavax.net.ssl.keyStore=$ALGOTRADER_HOME/bin/keystore \
+-Djavax.net.ssl.keyStorePassword=Zermatt11 \
+-Djavax.net.ssl.trustStore=$ALGOTRADER_HOME/bin/truststore \
+-Djavax.net.ssl.trustStorePassword=Zermatt11 \
 -Dlog4j.configuration=log4j-prod.xml \
 -Denvironment=$ENVIRONMENT \
 -javaagent:lib/agent.jar \
