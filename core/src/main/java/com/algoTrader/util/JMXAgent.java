030ea4b56db7809122aa0aa5c7a12febff2b31e7
aflury <aflury@19eef36c-86f0-de11-afab-0004ac968479>
2012-06-26 16:34:55 +0000

    JMXAgent add SSL support
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@5053 19eef36c-86f0-de11-afab-0004ac968479

--- core/src/main/java/com/algoTrader/util/JMXAgent.java
+++ core/src/main/java/com/algoTrader/util/JMXAgent.java
@@ -10,6 +10,9 @@ import javax.management.MBeanServer;
 import javax.management.remote.JMXConnectorServer;
 import javax.management.remote.JMXConnectorServerFactory;
 import javax.management.remote.JMXServiceURL;
+import javax.management.remote.rmi.RMIConnectorServer;
+import javax.rmi.ssl.SslRMIClientSocketFactory;
+import javax.rmi.ssl.SslRMIServerSocketFactory;
 
 public class JMXAgent {
 
@@ -21,13 +24,10 @@ public class JMXAgent {
 
     public static void premain(String agentArgs) throws Throwable {
 
-        final int rmiRegistryPort = Integer.parseInt(System.getProperty(RMI_REGISTRY_PORT, "44444"));
+        final int rmiRegistryPort = Integer.parseInt(System.getProperty(RMI_REGISTRY_PORT, "1099"));
         final int rmiServerPort = Integer.parseInt(System.getProperty(RMI_SERVER_CONNECTION_PORT, (rmiRegistryPort + 1) + ""));
         final String hostname = InetAddress.getLocalHost().getHostName();
         final String publicHostName = System.getProperty("java.rmi.server.hostname", hostname);
-
-        System.out.println(RMI_REGISTRY_PORT + ":" + rmiRegistryPort);
-        System.out.println(RMI_SERVER_CONNECTION_PORT + ":" + rmiServerPort);
 
         // Ensure cryptographically strong random number generator used
         // to choose the object number - see java.rmi.server.ObjID
@@ -38,18 +38,26 @@ public class JMXAgent {
         MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
 
         Map<String, Object> env = new HashMap<String, Object>();
+        // Provide SSL-based RMI socket factories.
+        //
+        // The protocol and cipher suites to be enabled will be the ones
+        // defined by the default JSSE implementation and only server
+        // authentication will be required.
+        //
+        SslRMIClientSocketFactory csf = new SslRMIClientSocketFactory();
+        SslRMIServerSocketFactory ssf = new SslRMIServerSocketFactory();
+        env.put(RMIConnectorServer.RMI_CLIENT_SOCKET_FACTORY_ATTRIBUTE, csf);
+        env.put(RMIConnectorServer.RMI_SERVER_SOCKET_FACTORY_ATTRIBUTE, ssf);
 
-        JMXServiceURL url = new JMXServiceURL("service:jmx:rmi://" + hostname + ":" + rmiServerPort + "/jndi/rmi://" + hostname + ":" + rmiRegistryPort
-                + "/jmxrmi");
+        JMXServiceURL privateUrl = new JMXServiceURL("service:jmx:rmi://" + hostname + ":" + rmiServerPort + "/jndi/rmi://" + hostname + ":" + rmiRegistryPort + "/jmxrmi");
 
         // Used only to display what the public address should be
-        JMXServiceURL publicUrl = new JMXServiceURL("service:jmx:rmi://" + publicHostName + ":" + rmiServerPort + "/jndi/rmi://" + publicHostName + ":"
-                + rmiRegistryPort + "/jmxrmi");
+        JMXServiceURL publicUrl = new JMXServiceURL("service:jmx:rmi://" + publicHostName + ":" + rmiServerPort + "/jndi/rmi://" + publicHostName + ":" + rmiRegistryPort + "/jmxrmi");
 
-        System.out.println("Local Connection URL: " + url);
+        System.out.println("Local Connection URL: " + privateUrl);
         System.out.println("Public Connection URL: " + publicUrl);
 
-        JMXConnectorServer cs = JMXConnectorServerFactory.newJMXConnectorServer(url, env, mbs);
+        JMXConnectorServer cs = JMXConnectorServerFactory.newJMXConnectorServer(privateUrl, env, mbs);
         cs.start();
     }
 }
