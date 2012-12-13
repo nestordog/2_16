77d1bb3dc10e1f80d9c72b5cb7fad179b732f528
aflury <aflury@19eef36c-86f0-de11-afab-0004ac968479>
2012-12-13 11:23:19 +0000

    forex service: improve rolling
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@5479 19eef36c-86f0-de11-afab-0004ac968479

--- core/src/main/java/com/algoTrader/service/ForexServiceImpl.java
+++ core/src/main/java/com/algoTrader/service/ForexServiceImpl.java
@@ -10,7 +10,6 @@ import org.apache.commons.math.util.MathUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 
-import com.algoTrader.ServiceLocator;
 import com.algoTrader.entity.Position;
 import com.algoTrader.entity.Strategy;
 import com.algoTrader.entity.StrategyImpl;
@@ -21,11 +20,8 @@ import com.algoTrader.entity.security.Future;
 import com.algoTrader.entity.security.FutureFamily;
 import com.algoTrader.entity.trade.MarketOrder;
 import com.algoTrader.entity.trade.Order;
-import com.algoTrader.entity.trade.OrderStatus;
 import com.algoTrader.enumeration.Currency;
 import com.algoTrader.enumeration.Side;
-import com.algoTrader.esper.EsperManager;
-import com.algoTrader.esper.TradeCallback;
 import com.algoTrader.util.DateUtil;
 import com.algoTrader.util.MyLogger;
 import com.algoTrader.util.RoundUtil;
@@ -34,6 +30,7 @@ import com.algoTrader.vo.BalanceVO;
 public class ForexServiceImpl extends ForexServiceBase {
 
     private static Logger logger = MyLogger.getLogger(ForexServiceImpl.class.getName());
+    private static Logger notificationLogger = MyLogger.getLogger("com.algoTrader.service.NOTIFICATION");
 
     private @Value("#{T(com.algoTrader.enumeration.Currency).fromString('${misc.portfolioBaseCurrency}')}") Currency portfolioBaseCurrency;
     private @Value("${misc.fxFutureEqualizationEnabled}") boolean fxFutureEqualizationEnabled;
@@ -55,6 +52,20 @@ public class ForexServiceImpl extends ForexServiceBase {
 
                 // check if expiration is below minimum
                 ForexFuture forexFuture = (ForexFuture) position.getSecurityInitialized();
+
+                Forex forex = (Forex) forexFuture.getUnderlyingInitialized();
+
+                Subscription forexSubscription = getSubscriptionDao().findByStrategyAndSecurity(StrategyImpl.BASE, forex.getId());
+                if (!forexSubscription.hasProperty("hedgingFamily")) {
+                    throw new IllegalStateException("no hedgingFamily defined for forex " + forex);
+                }
+
+                FutureFamily futureFamily = getFutureFamilyDao().load(forexSubscription.getIntProperty("hedgingFamily"));
+                if (!forexFuture.getSecurityFamily().equals(futureFamily)) {
+                    // continue forex is not hedged with this forexFutureFamily
+                    continue;
+                }
+
                 if (forexFuture.getTimeToExpiration() < this.fxFutureEqualizationMinTimeToExpiration) {
 
                     Order order = MarketOrder.Factory.newInstance();
@@ -70,15 +81,7 @@ public class ForexServiceImpl extends ForexServiceBase {
             // setup an TradeCallback so that new hedge positions are only setup when existing positions are closed
             if (orders.size() > 0) {
 
-                EsperManager.addTradeCallback(base.getName(), orders, new TradeCallback(true) {
-                    @Override
-                    public void onTradeCompleted(List<OrderStatus> orderStati) {
-
-                        // build new hedge positions by invoking equalizeForex again
-                        // use ServiceLocator because TradeCallback is executed in a new thread
-                        ServiceLocator.instance().getService("forexService", ForexService.class).equalizeForex();
-                    }
-                });
+                notificationLogger.info(orders.size() + " fx hedging position(s) have been closed due to approaching expiration, please run equalizeForex again");
 
                 // send the orders
                 for (Order order : orders) {
@@ -119,7 +122,7 @@ public class ForexServiceImpl extends ForexServiceBase {
 
                     Subscription forexSubscription = getSubscriptionDao().findByStrategyAndSecurity(StrategyImpl.BASE, forex.getId());
                     if (!forexSubscription.hasProperty("hedgingFamily")) {
-                        throw new IllegalStateException("no hedgingFamily defined for forex");
+                        throw new IllegalStateException("no hedgingFamily defined for forex " + forex);
                     }
 
                     FutureFamily futureFamily = getFutureFamilyDao().load(forexSubscription.getIntProperty("hedgingFamily"));
