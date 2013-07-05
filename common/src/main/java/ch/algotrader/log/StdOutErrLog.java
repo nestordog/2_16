5364c181c9921f41234404395f508a881388e5f1
aflury <aflury@19eef36c-86f0-de11-afab-0004ac968479>
2013-07-05 12:23:17 +0000

    add StdOutErrLog
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@6009 19eef36c-86f0-de11-afab-0004ac968479

---
+++ common/src/main/java/ch/algotrader/log/StdOutErrLog.java
@@ -0,0 +1,87 @@
+/***********************************************************************************
+ * AlgoTrader Enterprise Trading Framework
+ *
+ * Copyright (C) 2013 Flury Trading - All rights reserved
+ *
+ * All information contained herein is, and remains the property of Flury Trading.
+ * The intellectual and technical concepts contained herein are proprietary to
+ * Flury Trading. Modification, translation, reverse engineering, decompilation,
+ * disassembly or reproduction of this material is strictly forbidden unless prior
+ * written permission is obtained from Flury Trading
+ *
+ * Fur detailed terms and conditions consult the file LICENSE.txt or contact
+ *
+ * Flury Trading
+ * Badenerstrasse 16
+ * 8004 Zurich
+ ***********************************************************************************/
+package ch.algotrader.log;
+
+import java.io.PrintStream;
+
+import org.apache.log4j.Logger;
+
+import ch.algotrader.util.MyLogger;
+
+/**
+ * Redirects all System.out and System.err prints to log4j.
+ *
+ * @author <a href="mailto:aflury@algotrader.ch">Andy Flury</a>
+ *
+ * @version $Revision$ $Date$
+ */
+public class StdOutErrLog {
+
+    private static final Logger logger = MyLogger.getLogger(StdOutErrLog.class);
+
+    static {
+        tieSystemOutAndErrToLog();
+    }
+
+    public static void tieSystemOutAndErrToLog() {
+
+        System.setOut(createLoggingProxy(System.out, false));
+        System.setErr(createLoggingProxy(System.err, true));
+    }
+
+    public static PrintStream createLoggingProxy(final PrintStream realPrintStream, final boolean err) {
+
+        return new PrintStream(realPrintStream) {
+            @Override
+            public void print(final String string) {
+                if (err) {
+                    logger.error(string);
+                } else {
+                    logger.info(string);
+                }
+            }
+
+            @Override
+            public void print(final Object obj) {
+                if (err) {
+                    logger.error(obj);
+                } else {
+                    logger.info(obj);
+                }
+            }
+
+            @Override
+            public void println(final String string) {
+                if (err) {
+                    logger.error(string);
+                } else {
+                    logger.info(string);
+                }
+            }
+
+            @Override
+            public void println(final Object obj) {
+                if (err) {
+                    logger.error(obj);
+                } else {
+                    logger.info(obj);
+                }
+            }
+        };
+    }
+}
