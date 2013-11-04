3cbe7200426ec3a77690891f9cc38d3b38b9c273
aflury <aflury@19eef36c-86f0-de11-afab-0004ac968479>
2013-11-04 20:35:31 +0000

    add module test (containing integration tests)
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@6405 19eef36c-86f0-de11-afab-0004ac968479

---
+++ test/src/main/java/ch/algotrader/test/realtick/RTMessageListener.java
@@ -0,0 +1,62 @@
+/***********************************************************************************
+ * AlgoTrader Enterprise Trading Framework
+ *
+ * Copyright (C) 2013 Flury Trading - All rights reserved
+ *
+ * All information contained herein is, and remains the property of Flury Trading.
+ * The intellectual and technical concepts contained herein are proprietary to
+ * Flury Trading. Modification, translation, reverse engineering, decompilation,
+ * disassembly or reproduction of this material is strictly forbidden unless prior
+ * written permission is obtained from Flury Trading
+ *
+ * Fur detailed terms and conditions consult the file LICENSE.txt or contact
+ *
+ * Flury Trading
+ * Badenerstrasse 16
+ * 8004 Zurich
+ ***********************************************************************************/
+package ch.algotrader.test.realtick;
+
+import java.io.Serializable;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
+
+/**
+ * JMS MessageListener for RealTickMarketOrderTest
+ *
+ * @author <a href="mailto:okalnichevski@algotrader.ch">Oleg Kalnichevski</a>
+ *
+ * @version $Revision$ $Date$
+ */
+class RTMessageListener implements MessageListener {
+
+    private final LinkedBlockingQueue<Serializable> queue;
+
+    public RTMessageListener(final LinkedBlockingQueue<Serializable> queue) {
+        super();
+        this.queue = queue;
+    }
+
+    @Override
+    public void onMessage(final Message message) {
+        try {
+            try {
+                if (message instanceof ObjectMessage) {
+                    ObjectMessage objmsg = (ObjectMessage) message;
+                    this.queue.put(objmsg.getObject());
+                } else {
+                    org.junit.Assert.fail("Unxpected message: " + message);
+                }
+            } catch (InterruptedException e) {
+            } finally {
+                message.acknowledge();
+            }
+        } catch (JMSException ex) {
+            org.junit.Assert.fail(ex.getMessage());
+        }
+    }
+}
---
+++ test/src/main/java/ch/algotrader/test/realtick/RTTestSuite.java
@@ -0,0 +1,33 @@
+/***********************************************************************************
+ * AlgoTrader Enterprise Trading Framework
+ *
+ * Copyright (C) 2013 Flury Trading - All rights reserved
+ *
+ * All information contained herein is, and remains the property of Flury Trading.
+ * The intellectual and technical concepts contained herein are proprietary to
+ * Flury Trading. Modification, translation, reverse engineering, decompilation,
+ * disassembly or reproduction of this material is strictly forbidden unless prior
+ * written permission is obtained from Flury Trading
+ *
+ * Fur detailed terms and conditions consult the file LICENSE.txt or contact
+ *
+ * Flury Trading
+ * Badenerstrasse 16
+ * 8004 Zurich
+ ***********************************************************************************/
+package ch.algotrader.test.realtick;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * RealTick Test Suite
+ *
+ * @author <a href="mailto:okalnichevski@algotrader.ch">Oleg Kalnichevski</a>
+ *
+ * @version $Revision$ $Date$
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({RTOrderTest.class})
+public class RTTestSuite {
+}
