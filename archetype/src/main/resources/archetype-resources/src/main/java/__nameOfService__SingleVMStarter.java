2f8e31d2d547709f541cd761b211beaf2ebbdd1f
aurora <aurora@19eef36c-86f0-de11-afab-0004ac968479>
2013-11-05 10:22:41 +0000

    SingleVMStarte class fixed.
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@6410 19eef36c-86f0-de11-afab-0004ac968479

--- archetype/src/main/resources/archetype-resources/src/main/java/__nameOfService__SingleVMStarter.java
+++ archetype/src/main/resources/archetype-resources/src/main/java/__nameOfService__SingleVMStarter.java
@@ -1,11 +1,32 @@
 package ${package};
 
 import ch.algotrader.starter.BaseStarter;
+import ch.algotrader.esper.EsperManager;
+import ch.algotrader.starter.BaseStarter;
 
 /**
  * This class starts the strategy in Live Trading Mode in a single VM
  *
  */
-public class ${artifactId.substring(0,1).toUpperCase()}${artifactId.substring(1)}SingleVMStarter extends BaseStarter  {
+public class ${nameOfService}SingleVMStarter extends BaseStarter  {
 
+    public static void main(String[] args) throws Exception {
+
+        ServiceLocator.instance().init(ServiceLocator.SINGLE_BEAN_REFERENCE_LOCATION);
+
+        startBase();
+
+        ${nameOfService}Service ${artifactId}Service = ServiceLocator.instance().getService("${artifactId}Service", ${nameOfService}Service.class);
+        String strategyName = null;
+
+        EsperManager.initServiceProvider(strategyName);
+
+        EsperManager.deployInitModules(strategyName);
+
+        // switch to internalClock
+        EsperManager.setInternalClock(strategyName, true);
+
+        //activate the rest of the rules
+        EsperManager.deployRunModules(strategyName);
+    }
 }
