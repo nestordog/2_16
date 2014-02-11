f7b820585f34449f91a5824c354c520b570c360d
aflury <aflury@19eef36c-86f0-de11-afab-0004ac968479>
2014-02-11 18:19:19 +0000

    com.algotrader -> ch.algotrader
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@6783 19eef36c-86f0-de11-afab-0004ac968479

--- core/src/test/java/ch/algoTrader/entity/CostAccountingTest.java
+++ core/src/test/java/ch/algoTrader/entity/CostAccountingTest.java
@@ -15,7 +15,7 @@
  * Badenerstrasse 16
  * 8004 Zurich
  ***********************************************************************************/
-package com.algoTrader.entity;
+package ch.algoTrader.entity;
 
 import java.math.BigDecimal;
 import java.text.ParseException;
