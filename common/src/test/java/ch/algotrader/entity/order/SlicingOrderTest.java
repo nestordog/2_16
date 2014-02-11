0a2b99b9127523474ac2d562d5b5b6f794db1103
aflury <aflury@19eef36c-86f0-de11-afab-0004ac968479>
2014-02-11 16:44:54 +0000

    add test cases
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@6779 19eef36c-86f0-de11-afab-0004ac968479

---
+++ common/src/test/java/ch/algotrader/entity/order/SlicingOrderTest.java
@@ -0,0 +1,74 @@
+/***********************************************************************************
+ * AlgoTrader Enterprise Trading Framework
+ *
+ * Copyright (C) 2013 Flury Trading - All rights reserved
+ *
+ * All information contained herein is, and remains the property of Flury Trading.
+ * The intellectual and technical concepts contained herein are proprietary to
+ * Flury Trading. Modification, translation, reverse engineering, decompilation,
+ * disassembly or reproduction of this material is strictly forbidden unless prior
+ * written permission is obtained from Flury Trading
+ *
+ * Fur detailed terms and conditions consult the file LICENSE.txt or contact
+ *
+ * Flury Trading
+ * Badenerstrasse 16
+ * 8004 Zurich
+ ***********************************************************************************/
+package ch.algotrader.entity.order;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import ch.algotrader.entity.marketData.TickImpl;
+import ch.algotrader.entity.security.Security;
+import ch.algotrader.entity.trade.OrderValidationException;
+import ch.algotrader.entity.trade.SlicingOrder;
+import ch.algotrader.entity.trade.SlicingOrderImpl;
+import ch.algotrader.util.BeanUtil;
+
+/**
+ * @author <a href="mailto:aflury@algotrader.ch">Andy Flury</a>
+ *
+ * @version $Revision$ $Date$
+ */
+public class SlicingOrderTest {
+
+    @Test
+    public void testPopulate() throws IllegalAccessException, InvocationTargetException {
+
+        SlicingOrder order = SlicingOrder.Factory.newInstance();
+
+        String nameValues = "side=BUY,quantity=2,minVolPct=0.5,maxVolPct=1.5,minQuantity=5,maxQuantity=10,minDuration=1.0,minDuration=2.0,minDelay=1.0,maxDelay=2.0";
+
+        Map<String, String> properties = new HashMap<String, String>();
+        for (String nameValue : nameValues.split(",")) {
+            properties.put(nameValue.split("=")[0], nameValue.split("=")[1]);
+        }
+
+        BeanUtil.populate(order, properties);
+    }
+
+    @Test
+    public void testValidate() throws OrderValidationException {
+
+        Security security = Mockito.mock(Security.class);
+        Mockito.when(security.getCurrentMarketDataEvent()).thenReturn(new TickImpl());
+
+        SlicingOrder order = new SlicingOrderImpl();
+        order.setMinVolPct(0);
+        order.setMaxVolPct(0);
+        order.setMinQuantity(10);
+        order.setMaxQuantity(80);
+        order.setMaxDuration(1.0);
+        order.setMaxDelay(1.0);
+        order.setSecurity(security);
+
+        order.validate();
+    }
+
+}
---
+++ core/src/test/java/com/algoTrader/entity/CostAccountingTest.java
@@ -0,0 +1,142 @@
+/***********************************************************************************
+ * AlgoTrader Enterprise Trading Framework
+ *
+ * Copyright (C) 2013 Flury Trading - All rights reserved
+ *
+ * All information contained herein is, and remains the property of Flury Trading.
+ * The intellectual and technical concepts contained herein are proprietary to
+ * Flury Trading. Modification, translation, reverse engineering, decompilation,
+ * disassembly or reproduction of this material is strictly forbidden unless prior
+ * written permission is obtained from Flury Trading
+ *
+ * Fur detailed terms and conditions consult the file LICENSE.txt or contact
+ *
+ * Flury Trading
+ * Badenerstrasse 16
+ * 8004 Zurich
+ ***********************************************************************************/
+package com.algoTrader.entity;
+
+import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.math.util.MathUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import ch.algotrader.entity.Position;
+import ch.algotrader.entity.Transaction;
+import ch.algotrader.entity.security.Security;
+import ch.algotrader.entity.security.SecurityFamily;
+import ch.algotrader.entity.security.SecurityFamilyImpl;
+import ch.algotrader.entity.security.StockImpl;
+import ch.algotrader.entity.strategy.Strategy;
+import ch.algotrader.entity.strategy.StrategyImpl;
+import ch.algotrader.enumeration.Currency;
+import ch.algotrader.enumeration.TransactionType;
+import ch.algotrader.util.PositionUtil;
+
+/**
+ * Cost Accounting Test.
+ *
+ * Compare with "IT\Cost Accounting\Position Properties.xlsx"
+ *
+ * @author <a href="mailto:aflury@algotrader.ch">Andy Flury</a>
+ *
+ * @version $Revision$ $Date$
+ */
+public class CostAccountingTest {
+
+    private static SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+
+    @Test
+    public void test() throws ParseException {
+
+        Strategy strategy = new StrategyImpl();
+
+        SecurityFamily family = new SecurityFamilyImpl();
+        family.setContractSize(10);
+        family.setScale(2);
+
+        Security security = new StockImpl();
+        security.setSecurityFamily(family);
+
+        List<Transaction> transactions = new ArrayList<Transaction>();
+
+        Transaction transaction;
+
+        transaction = Transaction.Factory.newInstance(format.parse("08:00:00"), 10, new BigDecimal(100), Currency.USD, TransactionType.BUY, strategy);
+        transaction.setId(1);
+        transaction.setSecurity(security);
+        transaction.setExecutionCommission(new BigDecimal("10.0"));
+        transactions.add(transaction);
+
+        Position position = PositionUtil.processFirstTransaction(transaction);
+
+        assertPosition(position, 100, 10, 10010, 100.1, 10000, -10, 0);
+        assertPosition(position, 105, 10, 10010, 100.1, 10500, 490, 0);
+
+        transaction = Transaction.Factory.newInstance(format.parse("09:00:00"), 2, new BigDecimal(110), Currency.USD, TransactionType.BUY, strategy);
+        transaction.setId(2);
+        transaction.setSecurity(security);
+        transaction.setExecutionCommission(new BigDecimal("2.0"));
+        transactions.add(transaction);
+
+        PositionUtil.processTransaction(position, transaction);
+
+        assertPosition(position, 110, 12, 12212, 101.77, 13200, 988, 0);
+        assertPosition(position, 115, 12, 12212, 101.77, 13800, 1588, 0);
+
+        transaction = Transaction.Factory.newInstance(format.parse("10:00:00"), -12, new BigDecimal(120), Currency.USD, TransactionType.SELL, strategy);
+        transaction.setId(3);
+        transaction.setSecurity(security);
+        transaction.setExecutionCommission(new BigDecimal("12.0"));
+        transactions.add(transaction);
+
+        PositionUtil.processTransaction(position, transaction);
+
+        assertPosition(position, 120, 0, 0, Double.NaN, 0, 0, 2176);
+        assertPosition(position, 125, 0, 0, Double.NaN, 0, 0, 2176);
+
+        transaction = Transaction.Factory.newInstance(format.parse("11:00:00"), -20, new BigDecimal(130), Currency.USD, TransactionType.SELL, strategy);
+        transaction.setId(4);
+        transaction.setSecurity(security);
+        transaction.setExecutionCommission(new BigDecimal("20.0"));
+        transactions.add(transaction);
+
+        PositionUtil.processTransaction(position, transaction);
+
+        assertPosition(position, 130, -20, -25980, 129.9, -26000, -20, 2176);
+        assertPosition(position, 135, -20, -25980, 129.9, -27000, -1020, 2176);
+
+        transaction = Transaction.Factory.newInstance(format.parse("12:00:00"), 12, new BigDecimal(140), Currency.USD, TransactionType.BUY, strategy);
+        transaction.setId(5);
+        transaction.setSecurity(security);
+        transaction.setExecutionCommission(new BigDecimal("12.0"));
+        transactions.add(transaction);
+
+        PositionUtil.processTransaction(position, transaction);
+
+        assertPosition(position, 140, -8, -10392, 129.9, -11200, -808, 952);
+    }
+
+    private void assertPosition(Position position, double price, long eQty, double eCost, double eAvgPrice, double eMarketValue, double eUnrealizedPL, double eRealizedPL) {
+
+        long qty = position.getQuantity();
+        double cost = MathUtils.round(position.getCost(), 2);
+        double marketValue = MathUtils.round(qty * 10 * price, 2);
+        double unrealizedPL = MathUtils.round(marketValue - cost, 2);
+        double avgPrice = MathUtils.round(position.getAveragePrice(), 2);
+        double realizedPL = MathUtils.round(position.getRealizedPL(), 2);
+
+        Assert.assertEquals(eQty, qty);
+        Assert.assertEquals(eCost, cost, 0.001);
+        Assert.assertEquals(eMarketValue, marketValue, 0.001);
+        Assert.assertEquals(eUnrealizedPL, unrealizedPL, 0.001);
+        Assert.assertEquals(eAvgPrice, avgPrice, 0.001);
+        Assert.assertEquals(eRealizedPL, realizedPL, 0.001);
+    }
+}
