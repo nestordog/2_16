65b52799d729257e72c6bcc97621674922421bf3
aflury <aflury@19eef36c-86f0-de11-afab-0004ac968479>
2014-04-29 09:20:53 +0000

    add reporting
    git-svn-id: https://repo.algotrader.ch/svn/trunk/AlgoTrader@7092 19eef36c-86f0-de11-afab-0004ac968479

---
+++ common/src/main/java/ch/algotrader/report/ListReporter.java
@@ -0,0 +1,83 @@
+/***********************************************************************************
+ * AlgoTrader Enterprise Trading Framework
+ *
+ * Copyright (C) 2013 Flury Trading - All rights reserved
+ *
+ * All information contained herein is, and remains the property of Flury Trading.
+ * The intellectual and technical concepts contained herein are proprietary to
+ * Flury Trading. Modification, translation, reverse engineering, decompilation,
+ * disassembly or reproduction of this material is strictly forbidden unless prior
+ * written permission is obtained from Flury Trading
+ *
+ * Fur detailed terms and conditions consult the file LICENSE.txt or contact
+ *
+ * Flury Trading
+ * Badenerstrasse 16
+ * 8004 Zurich
+ ***********************************************************************************/
+package ch.algotrader.report;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+import org.supercsv.cellprocessor.ift.CellProcessor;
+import org.supercsv.io.CsvListWriter;
+import org.supercsv.prefs.CsvPreference;
+
+/**
+ * SuperCSV Writer that writes Maps to the specified CSV-File.
+ *
+ * @author <a href="mailto:aflury@algotrader.ch">Andy Flury</a>
+ *
+ * @version $Revision$ $Date$
+ */
+public class ListReporter {
+
+    private CellProcessor[] processor;
+    private CsvListWriter writer;
+
+    public ListReporter(File file, String[] header) throws IOException {
+
+        this(file, header, null);
+    }
+
+    public ListReporter(File file, String[] header, CellProcessor[] processor) throws IOException {
+
+        this.processor = processor;
+
+        this.writer = new CsvListWriter(new FileWriter(file, false), CsvPreference.EXCEL_PREFERENCE);
+
+        this.writer.writeHeader(header);
+    }
+
+    public ListReporter(String fileName, String[] header) throws IOException {
+
+        this(fileName, header, null);
+    }
+
+    public ListReporter(String fileName, String[] header, CellProcessor[] processor) throws IOException {
+
+        this(new File("files" + File.separator + "report" + File.separator + fileName + ".csv"), header, processor);
+    }
+
+    public void write(List<?> row) throws IOException {
+
+        if (processor != null) {
+            this.writer.write(row, processor);
+        } else {
+            this.writer.write(row);
+        }
+    }
+
+    public void write(Object... row) throws IOException {
+
+        this.writer.write(row);
+    }
+
+    public void close() throws IOException {
+
+        this.writer.close();
+    }
+}
---
+++ common/src/main/java/ch/algotrader/report/MapReporter.java
@@ -0,0 +1,80 @@
+/***********************************************************************************
+ * AlgoTrader Enterprise Trading Framework
+ *
+ * Copyright (C) 2013 Flury Trading - All rights reserved
+ *
+ * All information contained herein is, and remains the property of Flury Trading.
+ * The intellectual and technical concepts contained herein are proprietary to
+ * Flury Trading. Modification, translation, reverse engineering, decompilation,
+ * disassembly or reproduction of this material is strictly forbidden unless prior
+ * written permission is obtained from Flury Trading
+ *
+ * Fur detailed terms and conditions consult the file LICENSE.txt or contact
+ *
+ * Flury Trading
+ * Badenerstrasse 16
+ * 8004 Zurich
+ ***********************************************************************************/
+package ch.algotrader.report;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+import org.supercsv.cellprocessor.ift.CellProcessor;
+import org.supercsv.io.CsvMapWriter;
+import org.supercsv.prefs.CsvPreference;
+
+/**
+ * SuperCSV Writer that writes Maps to the specified CSV-File.
+ *
+ * @author <a href="mailto:aflury@algotrader.ch">Andy Flury</a>
+ *
+ * @version $Revision$ $Date$
+ */
+public class MapReporter {
+
+    private String[] header;
+    private CellProcessor[] processor;
+    private CsvMapWriter writer;
+
+    public MapReporter(File file, String[] header) throws IOException {
+
+        this(file, header, null);
+    }
+
+    public MapReporter(File file, String[] header, CellProcessor[] processor) throws IOException {
+
+        this.header = header;
+        this.processor = processor;
+
+        this.writer = new CsvMapWriter(new FileWriter(file, false), CsvPreference.EXCEL_PREFERENCE);
+
+        this.writer.writeHeader(header);
+    }
+
+    public MapReporter(String fileName, String[] header) throws IOException {
+
+        this(fileName, header, null);
+    }
+
+    public MapReporter(String fileName, String[] header, CellProcessor[] processor) throws IOException {
+
+        this(new File("files" + File.separator + "report" + File.separator + fileName + ".csv"), header, processor);
+    }
+
+    public void write(Map<String,?> row) throws IOException {
+
+        if (processor != null) {
+            this.writer.write(row, header, processor);
+        } else {
+            this.writer.write(row, header);
+        }
+    }
+
+    public void close() throws IOException {
+
+        this.writer.close();
+    }
+}
