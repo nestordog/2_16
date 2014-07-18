7c5cb0b45b28dd7be85ab535ad6beb057439c051
Oleg Kalnichevski <olegk@apache.org>
2014-07-18 11:52:17 +0200

    Fix for FXCM fill events with duplicate extIds

--- core/src/main/java/ch/algotrader/adapter/fxcm/FXCMFixOrderMessageHandler.java
+++ core/src/main/java/ch/algotrader/adapter/fxcm/FXCMFixOrderMessageHandler.java
@@ -71,7 +71,7 @@ public class FXCMFixOrderMessageHandler extends AbstractFix44OrderMessageHandler
     protected OrderStatus createStatus(final ExecutionReport executionReport, final Order order) throws FieldNotFound {
 
         // get the other fields
-        Status status = getStatus(executionReport.getOrdStatus(), executionReport.getCumQty());
+        Status status = getStatus(executionReport.getOrdStatus(), executionReport.getExecType(), executionReport.getCumQty());
         long filledQuantity = (long) executionReport.getCumQty().getValue();
         long remainingQuantity = (long) (executionReport.getOrderQty().getValue() - executionReport.getCumQty().getValue());
 
@@ -99,25 +99,26 @@ public class FXCMFixOrderMessageHandler extends AbstractFix44OrderMessageHandler
 
         // only create fills if status is FILLED (Note: FXCM advises to use STOPPED as the order execution confirmation)
         OrdStatus ordStatus = executionReport.getOrdStatus();
-        if (ordStatus.getValue() == OrdStatus.PARTIALLY_FILLED || ordStatus.getValue() == OrdStatus.STOPPED) {
+        CumQty qty = executionReport.getCumQty();
+        if (ordStatus.getValue() == OrdStatus.PARTIALLY_FILLED || (ordStatus.getValue() == OrdStatus.STOPPED && (long) qty.getValue() != 0L)) {
 
             // get the fields
             Date extDateTime = executionReport.getTransactTime().getValue();
             Side side = FixUtil.getSide(executionReport.getSide());
             long quantity = (long) executionReport.getCumQty().getValue();
 
-            // Note: DukasCopy does not use LastPx it only uses AvgPx
             BigDecimal price = RoundUtil.getBigDecimal(executionReport.getAvgPx().getValue(), order.getSecurity().getSecurityFamily().getScale());
             String extId = executionReport.getOrderID().getValue();
 
             // assemble the fill
+            // please note FXCM does not provide a unique exec report attribute
+            // that could be used as a unique extId
             Fill fill = Fill.Factory.newInstance();
             fill.setDateTime(new Date());
             fill.setExtDateTime(extDateTime);
             fill.setSide(side);
             fill.setQuantity(quantity);
             fill.setPrice(price);
-            fill.setExtId(extId);
 
             return fill;
         } else {
@@ -126,7 +127,7 @@ public class FXCMFixOrderMessageHandler extends AbstractFix44OrderMessageHandler
         }
     }
 
-    private static Status getStatus(OrdStatus ordStatus, CumQty cumQty) {
+    private static Status getStatus(OrdStatus ordStatus, ExecType execType, CumQty cumQty) {
 
         if (ordStatus.getValue() == OrdStatus.NEW || ordStatus.getValue() == OrdStatus.PENDING_NEW || ordStatus.getValue() == OrdStatus.REPLACED) {
             if (cumQty.getValue() == 0) {
@@ -134,8 +135,14 @@ public class FXCMFixOrderMessageHandler extends AbstractFix44OrderMessageHandler
             } else {
                 return Status.PARTIALLY_EXECUTED;
             }
-        } else if (ordStatus.getValue() == OrdStatus.STOPPED || ordStatus.getValue() == OrdStatus.FILLED) {
+        } else if (ordStatus.getValue() == OrdStatus.FILLED) {
             return Status.EXECUTED;
+        } else if (ordStatus.getValue() == OrdStatus.STOPPED) {
+            if (execType.getValue() == ExecType.TRADE) {
+                return Status.EXECUTED;
+            } else {
+                return Status.PARTIALLY_EXECUTED;
+            }
         } else if (ordStatus.getValue() == OrdStatus.CANCELED || ordStatus.getValue() == OrdStatus.REJECTED) {
             return Status.CANCELED;
         } else {
