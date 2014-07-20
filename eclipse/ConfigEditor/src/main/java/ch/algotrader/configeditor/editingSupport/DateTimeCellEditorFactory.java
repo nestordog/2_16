55ef131a24d79e829eb38f2dc119eb84062c02d9
Andrey Hihlovskiy <akhikhl@gmail.com>
2014-07-20 13:48:37 +0200

    introduced DateTimeCellEditor

--- eclipse/ConfigEditor/src/main/java/ch/algotrader/configeditor/editingSupport/DateTimeCellEditorFactory.java
+++ eclipse/ConfigEditor/src/main/java/ch/algotrader/configeditor/editingSupport/DateTimeCellEditorFactory.java
@@ -1,13 +1,12 @@
 package ch.algotrader.configeditor.editingSupport;
 
 import org.eclipse.jface.viewers.CellEditor;
-import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 
 public class DateTimeCellEditorFactory implements CellEditorFactory {
 
     @Override
     public CellEditor createCellEditor(Composite parent) {
-        return new DateCellEditor(parent, SWT.DATE | SWT.DROP_DOWN);
+        return new DateTimeCellEditor(parent);
     }
 }
