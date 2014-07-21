173807b7981c724e26877a8754343900287d47b9
Ilya Hihlovskiy <ilyakhi@googlemail.com>
2014-07-21 20:50:26 +0200

    Default propertyId fixed

--- eclipse/ConfigEditor/src/main/java/ch/algotrader/configeditor/FieldModel.java
+++ eclipse/ConfigEditor/src/main/java/ch/algotrader/configeditor/FieldModel.java
@@ -5,48 +5,48 @@ import java.util.Map;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class FieldModel {
-    ValueStruct values;
+  ValueStruct values;
 
-    public FieldModel(ValueStruct pValues) {
-        values = pValues;
-    }
+  public FieldModel(ValueStruct pValues) {
+    values = pValues;
+  }
 
-    public String getPropertyId() {
-        Map<String, Object> definition = getDefinition();
-        if (definition == null)
-            return "String";
-        Object type = definition.get("type");
-        return type == null ? null : type.toString();
-    }
+  public String getPropertyId() {
+    Map<String, Object> definition = getDefinition();
+    if (definition == null)
+      return "String";
+    Object type = definition.get("type");
+    return type == null ? "String" : type.toString();
+  }
 
-    public String getLabel() {
-        Map<String, Object> definition = getDefinition();
-        if (definition == null)
-            return null;
-        Object label = definition.get("label");
-        return label == null ? null : label.toString();
-    }
+  public String getLabel() {
+    Map<String, Object> definition = getDefinition();
+    if (definition == null)
+      return null;
+    Object label = definition.get("label");
+    return label == null ? null : label.toString();
+  }
 
-    public boolean getRequired() {
-        Map<String, Object> definition = getDefinition();
-        if (definition == null)
-            return true;
-        Object required = definition.get("required");
-        if (required == null)
-            return true;
-        return (Boolean) required;
-    }
+  public boolean getRequired() {
+    Map<String, Object> definition = getDefinition();
+    if (definition == null)
+      return true;
+    Object required = definition.get("required");
+    if (required == null)
+      return true;
+    return (Boolean) required;
+  }
 
-    @SuppressWarnings("unchecked")
-    private Map<String, Object> getDefinition() {
-        Map<String, Object> definition = null;
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            if (values.comments != null && !values.comments.isEmpty())
-                definition = mapper.readValue(values.comments.get(0), Map.class);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        return definition;
+  @SuppressWarnings("unchecked")
+  private Map<String, Object> getDefinition() {
+    Map<String, Object> definition = null;
+    ObjectMapper mapper = new ObjectMapper();
+    try {
+      if (values.comments != null && !values.comments.isEmpty())
+        definition = mapper.readValue(values.comments.get(0), Map.class);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
     }
+    return definition;
+  }
 }
