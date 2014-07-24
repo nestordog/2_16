6412401689ab46ce396ba5d25419de48c8c0d664
Ilya Hihlovskiy <ilyakhi@googlemail.com>
2014-07-24 21:17:37 +0200

    DateTime/Date format changed to american

--- eclipse/ConfigEditor/src/main/java/ch/algotrader/configeditor/editingsupport/DateTimeSerializer.java
+++ eclipse/ConfigEditor/src/main/java/ch/algotrader/configeditor/editingsupport/DateTimeSerializer.java
@@ -3,17 +3,13 @@
  *
  * Copyright (C) 2014 AlgoTrader GmbH - All rights reserved
  *
- * All information contained herein is, and remains the property of AlgoTrader GmbH.
- * The intellectual and technical concepts contained herein are proprietary to
- * AlgoTrader GmbH. Modification, translation, reverse engineering, decompilation,
- * disassembly or reproduction of this material is strictly forbidden unless prior
- * written permission is obtained from AlgoTrader GmbH
+ * All information contained herein is, and remains the property of AlgoTrader GmbH. The intellectual and technical concepts contained herein are proprietary to
+ * AlgoTrader GmbH. Modification, translation, reverse engineering, decompilation, disassembly or reproduction of this material is strictly forbidden unless
+ * prior written permission is obtained from AlgoTrader GmbH
  *
  * Fur detailed terms and conditions consult the file LICENSE.txt or contact
  *
- * AlgoTrader GmbH
- * Badenerstrasse 16
- * 8004 Zurich
+ * AlgoTrader GmbH Badenerstrasse 16 8004 Zurich
  ***********************************************************************************/
 package ch.algotrader.configeditor.editingsupport;
 
@@ -26,27 +22,27 @@ import ch.algotrader.configeditor.IPropertySerializer;
 
 public class DateTimeSerializer implements IPropertySerializer {
 
-  private final DateFormat format;
+    private final DateFormat format;
 
-  DateTimeSerializer() {
-    this.format = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
-  }
-
-  DateTimeSerializer(String format) {
-    this.format = new SimpleDateFormat(format);
-  }
-
-  @Override
-  public Object deserialize(String propValue) {
-    try {
-      return (Date) format.parse(propValue);
-    } catch (ParseException e) {
-      throw new RuntimeException(e);
+    DateTimeSerializer() {
+        this.format = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
     }
-  }
 
-  @Override
-  public String serialize(Object propObject) {
-    return format.format((Date) propObject);
-  }
+    DateTimeSerializer(String format) {
+        this.format = new SimpleDateFormat(format);
+    }
+
+    @Override
+    public Object deserialize(String propValue) {
+        try {
+            return (Date) format.parse(propValue);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String serialize(Object propObject) {
+        return format.format((Date) propObject);
+    }
 }
