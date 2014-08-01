bb95baf87d73557546bf0107fdac192c122de8ef
Andy Flury <aflury@algotrader.ch>
2014-08-01 16:26:48 +0200

    fix DateUtil

--- common/src/main/java/ch/algotrader/util/DateUtil.java
+++ common/src/main/java/ch/algotrader/util/DateUtil.java
@@ -45,7 +45,7 @@ public class DateUtil {
     public static Date getCurrentEPTime() {
 
         if (ServiceLocator.instance().isInitialized()) {
-            String strategyName = ServiceLocator.instance().getConfiguration().getStartedStrategyName();
+            String strategyName = ConfigLocator.instance().getCommonConfig().getStrategyName();
             if (EngineLocator.instance().hasEngine(strategyName) && !EngineLocator.instance().getEngine(strategyName).isInternalClock()) {
                 return new Date(EngineLocator.instance().getEngine(strategyName).getCurrentTime());
             }
