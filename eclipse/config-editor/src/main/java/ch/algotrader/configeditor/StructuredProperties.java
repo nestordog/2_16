9204cfe5806c0f41f0dca0a168b733fdb5359685
Andrey Hihlovskiy <akhikhl@gmail.com>
2014-08-23 18:18:04 +0200

    config-editor: implemented preservation of trailing comments in .properties files.

--- eclipse/config-editor/src/main/java/ch/algotrader/configeditor/StructuredProperties.java
+++ eclipse/config-editor/src/main/java/ch/algotrader/configeditor/StructuredProperties.java
@@ -26,6 +26,7 @@ import java.text.MessageFormat;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -39,134 +40,18 @@ public class StructuredProperties {
 
     private final Map<String, ValueStruct> properties;
     private final EditorPropertyPage propertyPage;
+    private List<String> trailingComments = null;
 
     public StructuredProperties(EditorPropertyPage propertyPage) {
         properties = new LinkedHashMap<String, ValueStruct>();
         this.propertyPage = propertyPage;
     }
 
-    public void load(File f, Collection<String> errorMessages) throws Exception {
-        ValueStruct n = new ValueStruct();
-        BufferedReader reader = new BufferedReader(new FileReader(f));
-        try {
-            String line;
-
-            while ((line = reader.readLine()) != null) {
-                if (line.isEmpty())
-                    n.comments.add("");
-                else if (line.charAt(0) == '#') {
-                    n.comments.add(line);
-                } else {
-                    try {
-                        parseKeyValueLine(n, line);
-                    } catch (Exception e) {
-                        if (errorMessages == null)
-                            throw e;
-                        String errMessage = MessageFormat.format("Error reading file ''{0}'': {1}", f.getName(), (e.getMessage() == null ? e.getClass().getName() : e.getMessage()));
-                        errorMessages.add(errMessage);
-                    }
-                    n = new ValueStruct();
-                }
-            }
-        } finally {
-            reader.close();
-        }
-    }
-
-    private void parseKeyValueLine(ValueStruct n, String line) throws Exception {
-        boolean escape = false;
-        boolean isKey = true;
-        StringBuilder key = new StringBuilder();
-        StringBuilder value = new StringBuilder();
-        String inlineComment = null;
-        for (int i = 0; i < line.length(); i++) {
-
-            if (escape) {
-                escape = false;
-                if (isKey)
-                    key.append(line.charAt(i));
-                else
-                    value.append(line.charAt(i));
-            } else if (line.charAt(i) == '\\') {
-                escape = true;
-            } else if (line.charAt(i) == '=') {
-                isKey = false;
-            } else if (line.charAt(i) == '#') {
-                inlineComment = line.substring(i + 1);
-                break;
-            } else {
-                if (isKey)
-                    key.append(line.charAt(i));
-                else
-                    value.append(line.charAt(i));
-            }
-        }
-        String stringKey = key.toString();
-        String stringValue = value.toString();
-
-        int firstNonSpaceIndex = stringKey.length();
-        for (int i = 0; i < stringKey.length(); i++)
-            if (!Character.isWhitespace(stringKey.charAt(i))) {
-                firstNonSpaceIndex = i;
-                break;
-            }
-        n.keyLeadingSpaces = stringKey.substring(0, firstNonSpaceIndex);
-        if (firstNonSpaceIndex != stringKey.length()) {
-            int lastNonSpaceIndex = -1;
-            for (int i = stringKey.length() - 1; i >= 0; i--)
-                if (!Character.isWhitespace(stringKey.charAt(i))) {
-                    lastNonSpaceIndex = i;
-                    break;
-                }
-            n.keyTrailingSpaces = stringKey.substring(lastNonSpaceIndex + 1, stringKey.length());
-        } else
-            n.keyTrailingSpaces = "";
-
-        firstNonSpaceIndex = stringValue.length();
-        for (int i = 0; i < stringValue.length(); i++)
-            if (!Character.isWhitespace(stringValue.charAt(i))) {
-                firstNonSpaceIndex = i;
-                break;
-            }
-        n.valueLeadingSpaces = stringValue.substring(0, firstNonSpaceIndex);
-        if (firstNonSpaceIndex != stringValue.length()) {
-            int lastNonSpaceIndex = -1;
-            for (int i = stringValue.length() - 1; i >= 0; i--)
-                if (!Character.isWhitespace(stringValue.charAt(i))) {
-                    lastNonSpaceIndex = i;
-                    break;
-                }
-            n.valueTrailingSpaces = stringValue.substring(lastNonSpaceIndex + 1, stringValue.length());
-        } else
-            n.valueTrailingSpaces = "";
-
-        String propertyId = new PropertyModel(n).getPropertyId();
-        n.value = propertyPage.projectProperties.deserialize(propertyId, stringValue.trim());
-        if (inlineComment != null)
-            n.inlineComment = inlineComment.trim();
-        properties.put(stringKey.trim(), n);
-    }
-
-    public void save(File f) throws IOException {
-        PrintWriter out = new PrintWriter(f);
-        try {
-            for (String key : properties.keySet()) {
-                ValueStruct struct = properties.get(key);
-                for (int i = 0; i < struct.comments.size(); i++) {
-                    if ((struct.comments.get(i)).equals(""))
-                        out.println();
-                    else
-                        out.println(struct.comments.get(i));
-                }
-                out.print(struct.keyLeadingSpaces + key + struct.keyTrailingSpaces + "=" + struct.valueLeadingSpaces + struct.getSaveReadyValue(propertyPage.projectProperties)
-                        + struct.valueTrailingSpaces);
-                if (struct.inlineComment != null)
-                    out.print(" #" + struct.inlineComment);
-                out.println();
-            }
-        } finally {
-            out.close();
-        }
+    public Iterable<String> getComments(String key) {
+        ValueStruct temp = properties.get(key);
+        if (temp != null && temp.comments != null)
+            return properties.get(key).comments;
+        return Collections.emptyList();
     }
 
     public Iterable<String> getKeys() {
@@ -184,6 +69,136 @@ public class StructuredProperties {
         return properties.get(key);
     }
 
+    public void load(File f, Collection<String> errorMessages) throws Exception {
+        ValueStruct struct = new ValueStruct();
+        BufferedReader reader = new BufferedReader(new FileReader(f));
+        try {
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                if (line.isEmpty())
+                    struct.comments.add("");
+                else if (line.charAt(0) == '#') {
+                    struct.comments.add(line);
+                } else {
+                    try {
+                        parseKeyValueLine(struct, line);
+                    } catch (Exception e) {
+                        if (errorMessages == null)
+                            throw e;
+                        String errMessage = MessageFormat.format("Error reading file ''{0}'': {1}", f.getName(), (e.getMessage() == null ? e.getClass().getName() : e.getMessage()));
+                        errorMessages.add(errMessage);
+                    }
+                    struct = new ValueStruct();
+                }
+            }
+        } finally {
+            reader.close();
+        }
+        if (!struct.comments.isEmpty())
+            trailingComments = struct.comments;
+    }
+
+    private void parseKeyValueLine(ValueStruct struct, String line) throws Exception {
+        boolean escape = false;
+        boolean isKey = true;
+        StringBuilder keyBuffer = new StringBuilder();
+        StringBuilder valueBuffer = new StringBuilder();
+        String inlineComment = null;
+        for (int i = 0; i < line.length(); i++) {
+
+            if (escape) {
+                escape = false;
+                if (isKey)
+                    keyBuffer.append(line.charAt(i));
+                else
+                    valueBuffer.append(line.charAt(i));
+            } else if (line.charAt(i) == '\\') {
+                escape = true;
+            } else if (line.charAt(i) == '=') {
+                isKey = false;
+            } else if (line.charAt(i) == '#') {
+                inlineComment = line.substring(i + 1);
+                break;
+            } else {
+                if (isKey)
+                    keyBuffer.append(line.charAt(i));
+                else
+                    valueBuffer.append(line.charAt(i));
+            }
+        }
+        String key = keyBuffer.toString();
+        String value = valueBuffer.toString();
+
+        int firstNonSpaceIndex = key.length();
+        for (int i = 0; i < key.length(); i++)
+            if (!Character.isWhitespace(key.charAt(i))) {
+                firstNonSpaceIndex = i;
+                break;
+            }
+        struct.keyLeadingSpaces = key.substring(0, firstNonSpaceIndex);
+        if (firstNonSpaceIndex != key.length()) {
+            int lastNonSpaceIndex = -1;
+            for (int i = key.length() - 1; i >= 0; i--)
+                if (!Character.isWhitespace(key.charAt(i))) {
+                    lastNonSpaceIndex = i;
+                    break;
+                }
+            struct.keyTrailingSpaces = key.substring(lastNonSpaceIndex + 1, key.length());
+        } else
+            struct.keyTrailingSpaces = "";
+
+        firstNonSpaceIndex = value.length();
+        for (int i = 0; i < value.length(); i++)
+            if (!Character.isWhitespace(value.charAt(i))) {
+                firstNonSpaceIndex = i;
+                break;
+            }
+        struct.valueLeadingSpaces = value.substring(0, firstNonSpaceIndex);
+        if (firstNonSpaceIndex != value.length()) {
+            int lastNonSpaceIndex = -1;
+            for (int i = value.length() - 1; i >= 0; i--)
+                if (!Character.isWhitespace(value.charAt(i))) {
+                    lastNonSpaceIndex = i;
+                    break;
+                }
+            struct.valueTrailingSpaces = value.substring(lastNonSpaceIndex + 1, value.length());
+        } else
+            struct.valueTrailingSpaces = "";
+
+        String propertyId = new PropertyModel(struct).getPropertyId();
+        struct.value = propertyPage.projectProperties.deserialize(propertyId, value.trim());
+        if (inlineComment != null)
+            struct.inlineComment = inlineComment.trim();
+        properties.put(key.trim(), struct);
+    }
+
+    public void save(File f) throws IOException {
+        PrintWriter out = new PrintWriter(f);
+        try {
+            for (String key : properties.keySet()) {
+                ValueStruct struct = properties.get(key);
+                writeComments(out, struct.comments);
+                out.print(struct.keyLeadingSpaces);
+                out.print(key);
+                out.print(struct.keyTrailingSpaces);
+                out.print("=");
+                out.print(struct.valueLeadingSpaces);
+                out.print(struct.getSaveReadyValue(propertyPage.projectProperties));
+                out.print(struct.valueTrailingSpaces);
+                if (struct.inlineComment != null) {
+                    out.print("#");
+                    out.print(struct.inlineComment);
+                }
+                out.println();
+            }
+            if (trailingComments != null)
+                writeComments(out, trailingComments);
+        } finally {
+            out.close();
+        }
+    }
+
     public void setValue(String key, Object value) {
         ValueStruct temp = properties.get(key);
         if (temp != null)
@@ -192,10 +207,8 @@ public class StructuredProperties {
             properties.put(key, new ValueStruct(value));
     }
 
-    public Iterable<String> getComments(String key) {
-        ValueStruct temp = properties.get(key);
-        if (temp != null && temp.comments != null)
-            return properties.get(key).comments;
-        return Collections.emptyList();
+    private void writeComments(PrintWriter out, List<String> comments) {
+        for (int i = 0; i < comments.size(); i++)
+            out.println(comments.get(i));
     }
 }
