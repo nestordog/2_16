972a400608bb3a76154373034014f9b6da3f95b1
Oleg Kalnichevski <olegk@apache.org>
2015-08-26 11:25:49 +0200

    Trading status event generation

---
+++ common/src/main/java/ch/algotrader/vo/marketData/TradingStatusEventVO.java
@@ -0,0 +1,66 @@
+/***********************************************************************************
+ * AlgoTrader Enterprise Trading Framework
+ *
+ * Copyright (C) 2014 AlgoTrader GmbH - All rights reserved
+ *
+ * All information contained herein is, and remains the property of AlgoTrader GmbH.
+ * The intellectual and technical concepts contained herein are proprietary to
+ * AlgoTrader GmbH. Modification, translation, reverse engineering, decompilation,
+ * disassembly or reproduction of this material is strictly forbidden unless prior
+ * written permission is obtained from AlgoTrader GmbH
+ *
+ * Fur detailed terms and conditions consult the file LICENSE.txt or contact
+ *
+ * AlgoTrader GmbH
+ * Badenerstrasse 16
+ * 8004 Zurich
+ ***********************************************************************************/
+package ch.algotrader.vo.marketData;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import ch.algotrader.enumeration.FeedType;
+import ch.algotrader.enumeration.TradingStatus;
+
+public class TradingStatusEventVO implements Serializable  {
+
+    private final TradingStatus status;
+    private final int securityId;
+    private final FeedType feedType;
+    private final Date dateTime;
+
+    public TradingStatusEventVO(final TradingStatus status, final int securityId, final FeedType feedType, final Date dateTime) {
+        this.status = status;
+        this.securityId = securityId;
+        this.feedType = feedType;
+        this.dateTime = dateTime;
+    }
+
+    public TradingStatus getStatus() {
+        return status;
+    }
+
+    public int getSecurityId() {
+        return securityId;
+    }
+
+    public FeedType getFeedType() {
+        return this.feedType;
+    }
+
+    public Date getDateTime() {
+        return this.dateTime;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "status=" + status +
+                ", securityId=" + securityId +
+                ", feedType=" + feedType +
+                ", dateTime=" + dateTime +
+                '}';
+    }
+
+}
--- core/src/main/java/ch/algotrader/adapter/lmax/LMAXFixMarketDataMessageHandler.java
+++ core/src/main/java/ch/algotrader/adapter/lmax/LMAXFixMarketDataMessageHandler.java
@@ -28,6 +28,7 @@ import ch.algotrader.esper.EngineLocator;
 import ch.algotrader.util.MyLogger;
 import ch.algotrader.vo.AskVO;
 import ch.algotrader.vo.BidVO;
+import ch.algotrader.vo.marketData.TradingHaltVO;
 import quickfix.FieldNotFound;
 import quickfix.Group;
 import quickfix.SessionID;
@@ -40,6 +41,7 @@ import quickfix.field.MDReqID;
 import quickfix.field.MDReqRejReason;
 import quickfix.field.NoMDEntries;
 import quickfix.field.SecurityID;
+import quickfix.field.SendingTime;
 import quickfix.fix44.MarketDataRequestReject;
 import quickfix.fix44.MarketDataSnapshotFullRefresh;
 
@@ -61,6 +63,13 @@ public class LMAXFixMarketDataMessageHandler extends AbstractFix44MarketDataMess
 
         Date date = null;
         int count = marketData.getGroupCount(NoMDEntries.FIELD);
+        if (count == 0) {
+
+            TradingHaltVO suspension = new TradingHaltVO(lmaxId, FeedType.LMAX, marketData.getHeader().getUtcTimeStamp(SendingTime.FIELD));
+            EngineLocator.instance().getServerEngine().sendEvent(suspension);
+            return;
+        }
+
         for (int i = 1; i <= count; i++) {
 
             Group group = marketData.getGroup(i, NoMDEntries.FIELD);
