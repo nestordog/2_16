407373164a9f9937b53c8fe4f8a8ce44e91f7a21
Oleg Kalnichevski <olegk@apache.org>
2015-09-21 16:57:47 +0200

    Updated archetype artifacts

--- archetype/src/main/resources/archetype-resources/src/main/java/__serviceName__Service.java
+++ archetype/src/main/resources/archetype-resources/src/main/java/__serviceName__Service.java
@@ -1,18 +1,18 @@
 package ${package};
 
 import java.math.BigDecimal;
-import ch.algotrader.service.StrategyServiceImpl;
+import ch.algotrader.service.StrategyService;
 import org.springframework.beans.factory.annotation.Value;
 
 /**
  * The main service class of the ${serviceName} Strategy
  */
-public class ${serviceName}Service extends StrategyServiceImpl {
+public class ${serviceName}Service extends StrategyService {
 
     // configuration variables
     private @Value("${strategyName}") String strategyName;
 
-    public void openPosition(String strategyName, int securityId, BigDecimal price) {
+    public void openPosition(String strategyName, long securityId, BigDecimal price) {
 
         //TODO: implement logic
     }
@@ -21,4 +21,23 @@ public class ${serviceName}Service extends StrategyServiceImpl {
 
         return this.strategyName;
     }
+
+    @Override
+    public void onInit(final LifecycleEventVO event) {
+        switch (event.getOperationMode()) {
+            case SIMULATION:
+                break;
+            case REAL_TIME:
+                break;
+        }
+    }
+
+    @Override
+    public void onPrefeed(final LifecycleEventVO event) {
+        switch (event.getOperationMode()) {
+            case REAL_TIME:
+                break;
+        }
+    }
+
 }
