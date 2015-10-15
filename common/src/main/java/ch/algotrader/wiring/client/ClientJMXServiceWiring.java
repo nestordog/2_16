b61dd20ae04e2076f18815396c90c0b499d415cd
Andy Flury <aflury@algotrader.ch>
2015-10-15 17:45:50 +0200

    rename client services

--- common/src/main/java/ch/algotrader/wiring/client/ClientJMXServiceWiring.java
+++ common/src/main/java/ch/algotrader/wiring/client/ClientJMXServiceWiring.java
@@ -15,7 +15,7 @@
  * Badenerstrasse 16
  * 8004 Zurich
  ***********************************************************************************/
-package ch.algotrader.wiring.client.service;
+package ch.algotrader.wiring.client;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
