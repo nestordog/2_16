6f21f892960da3c13174f57bbc1199b721c015f3
Oleg Kalnichevski <olegk@apache.org>
2016-01-06 19:50:36 +0100

    Better handling of absent market data in #getLeverage

---
+++ common/src/main/java/ch/algotrader/entity/security/LeverageCalculationException.java
@@ -0,0 +1,35 @@
+/***********************************************************************************
+ * AlgoTrader Enterprise Trading Framework
+ *
+ * Copyright (C) 2015 AlgoTrader GmbH - All rights reserved
+ *
+ * All information contained herein is, and remains the property of AlgoTrader GmbH.
+ * The intellectual and technical concepts contained herein are proprietary to
+ * AlgoTrader GmbH. Modification, translation, reverse engineering, decompilation,
+ * disassembly or reproduction of this material is strictly forbidden unless prior
+ * written permission is obtained from AlgoTrader GmbH
+ *
+ * Fur detailed terms and conditions consult the file LICENSE.txt or contact
+ *
+ * AlgoTrader GmbH
+ * Aeschstrasse 6
+ * 8834 Schindellegi
+ ***********************************************************************************/
+package ch.algotrader.entity.security;
+
+import ch.algotrader.service.ServiceException;
+
+/**
+ * Signals a problem with calculating leverage.
+ */
+public class LeverageCalculationException extends ServiceException {
+
+    public LeverageCalculationException(final String message) {
+        super(message);
+    }
+
+    public LeverageCalculationException(final String message, final Exception ex) {
+        super(message, ex);
+    }
+
+}
