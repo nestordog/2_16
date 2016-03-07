6fa93fcc34ac6e3e78ebe0f89ec7e6b3dec40df2
Oleg Kalnichevski <olegk@apache.org>
2016-03-07 11:00:43 +0100

    Issue #136: rejected order cancellation does not result in an order status message

--- core/src/main/java/ch/algotrader/adapter/fix/fix44/AbstractFix44OrderMessageHandler.java
+++ core/src/main/java/ch/algotrader/adapter/fix/fix44/AbstractFix44OrderMessageHandler.java
@@ -29,9 +29,9 @@ import ch.algotrader.service.OrderExecutionService;
 import ch.algotrader.service.TransactionService;
 import quickfix.FieldNotFound;
 import quickfix.SessionID;
+import quickfix.field.CxlRejReason;
 import quickfix.field.ExecType;
 import quickfix.field.MsgSeqNum;
-import quickfix.field.OrdStatus;
 import quickfix.field.Text;
 import quickfix.field.TransactTime;
 import quickfix.fix44.ExecutionReport;
@@ -202,37 +202,39 @@ public abstract class AbstractFix44OrderMessageHandler extends AbstractFix44Mess
 
     public void onMessage(final OrderCancelReject reject, final SessionID sessionID) throws FieldNotFound {
 
-        if (LOGGER.isWarnEnabled()) {
+        int value =  reject.isSetCxlRejReason() ? reject.getCxlRejReason().getValue() : CxlRejReason.OTHER;
+        String intId = reject.isSetClOrdID() ? reject.getClOrdID().getValue() : null;
+        String originalIntId = reject.isSetOrigClOrdID() ? reject.getOrigClOrdID().getValue() : null;
 
-            StringBuilder buf = new StringBuilder();
-            buf.append("Order cancel/replace has been rejected");
-            String clOrdID = reject.isSetClOrdID() ? reject.getClOrdID().getValue() : null;
-            buf.append(" [order ID: ").append(clOrdID).append("]");
-            String origClOrdID = reject.getOrigClOrdID().getValue();
-            buf.append(" [original order ID: ").append(origClOrdID).append("]");
-            if (reject.isSetField(Text.FIELD)) {
-                String text = reject.getText().getValue();
-                buf.append(": ").append(text);
+        if (value == CxlRejReason.TOO_LATE_TO_CANCEL) {
+            if (LOGGER.isInfoEnabled()) {
+
+                StringBuilder buf = new StringBuilder();
+                buf.append("Order has already been filled");
+                buf.append(" [order IntID: ").append(intId).append("]");
+                buf.append(" [original order IntID: ").append(originalIntId).append("]");
+                LOGGER.info(buf.toString());
             }
+        } else {
+            if (LOGGER.isErrorEnabled()) {
 
-            // warning only if order was already filled
-            OrdStatus ordStatus = reject.getOrdStatus();
-            if (ordStatus.getValue() == OrdStatus.FILLED) {
-                LOGGER.warn(buf.toString());
-            } else {
+                StringBuilder buf = new StringBuilder();
+                buf.append("Order cancel/replace has been rejected");
+                buf.append(" [order IntID: ").append(intId).append("]");
+                buf.append(" [original order IntID: ").append(originalIntId).append("]");
+                if (reject.isSetField(Text.FIELD)) {
+                    String text = reject.getText().getValue();
+                    buf.append(": ").append(text);
+                }
                 LOGGER.error(buf.toString());
             }
-        }
 
-        if (reject.isSetClOrdID()) {
-            String orderIntId = reject.getClOrdID().getValue();
-
-            Order order = this.orderExecutionService.getOpenOrderByIntId(orderIntId);
+            OrderStatus orderStatus = null;
+            Order order = intId != null ? this.orderExecutionService.getOpenOrderByIntId(intId) : null;
             if (order != null) {
-
-                OrderStatus orderStatus = OrderStatus.Factory.newInstance();
+                orderStatus = OrderStatus.Factory.newInstance();
                 orderStatus.setStatus(Status.REJECTED);
-                orderStatus.setIntId(orderIntId);
+                orderStatus.setIntId(intId);
                 orderStatus.setSequenceNumber(reject.getHeader().getInt(MsgSeqNum.FIELD));
                 orderStatus.setOrder(order);
                 if (reject.isSetField(TransactTime.FIELD)) {
@@ -243,7 +245,25 @@ public abstract class AbstractFix44OrderMessageHandler extends AbstractFix44Mess
 
                     orderStatus.setReason(reject.getText().getValue());
                 }
+            } else {
+                order = originalIntId != null ? this.orderExecutionService.getOpenOrderByIntId(originalIntId) : null;
+                if (order != null) {
+                    orderStatus = OrderStatus.Factory.newInstance();
+                    orderStatus.setStatus(Status.REJECTED);
+                    orderStatus.setIntId(originalIntId);
+                    orderStatus.setSequenceNumber(reject.getHeader().getInt(MsgSeqNum.FIELD));
+                    orderStatus.setOrder(order);
+                    if (reject.isSetField(TransactTime.FIELD)) {
 
+                        orderStatus.setExtDateTime(reject.getTransactTime().getValue());
+                    }
+                    if (reject.isSetField(Text.FIELD)) {
+
+                        orderStatus.setReason(reject.getText().getValue());
+                    }
+                }
+            }
+            if (orderStatus != null) {
                 this.serverEngine.sendEvent(orderStatus);
                 this.orderExecutionService.handleOrderStatus(orderStatus);
             }
---
+++ core/src/test/java/ch/algotrader/adapter/ib/TestIBFixOrderMessageHandler.java
@@ -0,0 +1,128 @@
+/***********************************************************************************
+ * AlgoTrader Enterprise Trading Framework
+ *
+ * Copyright (C) 2015 AlgoTrader GmbH - All rights reserved
+ *
+ * All information contained herein is, and remains the property of AlgoTrader GmbH.
+ * The intellectual and technical concepts contained herein are proprietary to
+ * AlgoTrader GmbH. Modification, translation, reverse engineering, decompilation,
+ * disassembly or reproduction of this material is strictly forbidden unless prior
+ * written permission is obtained from AlgoTrader GmbH
+ *
+ * Fur detailed terms and conditions consult the file LICENSE.txt or contact
+ *
+ * AlgoTrader GmbH
+ * Aeschstrasse 6
+ * 8834 Schindellegi
+ ***********************************************************************************/
+package ch.algotrader.adapter.ib;
+
+import java.util.concurrent.LinkedBlockingDeque;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import ch.algotrader.adapter.fix.fix42.FixTestUtils;
+import ch.algotrader.entity.security.Forex;
+import ch.algotrader.entity.security.ForexImpl;
+import ch.algotrader.entity.security.SecurityFamily;
+import ch.algotrader.entity.security.SecurityFamilyImpl;
+import ch.algotrader.entity.trade.MarketOrder;
+import ch.algotrader.entity.trade.MarketOrderImpl;
+import ch.algotrader.entity.trade.OrderStatus;
+import ch.algotrader.enumeration.Currency;
+import ch.algotrader.enumeration.Status;
+import ch.algotrader.esper.Engine;
+import ch.algotrader.service.OrderExecutionService;
+import ch.algotrader.service.TransactionService;
+import quickfix.fix42.OrderCancelReject;
+
+/**
+ * @author <a href="mailto:okalnichevski@algotrader.ch">Oleg Kalnichevski</a>
+ */
+public class TestIBFixOrderMessageHandler {
+
+    @Mock
+    private OrderExecutionService orderExecutionService;
+    @Mock
+    private TransactionService transactionService;
+    @Mock
+    private Engine engine;
+
+    private SecurityFamily family;
+    private Forex forex;
+
+    private IBFixOrderMessageHandler impl;
+
+    @Before
+    public void setup() throws Exception {
+
+        MockitoAnnotations.initMocks(this);
+
+        this.family = new SecurityFamilyImpl();
+        this.family.setCurrency(Currency.USD);
+        this.family.setScale(3);
+
+        this.forex = new ForexImpl();
+        this.forex.setSymbol("EUR.USD");
+        this.forex.setBaseCurrency(Currency.EUR);
+        this.forex.setSecurityFamily(family);
+
+
+        this.impl = new IBFixOrderMessageHandler(this.orderExecutionService, this.transactionService, new LinkedBlockingDeque<>(), this.engine);
+    }
+
+    @Test
+    public void testOrderCancelRejectedAlreadyFilled() throws Exception {
+
+        String s = "8=FIX.4.2|9=000151|35=9|34=012603|43=N|52=20160226-15:35:00|49=IB|56=algotr1|37=0|11=ibft354.1|41=ibft354.0|39=2|102=0|" +
+                "58=Cannot cancel the filled order|1=U1659362|434=1|10=054|";
+        OrderCancelReject cancelReject = FixTestUtils.parseFix42Message(s, OrderCancelReject.class);
+        Assert.assertNotNull(cancelReject);
+
+        MarketOrder order = new MarketOrderImpl();
+        order.setSecurity(this.forex);
+        Mockito.when(this.orderExecutionService.getOpenOrderByIntId("ibft354.0")).thenReturn(order);
+
+        this.impl.onMessage(cancelReject, FixTestUtils.fakeFix42Session());
+
+        Mockito.verify(this.orderExecutionService, Mockito.never()).handleOrderStatus(Mockito.any());
+    }
+
+    @Test
+    public void testOrderCancelRejectedUnknownOrder() throws Exception {
+
+        String s = "8=FIX.4.2|9=000134|35=9|34=011969|43=N|52=20160304-07:00:00|49=IB|56=algotr1|37=0|11=ibft639.1|41=ibft639.0|39=8|102=1|" +
+                "58=No such order|1=U1659362|434=1|10=004|";
+        OrderCancelReject cancelReject = FixTestUtils.parseFix42Message(s, OrderCancelReject.class);
+        Assert.assertNotNull(cancelReject);
+
+        SecurityFamily family = new SecurityFamilyImpl();
+        family.setCurrency(Currency.USD);
+        family.setScale(3);
+
+        Forex forex = new ForexImpl();
+        forex.setSymbol("EUR.USD");
+        forex.setBaseCurrency(Currency.EUR);
+        forex.setSecurityFamily(family);
+
+        MarketOrder order = new MarketOrderImpl();
+        order.setSecurity(forex);
+        Mockito.when(this.orderExecutionService.getOpenOrderByIntId("ibft639.0")).thenReturn(order);
+
+        this.impl.onMessage(cancelReject, FixTestUtils.fakeFix42Session());
+
+        ArgumentCaptor<OrderStatus> argumentCaptor = ArgumentCaptor.forClass(OrderStatus.class);
+        Mockito.verify(this.orderExecutionService, Mockito.times(1)).handleOrderStatus(argumentCaptor.capture());
+
+        OrderStatus orderStatus1 = argumentCaptor.getValue();
+        Assert.assertEquals("ibft639.0", orderStatus1.getIntId());
+        Assert.assertEquals(Status.REJECTED, orderStatus1.getStatus());
+    }
+
+}
